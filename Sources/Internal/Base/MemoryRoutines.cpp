dd2d03d50fc7a7311cd1367df512f4cc8d51fcd1
Vladimir Bondarenko <dominik@Vladimirs-Mac-mini.local>
2014-03-31 13:56:52 +0300

    AllocatorsStack made StaticSingleton

--- Sources/Internal/Base/MemoryRoutines.cpp
+++ Sources/Internal/Base/MemoryRoutines.cpp
@@ -9,7 +9,6 @@
 #include "Base/MemoryRoutines.h"
 
 #if defined(__USE_OWN_ALLOCATORS__)
-bool DAVA::AllocatorsStack::bInitialized = false;
 void * operator new(size_t _size) throw(std::bad_alloc )
 {
     void *p = NULL;
@@ -141,7 +140,7 @@ AllocatorsStack::AllocatorsStack()
     
 void *AllocatorsStack::Allocate(size_t _size)
 {
-    if(bInitialized && allocatorsStack.size() > 0)
+    if(allocatorsStack.size() > 0)
     {
         pthread_t threadID = pthread_self();
         Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
@@ -160,7 +159,7 @@ void *AllocatorsStack::Allocate(size_t _size)
     
 bool AllocatorsStack::Deallocate(void* ptr)
 {
-    if(bInitialized && allocatorsStack.size() > 0)
+    if(allocatorsStack.size() > 0)
     {
         pthread_t threadID = pthread_self();
         Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
@@ -180,38 +179,32 @@ bool AllocatorsStack::Deallocate(void* ptr)
 
 void AllocatorsStack::PushAllocator(Allocator *allocator)
 {
-    if(bInitialized)
+    pthread_t threadID = pthread_self();
+    Stack<Allocator*>* stack = NULL;
+    Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
+    if(it == allocatorsStack.end())
     {
-        pthread_t threadID = pthread_self();
-        Stack<Allocator*>* stack = NULL;
-        Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
-        if(it == allocatorsStack.end())
-        {
-            stack = new Stack<Allocator*>();
-            allocatorsStack[threadID] = stack;
-        }
-        else
-        {
-            stack = it->second;
-        }
-        if(stack)
-        {
-            stack->push(allocator);
-        }
+        stack = new Stack<Allocator*>();
+        allocatorsStack[threadID] = stack;
+    }
+    else
+    {
+        stack = it->second;
+    }
+    if(stack)
+    {
+        stack->push(allocator);
     }
 }
 
 void AllocatorsStack::PopAllocator()
 {
-    if(bInitialized)
+    pthread_t threadID = pthread_self();
+    Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
+    if(it != allocatorsStack.end())
     {
-        pthread_t threadID = pthread_self();
-        Map<pthread_t, Stack<Allocator*>*>::iterator it = allocatorsStack.find(threadID);
-        if(it != allocatorsStack.end())
-        {
-            Stack<Allocator*>* stack = it->second;
-            stack->pop();
-        }
+        Stack<Allocator*>* stack = it->second;
+        stack->pop();
     }
 }
     
