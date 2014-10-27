0567caa14a008a41a2276f79caabf7018aeeb451
leanid <leanid.chaika@gmail.com>
2014-10-27 15:26:40 +0300

    DF-5130 start iOS app from mac-book and get tests results

--- Projects/UnitTests/Reports/start_unit_tests.py
+++ Projects/UnitTests/Reports/start_unit_tests.py
@@ -1,3 +1,4 @@
+#!/bin/python
 
 import socket
 import subprocess
@@ -5,17 +6,32 @@ import sys
 
 current_host = socket.gethostname()
 current_ip = socket.gethostbyname(current_host)
-print("host ip: " + current_ip)
 
 HOST = current_ip
 PORT = 50007
 
+print("host ip: " + current_ip + ":" + str(PORT))
+
 # start application on device or else where then start server to listen unit test log output
 # TODO move to command line param of script
-start_on_android = False #  True
+start_on_android = False
+start_on_ios = False
 
-if start_on_android:
-    #start application on android device
+if len(sys.argv) > 1:
+    if sys.argv[1] == "android":
+        start_on_android = True
+    elif sys.argv[1] == "ios":
+        start_on_ios = True
+
+if start_on_ios:
+    app_name = "com.davaconsulting.UnitTests"
+    device_name = "iPadMini1FW" # TODO put your device name here
+    test_run_file = "testRun.js" # file with content: var target = UIATarget.localTarget(); target.delay( 7200 );
+    subprocess.Popen(["instruments", "-t", 
+        "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate", 
+        "-w", device_name, app_name, "-e",
+        "UIASCRIPT", test_run_file, "-host", str(HOST), "-port", str(PORT)])
+elif start_on_android:
     subprocess.Popen(["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests.UnitTests",
                       "-e", "-host", str(HOST), "-e", "-port", str(PORT)])
 elif sys.platform == 'win32':
