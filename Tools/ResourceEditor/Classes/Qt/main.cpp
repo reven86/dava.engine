e11b474683a3b922d1d40d0181ac7ef384a2a9ad
SaZ <dsazonoff@gmail.com>
2014-10-29 15:04:47 +0300

    [DF-5161] Fixed OpenGL compiling issues

--- Tools/ResourceEditor/Classes/Qt/main.cpp
+++ Tools/ResourceEditor/Classes/Qt/main.cpp
@@ -31,6 +31,7 @@
 #include "DAVAEngine.h"
 #include <QApplication>
 #include <QCryptographicHash>
+#include <QGLWidget>
 
 #include "version.h"
 #include "Main/mainwindow.h"
@@ -73,6 +74,8 @@ int main(int argc, char *argv[])
 	int ret = 0;
 
     QApplication a(argc, argv);
+
+	QGLWidget www;
 
 #if defined (__DAVAENGINE_MACOS__)
     DAVA::Core::Run(argc, argv);
