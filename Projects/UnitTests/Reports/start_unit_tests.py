69c7143cef425258f0b45b05222e940329d5d9d8
leanid <leanid.chaika@gmail.com>
2014-10-30 17:26:41 +0300

    DF-5129 add comments rename

--- Projects/UnitTests/Reports/start_unit_tests.py
+++ Projects/UnitTests/Reports/start_unit_tests.py
@@ -1,5 +1,22 @@
 #!/bin/python
 
+# start application on device or PC(Mac) 
+# then start server inside this script to listen unit test log output
+# exemple:
+# win32: 
+#> cd to dava.framevork/Projects/UnitTests/Reports
+#> python start_unit_tests.py
+# mac os x: 
+# CHANGE app_path in current script
+#> cd to dava.framevork/Projects/UnitTests/Reports
+#> python start_unit_tests.py
+# android:
+# deploy application on device first
+#> python start_unit_tests.py android
+# iOS:
+# deploy application on device first
+#> python start_unit_tests.py ios
+
 import socket
 import subprocess
 import sys
@@ -13,8 +30,6 @@ PORT = 50007
 
 print("host ip: " + current_ip + ":" + str(PORT))
 
-# start application on device or else where then start server to listen unit test log output
-# TODO move to command line param of script
 start_on_android = False
 start_on_ios = False
 
@@ -43,6 +58,7 @@ elif sys.platform == 'win32':
         subprocess.Popen(["..\\Release\\UnitTestsVS2010.exe", "-host", # run on local PC
                       str(HOST), "-port", str(PORT)], cwd="./..")
 elif sys.platform == "darwin":
+    # TODO set correct path for you
     app_path = "/Users/user123/Library/Developer/Xcode/DerivedData/TemplateProjectMacOS-bpogfklmgukhlmbnpxhfcjhfiwfq/Build/Products/Debug/UnitTests.app"
     subprocess.Popen(["open", "-a", app_path, "--args", "-host", str(HOST), "-port", str(PORT)])
 
