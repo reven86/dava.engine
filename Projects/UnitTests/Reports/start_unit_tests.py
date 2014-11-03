08dce2d1db7d57337c4042332f048c1f61daff8b
leanid <leanid.chaika@gmail.com>
2014-11-03 16:03:18 +0300

    DF5129 fix viktor review comment, make startup script detect crush on start, change linking for speedup on teamcity build agent

--- Projects/UnitTests/Reports/start_unit_tests.py
+++ Projects/UnitTests/Reports/start_unit_tests.py
@@ -21,6 +21,7 @@ import socket
 import subprocess
 import sys
 import os.path
+import time
 
 current_host = socket.gethostname()
 current_ip = socket.gethostbyname(current_host)
@@ -39,42 +40,64 @@ if len(sys.argv) > 1:
     elif sys.argv[1] == "ios":
         start_on_ios = True
 
+ponen_obj = None
+
 if start_on_ios:
     app_name = "com.davaconsulting.UnitTests"
     device_name = "iPadMini1FW" # TODO put your device name here
     test_run_file = "testRun.js" # file with content: var target = UIATarget.localTarget(); target.delay( 7200 );
-    subprocess.Popen(["instruments", "-t", 
+    ponen_obj = subprocess.Popen(["instruments", "-t",
         "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate", 
         "-w", device_name, app_name, "-e",
         "UIASCRIPT", test_run_file, "-host", str(HOST), "-port", str(PORT)])
 elif start_on_android:
-    subprocess.Popen(["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests.UnitTests",
+    ponen_obj = subprocess.Popen(["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests.UnitTests",
                       "-e", "-host", str(HOST), "-e", "-port", str(PORT)])
 elif sys.platform == 'win32':
     if os.path.isfile("..\\Release\\app\\UnitTestsVS2010.exe"): # run on build server (TeamCity)
-        subprocess.Popen(["..\\Release\\app\\UnitTestsVS2010.exe", "-host",
+        ponen_obj = subprocess.Popen(["..\\Release\\app\\UnitTestsVS2010.exe", "-host",
                       str(HOST), "-port", str(PORT)], cwd="./..")
     else:
-        subprocess.Popen(["..\\Release\\UnitTestsVS2010.exe", "-host", # run on local PC
+        ponen_obj = subprocess.Popen(["..\\Release\\UnitTestsVS2010.exe", "-host", # run on local PC
                       str(HOST), "-port", str(PORT)], cwd="./..")
 elif sys.platform == "darwin":
     # TODO set correct path for you
     app_path = "/Users/user123/Library/Developer/Xcode/DerivedData/TemplateProjectMacOS-bpogfklmgukhlmbnpxhfcjhfiwfq/Build/Products/Debug/UnitTests.app"
-    subprocess.Popen(["open", "-a", app_path, "--args", "-host", str(HOST), "-port", str(PORT)])
+    ponen_obj = subprocess.Popen(["open", "-a", app_path, "--args", "-host", str(HOST), "-port", str(PORT)])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 print("start listen")
-conn, addr = s.accept()
+try:
+    s.settimeout(5)
+    conn, addr = s.accept()
+except socket.error, msg:
+    print(msg)
+    print("can't accept client error: " + str(msg))
+
+    current_exit_code = ponen_obj.poll()
+    if current_exit_code is not None:
+        print("UnitTests finished with code: " + str(current_exit_code))
+    else:
+        print("UnitTests still running... may be increase timeout? ")
+    sys.exit(-1)
+
 print("connected by:" + str(addr))
+
+try:
+    s.setblocking(1)
+    s.settimeout(None)
+    conn.setblocking(1)
+except socket.error, msg:
+    print("can't make socket blocking:" + msg)
 
 while 1:
     data = None
     try:
         data = conn.recv(1024)
     except socket.error, msg:
-        print(msg)
+        print("can't receive data course:" + str(msg))
         break
     if not data:
         break
