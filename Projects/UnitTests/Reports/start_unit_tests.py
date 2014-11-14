1f1f57a7b6e2803ee6aefe3bbdf7575c36ce5c7c
leanid <leanid.chaika@gmail.com>
2014-11-14 15:53:28 +0300

    DF-5129 fix alexey review

--- Projects/UnitTests/Reports/start_unit_tests.py
+++ Projects/UnitTests/Reports/start_unit_tests.py
@@ -7,11 +7,12 @@
 #> cd to dava.framevork/Projects/UnitTests/Reports
 #> python start_unit_tests.py
 # mac os x: 
-# CHANGE app_path in current script
+#  make DerivedData relative to project goto Xcode->Preferences->Location->DerivedData select relative
+#  build project
 #> cd to dava.framevork/Projects/UnitTests/Reports
 #> python start_unit_tests.py
 # android:
-# deploy application on device first
+# deploy application to device first
 #> python start_unit_tests.py android
 # iOS:
 # deploy application on device first
@@ -31,6 +32,7 @@ s.close()
 
 HOST = current_ip
 PORT = 50007
+PRJ_NAME_BASE = "UnitTests"
 
 start_on_android = False
 start_on_ios = False
@@ -44,7 +46,7 @@ if len(sys.argv) > 1:
 popen_obj = None
 
 if start_on_ios:
-    app_name = "com.davaconsulting.UnitTests"
+    app_name = "com.davaconsulting."+PRJ_NAME_BASE
     device_name = "iPadMini1FW" # TODO put your device name here
     test_run_file = "testRun.js" # file with content: var target = UIATarget.localTarget(); target.delay( 7200 );
     popen_obj = subprocess.Popen(["instruments", "-t",
@@ -52,23 +54,23 @@ if start_on_ios:
         "-w", device_name, app_name, "-e",
         "UIASCRIPT", test_run_file, "-host", str(HOST), "-port", str(PORT)])
 elif start_on_android:
-    popen_obj = subprocess.Popen(["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests.UnitTests",
+    popen_obj = subprocess.Popen(["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests."+PRJ_NAME_BASE,
                       "-e", "-host", str(HOST), "-e", "-port", str(PORT)])
 elif sys.platform == 'win32':
-    if os.path.isfile("..\\Release\\app\\UnitTestsVS2010.exe"): # run on build server (TeamCity)
-        popen_obj = subprocess.Popen(["..\\Release\\app\\UnitTestsVS2010.exe", "-host",
+    if os.path.isfile("..\\Release\\app\\"+PRJ_NAME_BASE+"2010.exe"): # run on build server (TeamCity)
+        popen_obj = subprocess.Popen(["..\\Release\\app\\"+PRJ_NAME_BASE+"VS2010.exe", "-host",
                       str(HOST), "-port", str(PORT)], cwd="./..")
     else:
-        popen_obj = subprocess.Popen(["..\\Release\\UnitTestsVS2010.exe", "-host", # run on local PC
+        popen_obj = subprocess.Popen(["..\\Release\\"+PRJ_NAME_BASE+"VS2010.exe", "-host", # run on local PC
                       str(HOST), "-port", str(PORT)], cwd="./..")
 elif sys.platform == "darwin":
-    if os.path.exists("./UnitTests.app"):
+    if os.path.exists("./"+PRJ_NAME_BASE+".app"):
         # if run on teamcity current dir is: Projects/UnitTests/DerivedData/TemplateProjectMacOS/Build/Products/Release
         app_path = "./UnitTests.app"
     else:
         # run on local machine from dir: UnitTests/Report
         # Warning! To make DerivedData relative to project goto Xcode->Preferences->Location->DerivedData select relative 
-        app_path = "../DerivedData/TemplateProjectMacOS/Build/Products/Release/UnitTests.app"
+        app_path = "../DerivedData/TemplateProjectMacOS/Build/Products/Release/"+PRJ_NAME_BASE+".app"
     popen_obj = subprocess.Popen(["open", "-W", app_path, "--args", "-host", str(HOST), "-port", str(PORT)])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -85,9 +87,9 @@ except socket.error, msg:
 
     current_exit_code = popen_obj.poll()
     if current_exit_code is not None:
-        print("UnitTests finished with code: " + str(current_exit_code))
+        print(PRJ_NAME_BASE+" finished with code: " + str(current_exit_code))
     else:
-        print("UnitTests still running... may be increase timeout? ")
+        print(PRJ_NAME_BASE+" still running... may be increase timeout? ")
     sys.exit(-1)
 
 print("connected by:" + str(addr))
