e65ff7586a0283ace4b2dd0326b0562c46e66c14
Leanid Chaika <leanid.chaika@gmail.com>
2014-12-02 11:46:07 +0300

    DF-5129 fix unit test console output on max os x, now only team city messages go to output

--- Projects/UnitTests/Reports/start_unit_tests.py
+++ Projects/UnitTests/Reports/start_unit_tests.py
@@ -2,34 +2,26 @@
 
 # start application on device or PC(Mac) 
 # then start server inside this script to listen unit test log output
-# exemple:
+# example:
 # win32: 
-# > cd to dava.framevork/Projects/UnitTests/Reports
-#> python start_unit_tests.py
+# > cd to dava.framework/Projects/UnitTests/Reports
+# > python start_unit_tests.py
 # mac os x: 
-#  make DerivedData relative to project goto Xcode->Preferences->Location->DerivedData select relative
+# make DerivedData relative to project go to Xcode->Preferences->Location->DerivedData select relative
 #  build project
-#> cd to dava.framevork/Projects/UnitTests/Reports
-#> python start_unit_tests.py
+# > cd to dava.framework/Projects/UnitTests/Reports
+# > python start_unit_tests.py
 # android:
 # deploy application to device first
-#> python start_unit_tests.py android
+# > python start_unit_tests.py android
 # iOS:
 # deploy application on device first
 # "Enable UI Automation" must be turned on in the "Developer" section of the "Settings" app on device
-#> python start_unit_tests.py ios
+# > python start_unit_tests.py ios
 
-import socket
 import subprocess
 import sys
 import os.path
-import time
-
-# Workaround to get correct ip adress on mac os x on wifi connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("gmail.com", 80))
-current_ip = s.getsockname()[0]
-s.close()
 
 
 def get_postfix(platform):
@@ -40,8 +32,7 @@ def get_postfix(platform):
     else:
         return ''
 
-HOST = current_ip
-PORT = 50007
+
 PRJ_NAME_BASE = "UnitTests"
 PRJ_POSTFIX = get_postfix(sys.platform)
 
@@ -54,75 +45,56 @@ if len(sys.argv) > 1:
     elif sys.argv[1] == "ios":
         start_on_ios = True
 
-popen_obj = None
+sub_process = None
 
 if start_on_ios:
     app_name = "com.davaconsulting." + PRJ_NAME_BASE
     device_name = "Framework iPad Air2"  # TODO put your device name here
     test_run_file = "testRun.js"  # file with content: var target = UIATarget.localTarget(); target.delay( 7200 );
-    popen_obj = subprocess.Popen(["instruments", "-t",
-                                  "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate",
-                                  "-w", device_name, app_name, "-e",
-                                  "UIASCRIPT", test_run_file, "-host", str(HOST), "-port", str(PORT)])
+    sub_process = subprocess.Popen(["instruments", "-t",
+                                    "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns \
+                                  /AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate",
+                                    "-w", device_name, app_name, "-e",
+                                    "UIASCRIPT", test_run_file])
 elif start_on_android:
-    popen_obj = subprocess.Popen(
-        ["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests." + PRJ_NAME_BASE,
-         "-e", "-host", str(HOST), "-e", "-port", str(PORT)])
+    sub_process = subprocess.Popen(
+        ["adb", "shell", "am", "start", "-n", "com.dava.unittests/com.dava.unittests." + PRJ_NAME_BASE])
 elif sys.platform == 'win32':
     if os.path.isfile("..\\Release\\app\\" + PRJ_NAME_BASE + PRJ_POSTFIX):  # run on build server (TeamCity)
-        popen_obj = subprocess.Popen(["..\\Release\\app\\" + PRJ_NAME_BASE + PRJ_POSTFIX, "-host",
-                                      str(HOST), "-port", str(PORT)], cwd="./..")
+        sub_process = subprocess.Popen(["..\\Release\\app\\" + PRJ_NAME_BASE + PRJ_POSTFIX], cwd="./..")
     else:
-        popen_obj = subprocess.Popen(["..\\Release\\" + PRJ_NAME_BASE + PRJ_POSTFIX, "-host",  # run on local PC
-                                      str(HOST), "-port", str(PORT)], cwd="./..")
+        sub_process = subprocess.Popen(["..\\Release\\" + PRJ_NAME_BASE + PRJ_POSTFIX], cwd="./..")
 elif sys.platform == "darwin":
     if os.path.exists("./" + PRJ_NAME_BASE + PRJ_POSTFIX):
         # if run on teamcity current dir is: Projects/UnitTests/DerivedData/TemplateProjectMacOS/Build/Products/Release
         app_path = "./" + PRJ_NAME_BASE + PRJ_POSTFIX
     else:
         # run on local machine from dir: UnitTests/Report
-        # Warning! To make DerivedData relative to project goto Xcode->Preferences->Location->DerivedData select relative 
-        app_path = "../DerivedData/TemplateProjectMacOS/Build/Products/Release/" + PRJ_NAME_BASE + PRJ_POSTFIX
-    popen_obj = subprocess.Popen(["open", "-W", app_path, "--args", "-host", str(HOST), "-port", str(PORT)])
+        # Warning! To make DerivedData relative to project go to
+        # Xcode->Preferences->Location->DerivedData select relative
+        app_path = "../DerivedData/TemplateProjectMacOS/Build/Products/Release/UnitTests.app/Contents/MacOS/"\
+                   + PRJ_NAME_BASE
+    sub_process = subprocess.Popen([app_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("bind: " + str(HOST) + ":" + str(PORT))
-s.bind((HOST, PORT))
-s.listen(1)
-print("start listen")
-try:
-    s.settimeout(10)
-    conn, addr = s.accept()
-except socket.error, msg:
-    print(msg)
-    print("can't accept client error: " + str(msg))
+app_exit_code = -1
 
-    current_exit_code = popen_obj.poll()
-    if current_exit_code is not None:
-        print(PRJ_NAME_BASE + " finished with code: " + str(current_exit_code))
-    else:
-        print(PRJ_NAME_BASE + " still running... may be increase timeout? ")
-    sys.exit(-1)
-
-print("connected by:" + str(addr))
-
-try:
-    s.setblocking(1)
-    s.settimeout(None)
-    conn.setblocking(1)
-except socket.error, msg:
-    print("can't make socket blocking:" + msg)
-
-while 1:
-    data = None
+while True:
     try:
-        data = conn.recv(1024)
-    except socket.error, msg:
-        print("can't receive data course:" + str(msg))
-        break
-    if not data:
-        break
-    sys.stdout.write(data)
-    sys.stdout.flush()  #  want teamcity show output realtime for every test
+        line = sub_process.stdout.readline()
+        if line != '':
+            teamcity_line_index = line.find("##teamcity")
+            if teamcity_line_index != -1:
+                teamcity_line = line[teamcity_line_index:]
+                sys.stdout.write(teamcity_line)
+                sys.stdout.flush()
+        else:
+            break
+        line = sub_process.stderr.readline()
+        if line != '' and line.find("exited with status = 0") != -1:
+            app_exit_code = 0
 
-conn.close()
+    except IOError:
+        sys.stdout.write(IOError.message)
+        sys.stdout.flush()
+
+sys.exit(app_exit_code)
