348091ab4c24ed1875423148367a4dc6a6cf79e2
leanid <leanid.chaika@gmail.com>
2014-12-05 10:23:14 +0300

    DF-5129 fix android build

--- Projects/UnitTests/android/jni/Application.mk
+++ Projects/UnitTests/android/jni/Application.mk
@@ -6,12 +6,12 @@ APP_GNUSTL_FORCE_CPP_FEATURES := rtti
 #debug
 #APP_CFLAGS += -DNDK_DEBUG=1 -O0
 #APP_CFLAGS += -D__DAVAENGINE_DEBUG__
-#APP_OPTIM := debug
+APP_OPTIM := debug
 APP_CFLAGS += -DUSE_LOCAL_RESOURCES #use local resources
 
-release
+#release
 APP_CFLAGS += -O2
-APP_OPTIM := release
+#APP_OPTIM := release
 
 APP_CFLAGS += -Wno-invalid-offsetof
 APP_LDLIBS := -fuse-ld=gold -fno-exceptions
