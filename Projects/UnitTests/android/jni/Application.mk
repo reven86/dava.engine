297be3b34ff62fca92f0162092794f398ad42847
Aleksei Kanash <aleksei.kanash@gmail.com>
2014-12-30 13:23:37 +0300

    [DF-5228] JniHelper Call for jobject. C++11 for framework. Codestyle fixes.

--- Projects/UnitTests/android/jni/Application.mk
+++ Projects/UnitTests/android/jni/Application.mk
@@ -19,5 +19,4 @@ APP_LDLIBS := -fuse-ld=gold -fno-exceptions
 APP_ABI := armeabi-v7a x86
 APP_PLATFORM := android-14
 
-APP_CFLAGS += -std=c++11
-NDK_TOOLCHAIN_VERSION=4.8
+NDK_TOOLCHAIN_VERSION=clang
