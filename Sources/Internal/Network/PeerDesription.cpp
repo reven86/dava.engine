5ee4eb9f0c6934b7e9117a53a79b2d09ee33ebbd
Maksim Lazarev <m_lazarev@wargaming.net>
2015-01-06 10:49:43 +0300

    DF-5198 add comparison operators for IfAddress, Endpointm IPAddress

--- Sources/Internal/Network/PeerDesription.cpp
+++ Sources/Internal/Network/PeerDesription.cpp
@@ -60,5 +60,36 @@ PeerDescription::PeerDescription(const NetConfig& config)
     DVASSERT(true == netConfig.Validate());
 }
 
+#ifdef __DAVAENGINE_DEBUG__
+void PeerDescription::DumpToStdout() const
+{
+    printf("PeerDescription: %s, screen %dx%d, UDID=%s\n", name.c_str(), screenInfo.width, screenInfo.height, udid.c_str());
+    printf("  %s %s %s %s\n", manufacturer.c_str(), model.c_str(), platform.c_str(), version.c_str());
+    printf("  Network interfaces:\n");
+    for (size_t i = 0, n = ifaddr.size();i < n;++i)
+    {
+        printf("    %s\n", ifaddr[i].Address().ToString().c_str());
+    }
+    printf("  Network configuration:\n");
+    for (size_t i = 0, n = netConfig.Transports().size();i < n;++i)
+    {
+        const char* s = "unknown";
+        switch (netConfig.Transports()[i].type)
+        {
+        case TRANSPORT_TCP:
+            s = "TCP";
+            break;
+        }
+        printf("    %s: %hu\n", s, netConfig.Transports()[i].endpoint.Port());
+    }
+    printf("    services: ");
+    for (size_t i = 0, n = netConfig.Services().size();i < n;++i)
+    {
+        printf("%u; ", netConfig.Services()[i]);
+    }
+    printf("\n");
+}
+#endif
+
 }   // namespace Net
 }   // namespace DAVA
