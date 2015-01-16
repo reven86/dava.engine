f5a5a1fde42dcfd63291495b92c519ae4a37ccba
Kernel <Kernel@tut.by>
2015-01-16 15:09:07 +0300

    DF-5184 changed instanced draw for android

--- Sources/Internal/Render/RenderManagerGL.cpp
+++ Sources/Internal/Render/RenderManagerGL.cpp
@@ -458,8 +458,8 @@ void RenderManager::HWDrawElementsInstanced(ePrimitiveType type, int32 count, eI
     {
         Logger::FrameworkDebug("Draw arrays texture state: id %d", currentState.textureState);
     }
-#if defined(__DAVAENGINE_IPHONE__) || defined (__DAVAENGINE_ANDROID__)
-    RENDER_VERIFY(glDrawElementsInstancedEXT(mode, count, indexTypes[indexFormat], indices, instanceCount));
+#if defined(__DAVAENGINE_IPHONE__)
+    RENDER_VERIFY(glDrawElementsInstanced(mode, count, indexTypes[indexFormat], indices, instanceCount));
 #else 
     RENDER_VERIFY(glDrawElementsInstancedARB(mode, count, indexTypes[indexFormat], indices, instanceCount));
 #endif 
