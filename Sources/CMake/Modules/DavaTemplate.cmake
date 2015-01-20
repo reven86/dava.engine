cd573b19ae5fe68cbc6c2a73a207862dba03115f
Igor.Bratchik <stinvi@rambler.ru>
2015-01-20 18:54:01 +0300

    [DF-5703] minor fix

--- Sources/CMake/Modules/DavaTemplate.cmake
+++ Sources/CMake/Modules/DavaTemplate.cmake
@@ -67,11 +67,11 @@ if( APPLE )
 elseif ( MSVC )
 
     set_target_properties ( ${PROJECT_NAME} PROPERTIES LINK_FLAGS "/NODEFAULTLIB:\"libcmt.lib;libcmtd.lib\"" )    
-    set_target_properties ( ${PROJECT_NAME} PROPERTIES LINK_FLAGS_RELEASE "/SUBSYSTEM:WINDOWS" )
     
     if( DEBUG_INFO )   
-        set_target_properties ( ${PROJECT_NAME} PROPERTIES LINK_FLAGS_RELEASE "/DEBUG" )
-
+        set_target_properties ( ${PROJECT_NAME} PROPERTIES LINK_FLAGS_RELEASE "/DEBUG /SUBSYSTEM:WINDOWS" )
+    else()
+        set_target_properties ( ${PROJECT_NAME} PROPERTIES LINK_FLAGS_RELEASE "/SUBSYSTEM:WINDOWS" )
     endif()
 
     list( APPEND DAVA_BINARY_WIN32_DIR "${ADDED_BINARY_DIR}" )
