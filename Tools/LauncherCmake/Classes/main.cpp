58ee160a2c0ccb02634d23851465bcad0ea0355e
IgorSolovey <Salikq@gmail.com>
2015-03-21 17:24:22 +0300

    DF-5805 change platform plugin path for qt; fix warnings in third-party

--- Tools/LauncherCmake/Classes/main.cpp
+++ Tools/LauncherCmake/Classes/main.cpp
@@ -39,7 +39,23 @@ void LogMessageHandler(QtMsgType type, const QMessageLogContext &, const QString
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_WIN
+    char **argv1 = new char *[argc + 2];
+    memcpy(argv1, argv, argc * sizeof(char *));
+
+    argv1[argc] = "-platformpluginpath";
+    argv1[argc + 1] = ".";
+
+    argc += 2;
+    argv = argv1;
+#endif // Q_OS_WIN
+
     QApplication a(argc, argv);
+
+#ifdef Q_OS_WIN
+    delete[] argv1;
+#endif // Q_OS_WIN
+
     ErrorMessanger::Instance();
     qInstallMessageHandler(LogMessageHandler);
 
