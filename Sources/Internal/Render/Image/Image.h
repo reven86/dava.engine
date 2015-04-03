48324dda5f74efc78c9f42b696a5a5b6d961c7b6
klesch <victor.kleschenko@gmail.com>
2015-04-03 18:54:04 +0300

    [DF-6043] Added correct loading/unloading of TexDescriptor. Fixed compression to DXT

--- Sources/Internal/Render/Image/Image.h
+++ Sources/Internal/Render/Image/Image.h
@@ -82,7 +82,7 @@ public:
 
     Vector<Image *> CreateMipMapsImages(bool isNormalMap = false);
 
-    void Normalize();
+    bool Normalize();
 
     // changes size of image canvas to required size, if new size is bigger, sets 0 to all new pixels
     void ResizeCanvas(uint32 newWidth, uint32 newHeight);
