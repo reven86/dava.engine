37aa3e0b760f3779f17b5c980889243f3d94a0c3
Dima Belsky <dima.belsky@gmail.com>
2015-04-13 11:26:27 +0300

    [DF-6452] quicked: restored margins

--- Tools/QuickEd/Classes/Model/ControlProperties/ValueProperty.cpp
+++ Tools/QuickEd/Classes/Model/ControlProperties/ValueProperty.cpp
@@ -43,6 +43,13 @@ ValueProperty::ValueProperty(BaseObject *object, const InspMember *member, Value
         children.push_back(new SubValueProperty(2));
         children.push_back(new SubValueProperty(3));
     }
+    else if (defaultValue.GetType() == VariantType::TYPE_VECTOR4)
+    {
+        children.push_back(new SubValueProperty(0));
+        children.push_back(new SubValueProperty(1));
+        children.push_back(new SubValueProperty(2));
+        children.push_back(new SubValueProperty(3));
+    }
     else if (defaultValue.GetType() == VariantType::TYPE_INT32 && member->Desc().type == InspDesc::T_FLAGS)
     {
         const EnumMap *map = member->Desc().enumMap;
@@ -184,7 +191,24 @@ String ValueProperty::GetSubValueName(int index) const
             else
                 return "Alpha";
         }
-            
+
+        case VariantType::TYPE_VECTOR4:
+        {
+            if (index == 0)
+                return "left";
+            else if (index == 1)
+                return "top";
+            else if (index == 2)
+                return "right";
+            else if (index == 3)
+                return "bottom";
+            else
+            {
+                DVASSERT(false);
+                return "?";
+            }
+        }
+
         case VariantType::TYPE_INT32:
             if (member->Desc().type == InspDesc::T_FLAGS)
             {
@@ -223,7 +247,13 @@ VariantType ValueProperty::GetSubValue(int index) const
             DVASSERT(index >= 0 && index < 4);
             return VariantType(GetValue().AsColor().color[index]);
         }
-
+            
+        case VariantType::TYPE_VECTOR4:
+        {
+            DVASSERT(index >= 0 && index < 4);
+            return VariantType(GetValue().AsVector4().data[index]);
+        }
+            
         case VariantType::TYPE_INT32:
             if (member->Desc().type == InspDesc::T_FLAGS)
             {
@@ -246,48 +276,75 @@ VariantType ValueProperty::GetSubValue(int index) const
 
 void ValueProperty::SetSubValue(int index, const DAVA::VariantType &newValue)
 {
+    SetValue(ChangeValueComponent(GetValue(), newValue, index));
+}
+
+void ValueProperty::SetDefaultSubValue(int index, const DAVA::VariantType &newValue)
+{
+    SetDefaultValue(ChangeValueComponent(defaultValue, newValue, index));
+}
+
+void ValueProperty::ApplyValue(const DAVA::VariantType &value)
+{
+    member->SetValue(object, value);
+}
+
+VariantType ValueProperty::ChangeValueComponent(const VariantType &value, const VariantType &component, int32 index)
+{
     switch (defaultValue.GetType())
     {
         case VariantType::TYPE_VECTOR2:
         {
-            Vector2 val = GetValue().AsVector2();
+            Vector2 val = value.AsVector2();
             if (index == 0)
-                val.x = newValue.AsFloat();
+                val.x = component.AsFloat();
             else
-                val.y = newValue.AsFloat();
+                val.y = component.AsFloat();
             
-            SetValue(VariantType(val));
-            break;
+            return VariantType(val);
         }
             
         case VariantType::TYPE_COLOR:
         {
-            Color val = GetValue().AsColor();
-            if (index == 0)
-                val.r = newValue.AsFloat();
-            else if (index == 1)
-                val.g = newValue.AsFloat();
-            else if (index == 2)
-                val.b = newValue.AsFloat();
+            Color val = value.AsColor();
+            if (0 <= index && index < 4)
+            {
+                val.color[index ] = component.AsFloat();
+            }
             else
-                val.a = newValue.AsFloat();
+            {
+                DVASSERT(false);
+            }
             
-            SetValue(VariantType(val));
-            break;
+            return VariantType(val);
+        }
+            
+        case VariantType::TYPE_VECTOR4:
+        {
+            Vector4 val = value.AsVector4();
+            if (0 <= index && index < 4)
+            {
+                val.data[index] = component.AsFloat();
+            }
+            else
+            {
+                DVASSERT(false);
+            }
+            return VariantType(val);
         }
             
         case VariantType::TYPE_INT32:
             if (member->Desc().type == InspDesc::T_FLAGS)
             {
                 const EnumMap *map = member->Desc().enumMap;
-                int32 value = GetValue().AsInt32();
-
+                int32 intValue = value.AsInt32();
+                
                 int val = 0;
                 map->GetValue(index, val);
-                if (newValue.AsBool())
-                    SetValue(VariantType(value | val));
+                if (component.AsBool())
+                    return VariantType(intValue | val);
                 else
-                    SetValue(VariantType(value & (~val)));
+                    return VariantType(intValue & (~val));
             }
             else
             {
@@ -299,9 +356,5 @@ void ValueProperty::SetSubValue(int index, const DAVA::VariantType &newValue)
             DVASSERT(false);
             break;
     }
-}
-
-void ValueProperty::ApplyValue(const DAVA::VariantType &value)
-{
-    member->SetValue(object, value);
+    return VariantType();
 }
