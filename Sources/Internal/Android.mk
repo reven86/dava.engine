6828d201cc117f4119d4ea500061ca703b349c4b
leanid <leanid.chaika@gmail.com>
2015-05-08 17:43:35 +0300

    DF-6664 add for 5.0 android test if user click start change url in web view, minor fix change prefix in android.mk for custom variables

--- Sources/Internal/Android.mk
+++ Sources/Internal/Android.mk
@@ -98,90 +98,90 @@ include $(PREBUILT_STATIC_LIBRARY)
 DAVA_ROOT := $(LOCAL_PATH)
 
 # set path for includes
-MY_LOCAL_C_INCLUDES := $(LOCAL_PATH)
-MY_LOCAL_C_INCLUDES += $(LOCAL_PATH)/Platform/TemplateAndroid/
-MY_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/include
-MY_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/fmod/include
-MY_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/lua/include
+DV_LOCAL_C_INCLUDES := $(LOCAL_PATH)
+DV_LOCAL_C_INCLUDES += $(LOCAL_PATH)/Platform/TemplateAndroid/
+DV_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/include
+DV_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/fmod/include
+DV_LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Libs/lua/include
 
 # set exported includes
-MY_LOCAL_EXPORT_C_INCLUDES := $(MY_LOCAL_C_INCLUDES)
+DV_LOCAL_EXPORT_C_INCLUDES := $(DV_LOCAL_C_INCLUDES)
 
 ifeq ($(TARGET_ARCH_ABI), $(filter $(TARGET_ARCH_ABI), armeabi-v7a armeabi-v7a-hard))
-MY_USE_NEON := true
-MY_LOCAL_ARM_NEON := true
-MY_LOCAL_ARM_MODE := arm
-MY_LOCAL_NEON_CFLAGS := -mfloat-abi=softfp -mfpu=neon -march=armv7
-MY_LOCAL_CFLAGS += -DUSE_NEON
+DV_USE_NEON := true
+DV_LOCAL_ARM_NEON := true
+DV_LOCAL_ARM_MODE := arm
+DV_LOCAL_NEON_CFLAGS := -mfloat-abi=softfp -mfpu=neon -march=armv7
+DV_LOCAL_CFLAGS += -DUSE_NEON
 endif
 
 # set build flags
-MY_LOCAL_CPPFLAGS += -frtti -DGL_GLEXT_PROTOTYPES=1
-MY_LOCAL_CPPFLAGS += -std=c++1y
+DV_LOCAL_CPPFLAGS += -frtti -DGL_GLEXT_PROTOTYPES=1
+DV_LOCAL_CPPFLAGS += -std=c++1y
 
-MY_LOCAL_CFLAGS += -DDAVA_FMOD
+DV_LOCAL_CFLAGS += -DDAVA_FMOD
 
 # remove warnings about unused arguments to compiler
-MY_LOCAL_CFLAGS += -Qunused-arguments
+DV_LOCAL_CFLAGS += -Qunused-arguments
 # enable ALL warnings
 # we write C++ so check warnings only inside our C++ code not C libraries
-MY_LOCAL_CPPFLAGS += -Weverything
+DV_LOCAL_CPPFLAGS += -Weverything
 # treat warnings as errors
-MY_LOCAL_CPPFLAGS += -Werror
+DV_LOCAL_CPPFLAGS += -Werror
 # disable common simple warnings
 # read about any clang warning messages http://fuckingclangwarnings.com/
-MY_LOCAL_CPPFLAGS += -Wno-c++98-compat-pedantic
-MY_LOCAL_CPPFLAGS += -Wno-newline-eof
-MY_LOCAL_CPPFLAGS += -Wno-gnu-anonymous-struct
-MY_LOCAL_CPPFLAGS += -Wno-nested-anon-types
-MY_LOCAL_CPPFLAGS += -Wno-float-equal
-MY_LOCAL_CPPFLAGS += -Wno-extra-semi
-MY_LOCAL_CPPFLAGS += -Wno-unused-parameter
-MY_LOCAL_CPPFLAGS += -Wno-shadow
-MY_LOCAL_CPPFLAGS += -Wno-exit-time-destructors
-MY_LOCAL_CPPFLAGS += -Wno-documentation
-MY_LOCAL_CPPFLAGS += -Wno-global-constructors
-MY_LOCAL_CPPFLAGS += -Wno-padded
-MY_LOCAL_CPPFLAGS += -Wno-weak-vtables
-MY_LOCAL_CPPFLAGS += -Wno-variadic-macros
-MY_LOCAL_CPPFLAGS += -Wno-deprecated-register
-MY_LOCAL_CPPFLAGS += -Wno-sign-conversion
-MY_LOCAL_CPPFLAGS += -Wno-sign-compare
-MY_LOCAL_CPPFLAGS += -Wno-format-nonliteral
+DV_LOCAL_CPPFLAGS += -Wno-c++98-compat-pedantic
+DV_LOCAL_CPPFLAGS += -Wno-newline-eof
+DV_LOCAL_CPPFLAGS += -Wno-gnu-anonymous-struct
+DV_LOCAL_CPPFLAGS += -Wno-nested-anon-types
+DV_LOCAL_CPPFLAGS += -Wno-float-equal
+DV_LOCAL_CPPFLAGS += -Wno-extra-semi
+DV_LOCAL_CPPFLAGS += -Wno-unused-parameter
+DV_LOCAL_CPPFLAGS += -Wno-shadow
+DV_LOCAL_CPPFLAGS += -Wno-exit-time-destructors
+DV_LOCAL_CPPFLAGS += -Wno-documentation
+DV_LOCAL_CPPFLAGS += -Wno-global-constructors
+DV_LOCAL_CPPFLAGS += -Wno-padded
+DV_LOCAL_CPPFLAGS += -Wno-weak-vtables
+DV_LOCAL_CPPFLAGS += -Wno-variadic-macros
+DV_LOCAL_CPPFLAGS += -Wno-deprecated-register
+DV_LOCAL_CPPFLAGS += -Wno-sign-conversion
+DV_LOCAL_CPPFLAGS += -Wno-sign-compare
+DV_LOCAL_CPPFLAGS += -Wno-format-nonliteral
 
 # TODO fix next warnings first
-MY_LOCAL_CPPFLAGS += -Wno-cast-align
-MY_LOCAL_CPPFLAGS += -Wno-conversion
-MY_LOCAL_CPPFLAGS += -Wno-unreachable-code
-MY_LOCAL_CPPFLAGS += -Wno-zero-length-array
-MY_LOCAL_CPPFLAGS += -Wno-switch-enum
-MY_LOCAL_CPPFLAGS += -Wno-c99-extensions
-MY_LOCAL_CPPFLAGS += -Wno-missing-prototypes
-MY_LOCAL_CPPFLAGS += -Wno-missing-field-initializers
-MY_LOCAL_CPPFLAGS += -Wno-conditional-uninitialized
-MY_LOCAL_CPPFLAGS += -Wno-covered-switch-default
-MY_LOCAL_CPPFLAGS += -Wno-deprecated
-MY_LOCAL_CPPFLAGS += -Wno-unused-macros
-MY_LOCAL_CPPFLAGS += -Wno-disabled-macro-expansion
-MY_LOCAL_CPPFLAGS += -Wno-undef
-MY_LOCAL_CPPFLAGS += -Wno-non-virtual-dtor
-MY_LOCAL_CPPFLAGS += -Wno-char-subscripts
-MY_LOCAL_CPPFLAGS += -Wno-unneeded-internal-declaration
-MY_LOCAL_CPPFLAGS += -Wno-unused-variable
-MY_LOCAL_CPPFLAGS += -Wno-used-but-marked-unused
-MY_LOCAL_CPPFLAGS += -Wno-missing-variable-declarations
-MY_LOCAL_CPPFLAGS += -Wno-gnu-statement-expression
-MY_LOCAL_CPPFLAGS += -Wno-missing-braces
-MY_LOCAL_CPPFLAGS += -Wno-reorder
-MY_LOCAL_CPPFLAGS += -Wno-implicit-fallthrough
-MY_LOCAL_CPPFLAGS += -Wno-ignored-qualifiers
-MY_LOCAL_CPPFLAGS += -Wno-shift-sign-overflow
-MY_LOCAL_CPPFLAGS += -Wno-mismatched-tags
-MY_LOCAL_CPPFLAGS += -Wno-missing-noreturn
-MY_LOCAL_CPPFLAGS += -Wno-consumed
-MY_LOCAL_CPPFLAGS += -Wno-sometimes-uninitialized
+DV_LOCAL_CPPFLAGS += -Wno-cast-align
+DV_LOCAL_CPPFLAGS += -Wno-conversion
+DV_LOCAL_CPPFLAGS += -Wno-unreachable-code
+DV_LOCAL_CPPFLAGS += -Wno-zero-length-array
+DV_LOCAL_CPPFLAGS += -Wno-switch-enum
+DV_LOCAL_CPPFLAGS += -Wno-c99-extensions
+DV_LOCAL_CPPFLAGS += -Wno-missing-prototypes
+DV_LOCAL_CPPFLAGS += -Wno-missing-field-initializers
+DV_LOCAL_CPPFLAGS += -Wno-conditional-uninitialized
+DV_LOCAL_CPPFLAGS += -Wno-covered-switch-default
+DV_LOCAL_CPPFLAGS += -Wno-deprecated
+DV_LOCAL_CPPFLAGS += -Wno-unused-macros
+DV_LOCAL_CPPFLAGS += -Wno-disabled-macro-expansion
+DV_LOCAL_CPPFLAGS += -Wno-undef
+DV_LOCAL_CPPFLAGS += -Wno-non-virtual-dtor
+DV_LOCAL_CPPFLAGS += -Wno-char-subscripts
+DV_LOCAL_CPPFLAGS += -Wno-unneeded-internal-declaration
+DV_LOCAL_CPPFLAGS += -Wno-unused-variable
+DV_LOCAL_CPPFLAGS += -Wno-used-but-marked-unused
+DV_LOCAL_CPPFLAGS += -Wno-missing-variable-declarations
+DV_LOCAL_CPPFLAGS += -Wno-gnu-statement-expression
+DV_LOCAL_CPPFLAGS += -Wno-missing-braces
+DV_LOCAL_CPPFLAGS += -Wno-reorder
+DV_LOCAL_CPPFLAGS += -Wno-implicit-fallthrough
+DV_LOCAL_CPPFLAGS += -Wno-ignored-qualifiers
+DV_LOCAL_CPPFLAGS += -Wno-shift-sign-overflow
+DV_LOCAL_CPPFLAGS += -Wno-mismatched-tags
+DV_LOCAL_CPPFLAGS += -Wno-missing-noreturn
+DV_LOCAL_CPPFLAGS += -Wno-consumed
+DV_LOCAL_CPPFLAGS += -Wno-sometimes-uninitialized
 
-MY_LOCAL_CPP_FEATURES += exceptions
+DV_LOCAL_CPP_FEATURES += exceptions
 
 ifeq ($(DAVA_PROFILE), true)
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
@@ -189,61 +189,61 @@ $(info ==============)
 $(info profiling enabled!)
 $(info ==============)
 
-MY_LOCAL_CFLAGS += -pg
-MY_LOCAL_CFLAGS += -D__DAVAENGINE_PROFILE__
+DV_LOCAL_CFLAGS += -pg
+DV_LOCAL_CFLAGS += -D__DAVAENGINE_PROFILE__
 endif
 endif
 
 # set exported build flags
-MY_LOCAL_EXPORT_CFLAGS := $(LOCAL_CFLAGS)
+DV_LOCAL_EXPORT_CFLAGS := $(LOCAL_CFLAGS)
 
 # set exported used libs
-MY_LOCAL_EXPORT_LDLIBS := $(LOCAL_LDLIBS)
+DV_LOCAL_EXPORT_LDLIBS := $(LOCAL_LDLIBS)
 
 # set included libraries
-MY_LOCAL_STATIC_LIBRARIES := libbox2d
+DV_LOCAL_STATIC_LIBRARIES := libbox2d
 
 ifeq ($(DAVA_PROFILE), true)
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-MY_LOCAL_STATIC_LIBRARIES += android-ndk-profiler
+DV_LOCAL_STATIC_LIBRARIES += android-ndk-profiler
 endif
 endif
 
-MY_LOCAL_SHARED_LIBRARIES += iconv_android-prebuilt
-MY_LOCAL_SHARED_LIBRARIES += fmodex-prebuild
-MY_LOCAL_SHARED_LIBRARIES += fmodevent-prebuild
+DV_LOCAL_SHARED_LIBRARIES += iconv_android-prebuilt
+DV_LOCAL_SHARED_LIBRARIES += fmodex-prebuild
+DV_LOCAL_SHARED_LIBRARIES += fmodevent-prebuild
 
-MY_LOCAL_STATIC_LIBRARIES += xml_android
-MY_LOCAL_STATIC_LIBRARIES += png_android
-MY_LOCAL_STATIC_LIBRARIES += freetype_android
-MY_LOCAL_STATIC_LIBRARIES += yaml_android
-MY_LOCAL_STATIC_LIBRARIES += mongodb_android
-MY_LOCAL_STATIC_LIBRARIES += lua_android
-MY_LOCAL_STATIC_LIBRARIES += dxt_android
-MY_LOCAL_STATIC_LIBRARIES += jpeg_android
-MY_LOCAL_STATIC_LIBRARIES += curl_android
-MY_LOCAL_STATIC_LIBRARIES += ssl_android
-MY_LOCAL_STATIC_LIBRARIES += crypto_android
-MY_LOCAL_STATIC_LIBRARIES += zip_android
-MY_LOCAL_STATIC_LIBRARIES += fribidi_android
-MY_LOCAL_STATIC_LIBRARIES += unibreak_android
-MY_LOCAL_STATIC_LIBRARIES += uv_android
+DV_LOCAL_STATIC_LIBRARIES += xml_android
+DV_LOCAL_STATIC_LIBRARIES += png_android
+DV_LOCAL_STATIC_LIBRARIES += freetype_android
+DV_LOCAL_STATIC_LIBRARIES += yaml_android
+DV_LOCAL_STATIC_LIBRARIES += mongodb_android
+DV_LOCAL_STATIC_LIBRARIES += lua_android
+DV_LOCAL_STATIC_LIBRARIES += dxt_android
+DV_LOCAL_STATIC_LIBRARIES += jpeg_android
+DV_LOCAL_STATIC_LIBRARIES += curl_android
+DV_LOCAL_STATIC_LIBRARIES += ssl_android
+DV_LOCAL_STATIC_LIBRARIES += crypto_android
+DV_LOCAL_STATIC_LIBRARIES += zip_android
+DV_LOCAL_STATIC_LIBRARIES += fribidi_android
+DV_LOCAL_STATIC_LIBRARIES += unibreak_android
+DV_LOCAL_STATIC_LIBRARIES += uv_android
 
-MY_LOCAL_EXPORT_LDLIBS := -lGLESv1_CM -llog -lEGL
+DV_LOCAL_EXPORT_LDLIBS := -lGLESv1_CM -llog -lEGL
 
 ifeq ($(APP_PLATFORM), android-14)
-	MY_LOCAL_EXPORT_LDLIBS += -lGLESv2
+	DV_LOCAL_EXPORT_LDLIBS += -lGLESv2
 else 
 ifeq ($(APP_PLATFORM), android-15)
-	MY_LOCAL_EXPORT_LDLIBS += -lGLESv2
+	DV_LOCAL_EXPORT_LDLIBS += -lGLESv2
 else 
 ifeq ($(APP_PLATFORM), android-16)
-	MY_LOCAL_EXPORT_LDLIBS += -lGLESv2
+	DV_LOCAL_EXPORT_LDLIBS += -lGLESv2
 else 
 ifeq ($(APP_PLATFORM), android-17)
-	MY_LOCAL_EXPORT_LDLIBS += -lGLESv2
+	DV_LOCAL_EXPORT_LDLIBS += -lGLESv2
 else
-	MY_LOCAL_EXPORT_LDLIBS += -lGLESv3
+	DV_LOCAL_EXPORT_LDLIBS += -lGLESv3
 endif
 endif
 endif
@@ -255,21 +255,21 @@ include $(CLEAR_VARS)
 # set module name
 LOCAL_MODULE := libInternalPart1
 
-LOCAL_C_INCLUDES := $(MY_LOCAL_C_INCLUDES)
-LOCAL_EXPORT_C_INCLUDES := $(MY_LOCAL_EXPORT_C_INCLUDES)
-LOCAL_ARM_NEON := $(MY_LOCAL_ARM_NEON)
-LOCAL_ARM_MODE := $(MY_LOCAL_ARM_MODE)
-LOCAL_NEON_CFLAGS := $(MY_LOCAL_NEON_CFLAGS)
-LOCAL_CPPFLAGS := $(MY_LOCAL_CPPFLAGS)
-LOCAL_CFLAGS := $(MY_LOCAL_CFLAGS)
-LOCAL_CPP_FEATURES := $(MY_LOCAL_CPP_FEATURES)
-LOCAL_EXPORT_CFLAGS := $(MY_LOCAL_EXPORT_CFLAGS)
-LOCAL_EXPORT_LDLIBS := $(MY_LOCAL_EXPORT_LDLIBS)
-LOCAL_STATIC_LIBRARIES := $(MY_LOCAL_STATIC_LIBRARIES)
-LOCAL_SHARED_LIBRARIES := $(MY_LOCAL_SHARED_LIBRARIES)
-LOCAL_STATIC_LIBRARIES := $(MY_LOCAL_STATIC_LIBRARIES)
+LOCAL_C_INCLUDES := $(DV_LOCAL_C_INCLUDES)
+LOCAL_EXPORT_C_INCLUDES := $(DV_LOCAL_EXPORT_C_INCLUDES)
+LOCAL_ARM_NEON := $(DV_LOCAL_ARM_NEON)
+LOCAL_ARM_MODE := $(DV_LOCAL_ARM_MODE)
+LOCAL_NEON_CFLAGS := $(DV_LOCAL_NEON_CFLAGS)
+LOCAL_CPPFLAGS := $(DV_LOCAL_CPPFLAGS)
+LOCAL_CFLAGS := $(DV_LOCAL_CFLAGS)
+LOCAL_CPP_FEATURES := $(DV_LOCAL_CPP_FEATURES)
+LOCAL_EXPORT_CFLAGS := $(DV_LOCAL_EXPORT_CFLAGS)
+LOCAL_EXPORT_LDLIBS := $(DV_LOCAL_EXPORT_LDLIBS)
+LOCAL_STATIC_LIBRARIES := $(DV_LOCAL_STATIC_LIBRARIES)
+LOCAL_SHARED_LIBRARIES := $(DV_LOCAL_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES := $(DV_LOCAL_STATIC_LIBRARIES)
 
-USE_NEON := $(MY_USE_NEON)
+USE_NEON := $(DV_USE_NEON)
 
 # set source files 
 LOCAL_SRC_FILES := \
@@ -305,23 +305,23 @@ include $(CLEAR_VARS)
 # set module name
 LOCAL_MODULE := libInternal
 
-LOCAL_C_INCLUDES := $(MY_LOCAL_C_INCLUDES)
-LOCAL_EXPORT_C_INCLUDES := $(MY_LOCAL_EXPORT_C_INCLUDES)
-LOCAL_ARM_NEON := $(MY_LOCAL_ARM_NEON)
-LOCAL_ARM_MODE := $(MY_LOCAL_ARM_MODE)
-LOCAL_NEON_CFLAGS := $(MY_LOCAL_NEON_CFLAGS)
-LOCAL_CPPFLAGS := $(MY_LOCAL_CPPFLAGS)
-LOCAL_CFLAGS := $(MY_LOCAL_CFLAGS)
-LOCAL_CPP_FEATURES := $(MY_LOCAL_CPP_FEATURES)
-LOCAL_EXPORT_CFLAGS := $(MY_LOCAL_EXPORT_CFLAGS)
-LOCAL_EXPORT_LDLIBS := $(MY_LOCAL_EXPORT_LDLIBS)
-LOCAL_STATIC_LIBRARIES := $(MY_LOCAL_STATIC_LIBRARIES)
-LOCAL_SHARED_LIBRARIES := $(MY_LOCAL_SHARED_LIBRARIES)
-LOCAL_STATIC_LIBRARIES := $(MY_LOCAL_STATIC_LIBRARIES)
+LOCAL_C_INCLUDES := $(DV_LOCAL_C_INCLUDES)
+LOCAL_EXPORT_C_INCLUDES := $(DV_LOCAL_EXPORT_C_INCLUDES)
+LOCAL_ARM_NEON := $(DV_LOCAL_ARM_NEON)
+LOCAL_ARM_MODE := $(DV_LOCAL_ARM_MODE)
+LOCAL_NEON_CFLAGS := $(DV_LOCAL_NEON_CFLAGS)
+LOCAL_CPPFLAGS := $(DV_LOCAL_CPPFLAGS)
+LOCAL_CFLAGS := $(DV_LOCAL_CFLAGS)
+LOCAL_CPP_FEATURES := $(DV_LOCAL_CPP_FEATURES)
+LOCAL_EXPORT_CFLAGS := $(DV_LOCAL_EXPORT_CFLAGS)
+LOCAL_EXPORT_LDLIBS := $(DV_LOCAL_EXPORT_LDLIBS)
+LOCAL_STATIC_LIBRARIES := $(DV_LOCAL_STATIC_LIBRARIES)
+LOCAL_SHARED_LIBRARIES := $(DV_LOCAL_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES := $(DV_LOCAL_STATIC_LIBRARIES)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := libInternalPart1
 
-USE_NEON := $(MY_USE_NEON)
+USE_NEON := $(DV_USE_NEON)
 
 LOCAL_SRC_FILES := \
                      $(subst $(LOCAL_PATH)/,, \
