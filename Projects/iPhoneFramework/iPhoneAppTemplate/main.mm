77f369092e1ec381d131acd1a0e7059dc51f0948
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-05-26 12:07:40 +0300

    [DF-6758] some creepy files edited manually

--- Projects/iPhoneFramework/iPhoneAppTemplate/main.mm
+++ Projects/iPhoneFramework/iPhoneAppTemplate/main.mm
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  MahJongQuest
-//
-//  Created by Vitaliy  Borodovsky on 10/6/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #include "DAVAEngine.h"
 
