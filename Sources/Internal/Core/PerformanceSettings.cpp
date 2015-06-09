fe4fc2096fcd0499bd209fa5f14846e257afb8d7
Dmitriy Artsukevich <dartsukevichwork@gmail.com>
2015-06-09 14:12:13 +0300

    [DF-6750] - Performance settings new params

--- Sources/Internal/Core/PerformanceSettings.cpp
+++ Sources/Internal/Core/PerformanceSettings.cpp
@@ -34,10 +34,10 @@ namespace DAVA
 PerformanceSettings::PerformanceSettings()
 {
 	//init with some default values
-	psPerformanceMinFPS = 10;
-	psPerformanceMaxFPS = 20;
-	psPerformanceLodOffset = 10;
-	psPerformanceLodMult = 1.2f;
+	psPerformanceMinFPS = 35;
+	psPerformanceMaxFPS = 50;
+	psPerformanceLodOffset = 80;
+	psPerformanceLodMult = 10;
 	psPerformanceSpeedMult = 2;
 }
 float32 PerformanceSettings::GetPsPerformanceMinFPS()
