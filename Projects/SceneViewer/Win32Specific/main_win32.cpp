fba1b23f168e7dc549c2118d7bb8ef2141de4b96
Andrey Derkach <a_derkach2@wargaming.net>
2015-06-12 11:46:36 +0300

    [DF-6946] SceneViewer building for Windows 10
    Former-commit-id: 9ce66bc100fa904a145fda71546e1dc2794c8833

--- Projects/SceneViewer/Win32Specific/main_win32.cpp
+++ Projects/SceneViewer/Win32Specific/main_win32.cpp
@@ -26,27 +26,20 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
+#include "Base/Platform.h"
+#if defined(__DAVAENGINE_WIN32__)
 
 #include "DAVAEngine.h"
 
 int APIENTRY WinMain(HINSTANCE hInstance,
-					   HINSTANCE hPrevInstance,
-					   LPSTR    lpCmdLine,
-					   int       nCmdShow)
+                    HINSTANCE hPrevInstance,
+                    LPSTR    lpCmdLine,
+                    int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	return DAVA::Core::Run(0, 0, hInstance);
+    return DAVA::Core::Run(0, 0, hInstance);
 }
 
-
-/*int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR    lpCmdLine, int       nCmdShow)
-{
-	// find leaks mega-string: {,,msvcr71d.dll}_crtBreakAlloc
-	//
-	// POSSIBLE LEAKS LIST:
-	// remember -- always clear all allocated data for static STL containers
-	// 
-	return 0;
-}*/
+#endif // defined(__DAVAENGINE_WIN32__)
