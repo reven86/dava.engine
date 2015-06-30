c81f0401ca462a9102ae902acc497ef8fda3c4bc
Alexander Volchkevich <a_valchkevich@wargaming.net>
2015-06-30 16:13:22 +0300

    DF-6912 we need more files
    Former-commit-id: fdc6e05bdb2c7ea2dcf310132027a1f60b0127b3

--- Sources/Internal/UI/Styles/UIStyleSheetPropertyTable.h
+++ Sources/Internal/UI/Styles/UIStyleSheetPropertyTable.h
@@ -27,44 +27,26 @@
 =====================================================================================*/
 
 
-#ifndef __DAVAENGINE_UI_STYLESHEET_H__
-#define __DAVAENGINE_UI_STYLESHEET_H__
+#ifndef __DAVAENGINE_UI_STYLESHEET_PROPERTY_TABLE_H__
+#define __DAVAENGINE_UI_STYLESHEET_PROPERTY_TABLE_H__
 
 #include "Base/BaseObject.h"
 #include "Base/BaseTypes.h"
-#include "Base/FastName.h"
-#include "FileSystem/VariantType.h"
-#include "UI/Styles/UIStyleSheetPropertyDataBase.h"
 #include "UI/Styles/UIStyleSheetStructs.h"
-#include "UI/Styles/UIStyleSheetSelectorChain.h"
-#include "UI/Styles/UIStyleSheetPropertyTable.h"
 
 namespace DAVA
 {
 
-class UIStyleSheet :
+class UIStyleSheetPropertyTable :
     public BaseObject
 {
 protected:
-    virtual ~UIStyleSheet();
+    virtual ~UIStyleSheetPropertyTable() {};
 public:
-    UIStyleSheet();
-
-    int32 GetScore() const;
-
-    const UIStyleSheetPropertyTable* GetPropertyTable() const;
-    const UIStyleSheetSelectorChain& GetSelectorChain() const;
-
-    void SetPropertyTable(UIStyleSheetPropertyTable* properties);
-    void SetSelectorChain(const UIStyleSheetSelectorChain& selectorChain);
+    void SetProperties(const Vector<UIStyleSheetProperty>& properties);
+    const Vector<UIStyleSheetProperty>& GetProperties() const;
 private:
-    void RecalculateScore();
-
-    UIStyleSheetSelectorChain selectorChain;
-        
-    RefPtr<UIStyleSheetPropertyTable> properties;
-
-    int32 score;
+    Vector<UIStyleSheetProperty> properties;
 };
 
 };
