5a6ec488cae1ab20915d56dd70cc96093bec7a80
Maksim Lazarev <m_lazarev@wargaming.net>
2015-07-01 14:53:24 +0300

    DF-7124 render loop moved to dedicated thread (some problems with suspend/resume remain)
    Former-commit-id: 2bdec54a756e9de4b4542299a0fdd22c6f20508c

--- Projects/TestBed/Win32Specific/main.cpp
+++ Projects/TestBed/Win32Specific/main.cpp
@@ -47,8 +47,7 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 [Platform::MTAThread]
 int main(Platform::Array<Platform::String^>^ args)
 {
-    //return DAVA::Core::Run(0, 0, 0);
-    return DAVA::Core::Run2(0, 0, 0);
+    return DAVA::Core::Run(0, 0, 0);
 }
 
 #endif // defined(__DAVAENGINE_WIN32__)
