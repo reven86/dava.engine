261479adc30e4ad6c81679fff4e298556e414d74
Maksim Lazarev <m_lazarev@wargaming.net>
2015-07-15 14:46:36 +0300

    DF-7041 some fixes after merge
    Former-commit-id: f212659b2f68412bde78bd80acc963f54194d94d

--- Projects/TestBed/Classes/Tests/MultilineTest.cpp
+++ Projects/TestBed/Classes/Tests/MultilineTest.cpp
@@ -32,7 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 using namespace DAVA;
 
 MultilineTest::MultilineTest ()
-: BaseScreen("MultilineTest")
+    : BaseScreen("MultilineTest")
 {
 }
 
