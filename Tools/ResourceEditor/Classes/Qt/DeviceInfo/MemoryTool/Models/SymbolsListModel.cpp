3833b9621d027cdd2b166b49657248fbad7e7145
Maksim Lazarev <m_lazarev@wargaming.net>
2015-08-18 14:42:17 +0300

    DF-7369 introduce SymbolsWidget and move SymbolsFilterModel class into SymbolsListModel's sources
    Former-commit-id: cd82da7e420bbfc8efae956d83e0c111591b26e1

--- Tools/ResourceEditor/Classes/Qt/DeviceInfo/MemoryTool/Models/SymbolsListModel.cpp
+++ Tools/ResourceEditor/Classes/Qt/DeviceInfo/MemoryTool/Models/SymbolsListModel.cpp
@@ -12,12 +12,6 @@ SymbolsListModel::SymbolsListModel(const BacktraceSymbolTable& symbolTable_, QOb
 
 SymbolsListModel::~SymbolsListModel() = default;
 
-const String* SymbolsListModel::Symbol(int row) const
-{
-    Q_ASSERT(row < static_cast<int>(allSymbols.size()));
-    return allSymbols[row];
-}
-
 int SymbolsListModel::rowCount(const QModelIndex& parent) const
 {
     return static_cast<int>(allSymbols.size());
@@ -25,10 +19,18 @@ int SymbolsListModel::rowCount(const QModelIndex& parent) const
 
 QVariant SymbolsListModel::data(const QModelIndex& index, int role) const
 {
-    if (index.isValid() && Qt::DisplayRole == role)
+    if (index.isValid())
     {
         const String* name = allSymbols[index.row()];
-        return QString(name->c_str());
+        if (Qt::DisplayRole == role)
+        {
+            return QString(name->c_str());
+        }
+        else if (ROLE_SYMBOL_POINTER == role)
+        {
+            // Remove constness as QVariant does not accept const void*
+            return QVariant::fromValue(const_cast<void*>(static_cast<const void*>(name)));
+        }
     }
     return QVariant();
 }
@@ -39,4 +41,58 @@ void SymbolsListModel::PrepareSymbols()
     symbolTable.IterateOverSymbols([this](const String& name) {
         allSymbols.push_back(&name);
     });
+}
+
+//////////////////////////////////////////////////////////////////////////
+SymbolsFilterModel::SymbolsFilterModel(SymbolsListModel* model_, QObject* parent)
+    : QSortFilterProxyModel(parent)
+{
+    QSortFilterProxyModel::setSourceModel(model_);
+}
+
+void SymbolsFilterModel::SetFilterString(const QString& filterString)
+{
+    QString lowerCaseFilter = filterString.toLower();
+    if (lowerCaseFilter != filter)
+    {
+        filter = lowerCaseFilter;
+        invalidateFilter();
+    }
+}
+
+void SymbolsFilterModel::ToggleHideStdAndUnresolved()
+{
+    hideStdAndUnresolved = !hideStdAndUnresolved;
+    invalidateFilter();
+}
+
+bool SymbolsFilterModel::lessThan(const QModelIndex& left, const QModelIndex& right) const
+{
+    QAbstractItemModel* source = sourceModel();
+    QString nameLeft = source->data(left, Qt::DisplayRole).toString();
+    QString nameRight = source->data(right, Qt::DisplayRole).toString();
+    return nameLeft < nameRight;
+}
+
+bool SymbolsFilterModel::filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const
+{
+    return true;
+}
+
+bool SymbolsFilterModel::filterAcceptsRow(int source_row, const QModelIndex& source_parent) const
+{
+    QAbstractItemModel* source = sourceModel();
+    QModelIndex index = source->index(source_row, 0, source_parent);
+
+    QVariant v = source->data(index, SymbolsListModel::ROLE_SYMBOL_POINTER);
+    const String* namePointer = static_cast<const String*>(v.value<void*>());
+    Q_ASSERT(namePointer != nullptr);
+    QString name = QString(namePointer->c_str()).toLower();
+
+    bool decline = hideStdAndUnresolved && (name.startsWith("std::") || name.startsWith("#"));
+    if (!decline)
+    {
+        decline = !name.contains(filter);
+    }
+    return !decline;
 }
