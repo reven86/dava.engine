5bfd9a43590d5deafef9098127f5562114aae11a
leanid chaika <l_chaika@wargaming.net>
2015-08-19 19:12:40 +0300

    DF-7374 continue develop Packer
    Former-commit-id: b142b1c46ab89dbf80caf95694fc2a204df8553c

--- Tools/DavaPak/Classes/main.cpp
+++ Tools/DavaPak/Classes/main.cpp
@@ -33,74 +33,44 @@
 #include <cctype>
 #include <cstdio>
 
-#include "ResourceArchive.h"
-#include "AnsiFileSystem.h"
+#include <FileSystem/FileSystem.h>
+#include <FileSystem/ResourceArchive.h>
+#include <FileSystem/FileList.h>
+#include <CommandLine/ProgramOptions.h>
 
-#ifdef WITH_LIBZ
-    #include "zlibPack.c"
-#endif
+using namespace DAVA;
 
-using namespace Log;
-using namespace IO;
-
-class HeaderNameMake
-{
-public:
-	HeaderNameMake()
-		: dot(false)
-	{
-	}
-
-	char operator () (char c)
-	{
-		if (dot)return ' ';
-		if (c == '/')return '_';
-		if (c == '.')
-		{
-			dot = true;
-			return ' ';
-		}
-		return c;
-	}
-private:
-	bool dot;
-};
 
 class Packer
 {
 public:
-	Packer(const String & _resourceName, const String & _packedDir, const String & _headerName)
-		: resourceName(_resourceName), packedDir(_packedDir), headerName(_headerName), resourceCount(0)
+	Packer(const String & _resourceName, const String & _packedDir)
+        : fileSystem{ new FileSystem() }
+        , resourceArchive(new ResourceArchive(), [](ResourceArchive* ar){SafeRelease(ar); })
+        , resourceName(_resourceName)
+        , packedDir(_packedDir) 
 	{
-		fileSystem = Singleton<IO::IFileSystem>::Instance();
-		
 	}
 
 	~Packer()
 	{
-
+        fileSystem->Release();
 	}
 	
 
-	void Pack(bool withPaths)
+	bool Pack()
 	{
-		resourceArchive.StartResource(resourceName, withPaths, packedDir);
-		PackAllFilesInDirectory(packedDir, ExtractName(packedDir));
-		resourceArchive.FinishResource();
+        bool withPaths = true;
+		resourceArchive->StartResource(resourceName, withPaths, packedDir);
+        String dirName = ExtractName(packedDir);
+		CollectAllFilesInDirectory(packedDir, "");
+		resourceArchive->FinishResource();
 	
 
 		std::cout << "Saving archive... " << std::endl;
 		std::cout << "file count:" << resourceCount << std::endl;
 			
 		// saving process
-		
-		//std::ofstream headerFile;
-
-		//headerFile.open(headerName.c_str());
-		//
-		//headerFile << "#ifdef __RESOURCE_H__" << std::endl;
-		//headerFile << "#define __RESOURCE_H__" << std::endl << std::endl;
-		
 		List<String>::iterator name = fileNameList.begin();
 
 		int32 resourceId = 0;
@@ -114,11 +84,11 @@ public:
 			int32 resourcePackedSize; 
 			int32 resourceRealSize;
 						
-			if (-1 == resourceArchive.SaveProgress(&resourcePackedSize, &resourceRealSize))
+			if (-1 == resourceArchive->SaveProgress(&resourcePackedSize, &resourceRealSize))
 			{
-				printf("*** Resource Archive Error\n");
+				std::cout << "*** Resource Archive Error\n";
 				std::cout << "file:" << * name;
-				return;
+				return false;
 			}
 
 			resourceRealSum += resourceRealSize;
@@ -127,27 +97,17 @@ public:
 			// process information
 			std::cout << "file:" << * name;
 			std::cout << " size:" << resourceRealSize << " packed: " << resourcePackedSize << std::endl;
-	
-
-			// header file 
-			String id = String("IDR") + *name;
-			std::transform(id.begin(), id.end(), id.begin(), toupper);
-			std::transform(id.begin(), id.end(), id.begin(), HeaderNameMake());
-			//headerFile << "#define " << id << "\t\t" << resourceId ++ << std::endl;
 
 			name++;
 		}
-		//headerFile << std::endl << std::endl;
 
-		//headerFile << "#endif // __RESOURCE_H__" << std::endl;
-		//headerFile.close();
-		// pack summary
-			
-		std::cout << std::endl << "Summary: " << std::endl;
-		std::cout << "size: " << resourceRealSum << std::endl;
-		std::cout << "packed size: " << resourcePackedSum << std::endl;
-		std::cout << "compression rate: " << ((float32)resourcePackedSum / (float32) resourceRealSum) << std::endl;
+		// pack summary	
+		std::cout << "Summary:\n"
+		          << "size: " << resourceRealSum << '\n'
+		          << "packed size: " << resourcePackedSum << '\n'
+		          << "compression rate: " << (static_cast<float32>(resourcePackedSum) / resourceRealSum) << std::endl;
 		
+        return true;
 	}	
 
 	String ExtractName(const String & name)
@@ -163,60 +123,46 @@ public:
 		}
 	}
 	
-	void PackAllFilesInDirectory(const String & path, const String & redusedPath)
+	void CollectAllFilesInDirectory(const String & pathDirName, const String & redusedPath)
 	{
-		fileSystem->SetCurrentDirectory(path.c_str());
-		IFileList * fileList = fileSystem->EnumFiles();
+        FilePath pathToDir = pathDirName;
+        fileSystem->SetCurrentWorkingDirectory(pathToDir);
+        bool includeHidden = false;
+        FileList * fileList = new FileList(pathToDir, includeHidden);
 		for (int file = 0; file < fileList->GetCount(); ++file)
 		{
-			String filename = fileList->GetFilename(file);
 			if (fileList->IsDirectory(file))
 			{
-				if ((filename != "..") && (filename != "."))
+                String directoryName = fileList->GetFilename(file);
+                if ((directoryName != "..") && (directoryName != "."))
 				{
-					//
-					std::cout << "Directory: " << filename << std::endl;
-					
-					PackAllFilesInDirectory(path + String("/") + filename, redusedPath + String("/") + filename);
+                    std::cout << "Directory: " << directoryName << '\n';
+                    CollectAllFilesInDirectory(directoryName + '/', redusedPath + directoryName + '/');
 				}
 			}else
 			{
-				String pathname = path + String("/") + filename;
-				String printPathName = redusedPath + String("/") + filename;
+                String filename = fileList->GetFilename(file);
+				String pathname = redusedPath + filename;
 				
-				std::cout << "file: " << printPathName << std::endl;
-				fileNameList.push_back(printPathName);
+				std::cout << "file: " << pathname << '\n';
+				fileNameList.push_back(pathname);
 
-				resourceArchive.AddFile(pathname);
+				resourceArchive->AddFile(pathname);
 				resourceCount++;
 			}
 		}
 		fileList->Release();
 	}
-
-    void PackWithzlib()
-    {
-#ifdef WITH_LIBZ
-        FILE* sFile = fopen( resourceName.c_str(), "r");
-        FILE* dFile = fopen( (resourceName + String(".gz")).c_str(), "w");
-        int ret = def(sFile, dFile, 8);
-        if (ret != Z_OK)
-            zerr(ret);
-        fclose(dFile);
-        fclose(sFile);
-#endif
-    }
-    
 private:
+
+    FileSystem*	    fileSystem = nullptr;
+    std::unique_ptr<ResourceArchive, void(*)(ResourceArchive*)> resourceArchive;
 
 	List<String>	fileNameList;
 
 	String			resourceName;
 	String			packedDir;
-	String			headerName;
-	IFileSystem *	fileSystem;
-	ResourceArchive resourceArchive;
-	int32			resourceCount;
+	int32			resourceCount = 0;
 };
 
 void FrameworkDidLaunched()
@@ -229,198 +175,114 @@ void FrameworkWillTerminate()
     
 }
 
+int PackDirectoryIntoPakfile(const String& dir, const String& pak, const String& compression)
+{
+    int result = EXIT_FAILURE;
+
+    std::cout << "===================================================\n"
+              << "=== Packer started\n"
+              << "=== Pack directory: " << dir << '\n'
+              << "=== Pack archiveName: " << pak << '\n'
+              << "===================================================\n";
+
+    const String dirWithSlash = (dir.back() == '/' ? dir : dir + '/');
+
+    Packer packer(pak, dirWithSlash);
+
+    if(packer.Pack())
+    {
+        result = EXIT_SUCCESS;
+    }
+
+    return result;
+}
+
+int UnpackPackfileIntoDirectory(const String& pak, const String& dir)
+{
+    FileSystem * fs = new FileSystem();
+    DVASSERT(fs);
+    String programmPath = fs->GetCurrentWorkingDirectory().GetAbsolutePathname();
+
+    auto pathArchiveNameDir = programmPath + "/" + pak;
+
+    std::cout << "===================================================\n"
+        << "=== Unpacker started\n"
+        << "=== Unpack directory: " << dir << '\n'
+        << "=== Unpack archiveName: " << pak << '\n'
+        << "===================================================\n";
+
+
+    fs->CreateDirectory(dir);
+
+    std::unique_ptr<ResourceArchive, void (*)(ResourceArchive*)> ra(new ResourceArchive(), [](ResourceArchive*ptr){ SafeRelease(ptr); });
+    ra->Open(pathArchiveNameDir);
+
+    for (int file = 0; file < ra->GetFileCount(); ++file)
+    {
+        String pathName = ra->GetResourcePathname(file);
+        pathName = dir + pathName;
+
+        String::size_type pos = pathName.rfind('/');
+        if (pos != String::npos)
+        {
+            String dirCreate = pathName.substr(0, pos);
+            fs->CreateDirectory(dirCreate.c_str());
+        }
+
+        int32 size = ra->LoadResource(file, 0);
+        uint8 * data = new uint8[size];
+        ra->LoadResource(file, data);
+
+        File * unpackFile = File::Create(pathName, File::CREATE | File::WRITE);
+        if (unpackFile)
+        {
+            unpackFile->Write(data, size);
+            SafeRelease(unpackFile);
+        } else
+        {
+            // TODO error
+        }
+
+        delete[] data;
+    }
+    return EXIT_SUCCESS;
+}
+
 
 int main(int argc, char* argv[])
 {
-	
-	if (argc <= 2)
-	{
-		printf("Usage: resourcepacker.exe [-d] [-u] [resourceDirectoryPath] [archiveName]\n");
-		printf("Written by Borodovsky Vitaliy (C) 2006.\n");
-		printf("Example:\n");
-		printf("resourcepacker.exe -d data data.re\n\t- pack directory data to archive data.re\n");
-		printf("resourcepacker.exe -u data data.re\n\t- unpack archive data.re to directory data\n");
-		printf("Options:\n");
-		printf("\t -d don't include to archive relative pathnames\n");
-		printf("\t -u unpack archive with contents\n");
-		printf("\t -z pack with libz \n");
-		return 0;
-	}
-	
-	bool	packPaths = true;
-	bool	showMode = false;
-    bool    useLibz = false;
-	String	directoryPath;
-	String  archiveName;
-	String	headerName;
-	
-	int32 pathIndex = 1;
-	
-	for (int flag = 0; flag < 3; ++flag)
-	{
-		if (String(argv[pathIndex]) == "-d")
-		{
-			pathIndex++;
-			packPaths = false;
-		}
-		else if (String(argv[pathIndex]) == "-u")
-		{
-			pathIndex++;
-			showMode = true;
-		}
-        else if (String(argv[pathIndex]) == "-z")
-        {
-			++pathIndex;
-			useLibz = true;
-        }
-	}
-	
+    int result = EXIT_FAILURE;
 
-//	IEngineSystem * es = CreateConsoleEngine();
-	new AnsiFileSystem();
-	
-	IFileSystem * fs = Singleton<IO::IFileSystem>::Instance();
-	String programmPath = fs->GetCurrentDirectory();
+    ProgramOptions packOptions("pack");
+    packOptions.AddOption("--compression", VariantType(String("lz4")), "compression method, lz4 - default");
+    packOptions.AddArgument("directory");
+    packOptions.AddArgument("pakfile");
 
-	if (showMode)	// show mode of ResourcePacker tool
-	{
-		directoryPath = argv[pathIndex];	
-		
-		if (directoryPath.find(':') == -1)
-		{
-			directoryPath = programmPath + "/" + directoryPath;
-		}
+    ProgramOptions unpackOptions("unpack");
+    unpackOptions.AddArgument("directory");
+    unpackOptions.AddArgument("pakfile");
 
-		if ((pathIndex + 1) > (argc - 1))
-		{
-			String::size_type pos = directoryPath.rfind('/');
-			if (pos == -1)
-			{
-				archiveName = directoryPath + ".re";
-			}else
-			{
-				archiveName = directoryPath.substr(pos + 1) + ".re";
-			}
-		}else
-		{
-			archiveName = argv[pathIndex + 1];
-			String::size_type pos = archiveName.rfind('.');
-			if (pos == -1)
-			{
-				headerName = archiveName;
-				archiveName += ".re";
-				headerName += ".h";
-			}
-		}
+    if (packOptions.Parse(argc, argv))
+    {
+        String compression = packOptions.GetOption("--compression").AsString();
 
+        String dirName = packOptions.GetArgument("directory");
+        String pakFile = packOptions.GetArgument("pakfile");
 
-		printf("===================================================\n");
-		printf("=== Unpacker started\n");
-		printf("=== Unpack directory: %s\n", directoryPath.c_str());
-		printf("=== Unpack archiveName: %s\n", archiveName.c_str());
-		printf("===================================================\n");
-
-
-		fs->CreateDirectory(directoryPath.c_str());
-
-		ResourceArchive ra;
-		ra.Open(programmPath + "/" + archiveName);
-		
-		
-
-		for (int file = 0; file < ra.GetFileCount(); ++file)
-		{
-			String pathName = ra.GetResourcePathname(file);
-			pathName = directoryPath + pathName;
-			
-			String::size_type pos = pathName.rfind('/');
-			if (pos == -1)
-			{
-			}else
-			{
-				String dirCreate = pathName.substr(0, pos);
-				fs->CreateDirectory(dirCreate.c_str());
-			}
-
-			int32 size = ra.LoadResource(file, 0);
-			uint8 * data = new uint8[size];
-			ra.LoadResource(file, data);
-					
-			IFile * unpackFile = fs->CreateFile(pathName.c_str(), IO::EFA_CREATE | IO::EFA_WRITE);
-			if (unpackFile)
-			{
-				unpackFile->Write(data, size);
-				SafeRelease(unpackFile);
-			}
-			else
-			{
-				
-			}
-
-			delete [] data;
-		}
-
-	}else	// archive mode of ResourcePacker tool
-	{
-		directoryPath = argv[pathIndex];	
-
-		if (directoryPath.find(':') == -1)
-		{
-			directoryPath = programmPath + "/" + directoryPath;
-		}
-
-
-		if ((pathIndex + 1) > (argc - 1))
-		{
-			String::size_type pos = directoryPath.rfind('/');
-			if (pos == -1)
-			{
-				archiveName = directoryPath + ".re";
-				headerName = directoryPath + ".h";
-			}else
-			{
-				archiveName = directoryPath.substr(pos + 1) + ".re";
-				headerName = directoryPath.substr(pos + 1) + ".h";
-			}
-		}else
-		{
-			archiveName = argv[pathIndex + 1];
-			String::size_type pos = archiveName.rfind('.');
-			if (pos == -1)
-			{
-				headerName = archiveName;
-				archiveName += ".re";
-				headerName += ".h";
-			}else
-			{
-				headerName = archiveName.substr(0, pos) + ".h";
-			}
-		}
-
-		printf("===================================================\n");
-		printf("=== Packer started\n");
-		printf("=== Include relative filepaths %s\n", (packPaths == true)? ("enabled"):("disabled"));
-		printf("=== Pack directory: %s\n", directoryPath.c_str());
-		printf("=== Pack archiveName: %s\n", archiveName.c_str());
-		printf("=== Pack headerName: %s\n", headerName.c_str());
-		printf("===================================================\n");
-
-		Packer * packer = new Packer(programmPath + "/" + archiveName, directoryPath, programmPath + "/" + headerName);
-
-		packer->Pack(packPaths);
-        if (useLibz)
-        {
-            packer->PackWithzlib();
-        }
+        result = PackDirectoryIntoPakfile(dirName, pakFile, compression);
+    } else if (unpackOptions.Parse(argc, argv))
+    {
+        String pakFile = unpackOptions.GetArgument("pakfile");
+        String dirName = unpackOptions.GetArgument("directory");
         
-		delete packer;
-		packer = 0;
-	}
-	
+        result = UnpackPackfileIntoDirectory(pakFile, dirName);
+    } else
+    {
+        packOptions.PrintUsage();
+        unpackOptions.PrintUsage();
+    }
 
-//	SafeRelease(es);
-
-	return 0;
+	return result;
 }
 
 
