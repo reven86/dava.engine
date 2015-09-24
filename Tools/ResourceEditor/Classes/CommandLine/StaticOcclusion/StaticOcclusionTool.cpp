b553b5727b9b73167054d4cf36c669d4f0c39f63
Stanislav Madytsky <s_madytskiy@wargaming.net>
2015-09-24 18:01:50 +0300

    DF-7836 packer flags fixes + refactoring
    Former-commit-id: c49e0e6b12aee672840f7382645ca48315507e8c

--- Tools/ResourceEditor/Classes/CommandLine/StaticOcclusion/StaticOcclusionTool.cpp
+++ Tools/ResourceEditor/Classes/CommandLine/StaticOcclusion/StaticOcclusionTool.cpp
@@ -29,7 +29,7 @@
 
 #include "StaticOcclusionTool.h"
 
-#include "TexturePacker/CommandLineParser.h"
+#include "CommandLine/CommandLineParser.h"
 #include "Scene/SceneEditor2.h"
 
 using namespace DAVA;
