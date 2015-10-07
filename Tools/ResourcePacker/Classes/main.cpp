b98eff80283c4feb25abbe39ded158285e0c3e40
Stanislav Madytsky <s_madytskiy@wargaming.net>
2015-10-07 16:41:09 +0300

    DF-8027 moving respacker on cmake
    Former-commit-id: ccff9915b8f30755f16fb5871eddf0bd7ed997e9

--- Tools/ResourcePacker/Classes/main.cpp
+++ Tools/ResourcePacker/Classes/main.cpp
@@ -26,26 +26,9 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =====================================================================================*/
 
-
 #include "DAVAEngine.h"
 
-int APIENTRY WinMain(HINSTANCE hInstance,
-					   HINSTANCE hPrevInstance,
-					   LPTSTR    lpCmdLine,
-					   int       nCmdShow)
+int main(int argc, char* argv[])
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
-	return DAVA::Core::Run(0, 0, hInstance);
+    return DAVA::Core::RunCmdTool(argc, argv);
 }
-
-/*int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR    lpCmdLine, int       nCmdShow)
-{
-	// find leaks mega-string: {,,msvcr71d.dll}_crtBreakAlloc
-	//
-	// POSSIBLE LEAKS LIST:
-	// remember -- always clear all allocated data for static STL containers
-	// 
-	return 0;
-}*/
