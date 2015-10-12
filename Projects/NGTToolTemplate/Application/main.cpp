606bfaba13c9cf569c81afd8dda5d4cfaef15ebf
y_rakhuba <y_rakhuba@wargaming.net>
2015-10-12 16:53:21 +0300

    [DF-8136] Removed auto append of debug postfix in plugin names Removed file system root on windows. View try to accumulate all nodes recursively and applications freeze on start
    Former-commit-id: 071092d94c88501333316ccc25b87b438752dbba

--- Projects/NGTToolTemplate/Application/main.cpp
+++ Projects/NGTToolTemplate/Application/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char ** argv)
     }
 
     {
-        GenericPluginManager pluginManager;
+        GenericPluginManager pluginManager(false);
 
         pluginManager.loadPlugins(plugins);
 
