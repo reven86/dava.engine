fc7746a546cf2c38c30090ddd754d6e68c62494f
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-11-11 15:53:52 +0300

    [DF-7344] localization dialog and font dialog was brutally killed
    Former-commit-id: 0d9f319b735c645d1636dd8d5d479c764117b39f

--- Tools/QuickEd/Classes/Project/EditorLocalizationSystem.cpp
+++ Tools/QuickEd/Classes/Project/EditorLocalizationSystem.cpp
@@ -29,42 +29,97 @@
 
 #include "Project/EditorLocalizationSystem.h"
 #include "FileSystem/LocalizationSystem.h"
-#include "FileSystem/FileList.h"
+
+#include <QLocale>
+#include <QDirIterator>
+#include <QDir>
 
 using namespace DAVA;
 
 EditorLocalizationSystem::EditorLocalizationSystem(QObject* parent)
+    : QObject(parent)
 {
 
 }
 
-void EditorLocalizationSystem::InitLanguageWithDirectory(const FilePath &directoryPath, const String &localeId)
+QStringList EditorLocalizationSystem::GetAvailableLocaleNames() const
 {
-    LocalizationSystem::Instance()->SetDirectory(directoryPath);
-    if (!directoryPath.IsEmpty())
+    return availableLocales.keys();
+}
+
+QStringList EditorLocalizationSystem::GetAvailableLocaleValues() const
+{
+    return availableLocales.values();
+}
+
+void EditorLocalizationSystem::SetDirectory(const QDir &directoryPath)
+{
+    LocalizationSystem *localizationSystem = LocalizationSystem::Instance();
+    DVASSERT(nullptr != localizationSystem);
+
+    FilePath directoryFilePath(directoryPath.absolutePath().toStdString() + "/"); //absolutePath doesn't contains with '/' symbol at end
+    localizationSystem->SetDirectory(directoryFilePath);
+    if (!directoryFilePath.IsEmpty())
     {
-        FileList * fileList = new FileList(directoryPath);
-        for (auto count = fileList->GetCount(), k = 0; k < count; ++k)
+        QDirIterator dirIterator(directoryPath, QDirIterator::NoIteratorFlags);
+        while (dirIterator.hasNext())
         {
-            if (!fileList->IsDirectory(k))
+            dirIterator.next();
+            QFileInfo fileInfo = dirIterator.fileInfo();
+            if (!fileInfo.isDir())
             {
-                availableLocales.push_back(QString::fromStdString(fileList->GetPathname(k).GetBasename()));
+                QString localeStr = fileInfo.baseName();
+                QString localeName = GetLocaleNameFromStr(localeStr);
+                availableLocales.insert(localeName, localeStr);
             }
         }
-
-        SafeRelease(fileList);
     }
+}
 
-    LocalizationSystem::Instance()->SetCurrentLocale(localeId);
-
-    LocalizationSystem::Instance()->Init();
-
-    emit LocaleChanged(LocalizationSystem::Instance()->GetCurrentLocale());
+void EditorLocalizationSystem::SetCurrentLocaleValue(const QString& localeStr)
+{
+    QStringList keys = availableLocales.keys(localeStr);
+    DVASSERT(keys.size() == 1);
+    SetCurrentLocale(keys.front());
 }
 
 void EditorLocalizationSystem::Cleanup()
 {
     availableLocales.clear();
     LocalizationSystem::Instance()->Cleanup();
+}
 
+QString EditorLocalizationSystem::GetLocaleNameFromStr(QString localeStr)
+{
+    QLocale locale(localeStr);
+    switch (locale.script())
+    {
+    default:
+        return QLocale::languageToString(locale.language());
+
+    case QLocale::SimplifiedChineseScript:
+        return "Chinese simpl.";
+
+    case QLocale::TraditionalChineseScript:
+        return "Chinese trad.";
+    }
+}
+
+QString EditorLocalizationSystem::GetCurrentLocale() const
+{
+    return currentLocale;
+}
+
+void EditorLocalizationSystem::SetCurrentLocale(const QString& locale)
+{
+    DVASSERT(!locale.isEmpty());
+    if (currentLocale != locale)
+    {
+        currentLocale = locale;
+        DVASSERT(availableLocales.contains(locale));
+        LocalizationSystem::Instance()->SetCurrentLocale(availableLocales[locale].toStdString());
+        LocalizationSystem::Instance()->Init();
+
+        emit CurrentLocaleChanged(currentLocale);
+    }
 }
