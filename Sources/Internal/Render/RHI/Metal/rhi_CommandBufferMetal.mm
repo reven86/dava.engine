b064fdcccffc045e4515fa2b2371bed76e6facb2
d_shpakov <d_shpakov@wargaming.net>
2015-11-12 19:07:48 +0300

    DF-185 replaced !(defined(TARGET_IPHONE_SIMULATOR) && TARGET_IPHONE_SIMULATOR==1) by !(TARGET_IPHONE_SIMULATOR==1)
    Former-commit-id: 8c4e4c01eeeabc3e851a6c843be674b1942e13db

--- Sources/Internal/Render/RHI/Metal/rhi_CommandBufferMetal.mm
+++ Sources/Internal/Render/RHI/Metal/rhi_CommandBufferMetal.mm
@@ -41,7 +41,7 @@ using DAVA::Logger;
 
 #include "_metal.h"
 
-#if !(defined(TARGET_IPHONE_SIMULATOR) && TARGET_IPHONE_SIMULATOR==1)
+#if !(TARGET_IPHONE_SIMULATOR==1)
 namespace rhi
 {
 struct
@@ -725,4 +725,4 @@ void SetupDispatch(Dispatch* dispatch)
 
 } // namespace rhi
 
-#endif //#if !(defined(TARGET_IPHONE_SIMULATOR) && TARGET_IPHONE_SIMULATOR==1)
+#endif //#if !(TARGET_IPHONE_SIMULATOR==1)
