64bf10efaaa91f6d8ca1de3fb951e7515686415c
Maksim Lazarev <m_lazarev@wargaming.net>
2015-11-18 17:00:52 +0300

    DF-8600 implement functions to obtain backtrace and symbols
    Former-commit-id: fd816efd562ef4b9cad17a7c163511bc0f014165

--- Sources/Internal/Debug/Backtrace.h
+++ Sources/Internal/Debug/Backtrace.h
@@ -29,4 +29,51 @@
 #ifndef __DAVAENGINE_BACKTRACE_H__
 #define __DAVAENGINE_BACKTRACE_H__
 
+// clang-format off
+#include "Base/BaseTypes.h"
+#include "FileSystem/Logger.h"
+
+namespace DAVA
+{
+namespace Debug
+{
+
+struct StackFrame
+{
+    StackFrame() = default;
+    StackFrame(void* addr_, const char8* func_)
+        : addr(addr_)
+        , function(func_)
+    {}
+    StackFrame(void* addr_, String func_)
+        : addr(addr_)
+        , function(std::move(func_))
+    {}
+
+    void* addr;
+    String function;
+};
+
+Vector<StackFrame> GetBacktrace(size_t framesToCapture = -1);
+
+// Convert backtrace to string
+//  1st version uses already obtained backtrace and formats up to nframes items
+//  2nd version internally gets backtrace and formats all items
+String BacktraceToString(const Vector<StackFrame>& backtrace, size_t nframes = -1);
+String BacktraceToString(size_t framesToCapture);
+
+// Prints backtrace to log using specified log level
+//  1st version uses already obtained backtrace
+//  2nd version internally gets backtrace and logs all items
+void BacktraceToLog(const Vector<StackFrame>& backtrace, Logger::eLogLevel ll = Logger::LEVEL_ERROR);
+void BacktraceToLog(size_t framesToCapture = -1, Logger::eLogLevel ll = Logger::LEVEL_ERROR);
+
+// Low level function to get stack frames and symbols
+DAVA_NOINLINE size_t GetStackFrames(void* frames[], size_t framesToCapture);
+String GetSymbolFromAddr(void* addr, bool demangle = true);
+String DemangleSymbol(const char8* symbol);
+
+} // namespace Debug
+} // namespace DAVA
+
 #endif // __DAVAENGINE_BACKTRACE_H__
