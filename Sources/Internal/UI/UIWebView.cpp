a3c9bd92350a5e042b96789a515d8ebc55f45677
k_lyashkevich <k_lyashkevich@wargaming.net>
2015-12-11 14:56:16 +0300

    [DF-8610] revert webview files
    Former-commit-id: 54977a8f8ca8d50f69e080f2f479cd9dc788038a

--- Sources/Internal/UI/UIWebView.cpp
+++ Sources/Internal/UI/UIWebView.cpp
@@ -192,6 +192,7 @@ void UIWebView::UpdateControlRect()
 
 void UIWebView::SetRenderToTexture(bool value)
 {
+    // for now disable this functionality
     value = false;
     webViewControl->SetRenderToTexture(value);
 }
