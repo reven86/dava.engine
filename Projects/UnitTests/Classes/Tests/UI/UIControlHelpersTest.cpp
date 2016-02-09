130c651ec293986ed98fa3176d279b6f39ed24da
Dmitry Belsky <d_belsky@wargaming.net>
2016-02-09 13:38:53 +0300

    [DF-9412] review notes
    Former-commit-id: 7dba5ba83d27641779dd61399e83782835db09fc

--- Projects/UnitTests/Classes/Tests/UI/UIControlHelpersTest.cpp
+++ Projects/UnitTests/Classes/Tests/UI/UIControlHelpersTest.cpp
@@ -41,34 +41,50 @@ DAVA_TESTCLASS(UIControlHelpersTest)
     UIControl* smallControl = nullptr;
     UIControl* bigControl = nullptr;
 
-    void SetUp(const String& testName) override
+    void Start(bool withPivots)
     {
         scrollView = new UIScrollView();
         scrollView->SetAutoUpdate(true);
         scrollView->SetSize(Vector2(100.0f, 100.0f));
         scrollView->GetContainer()->SetSize(Vector2(1000.0f, 1000.0f));
+        if (withPivots)
+        {
+            scrollView->SetPivot(Vector2(0.5f, 0.5f));
+            scrollView->SetPosition(Vector2(50.0f, 50.0f));
+        }
 
         bigControl = new UIControl();
         bigControl->SetSize(Vector2(200.0f, 200.0f));
         bigControl->SetPosition(Vector2(200.0f, 200.0f));
+        if (withPivots)
+        {
+            bigControl->SetPivot(Vector2(0.5f, 0.5f));
+            bigControl->SetPosition(Vector2(300.0f, 300.0f));
+        }
         scrollView->AddControlToContainer(bigControl);
 
         smallControl = new UIControl();
         smallControl->SetSize(Vector2(50.0f, 50.0f));
         smallControl->SetPosition(Vector2(0.0f, 0.0f));
+        if (withPivots)
+        {
+            smallControl->SetPivot(Vector2(0.5f, 0.5f));
+            smallControl->SetPosition(Vector2(25.0f, 25.0f));
+        }
         bigControl->AddControl(smallControl);
     }
 
-    void TearDown(const String& testName) override
+    void End()
     {
         SafeRelease(scrollView);
         SafeRelease(smallControl);
         SafeRelease(bigControl);
     }
 
-    // UIControlHelpers::ScrollToControl
-    DAVA_TEST(SmallControlOnScreen_ScrollDontWork)
+    void SmallControlOnScreen_ScrollDontWork(bool withPivots)
     {
+        Start(withPivots);
+
         Vector2 result;
 
         scrollView->SetScrollPosition(Vector2(-175.0f, -175.0f));
@@ -81,11 +97,21 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = smallControl->GetAbsolutePosition() - smallControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, 25.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, 25.0f));
+
+        End();
     }
 
     // UIControlHelpers::ScrollToControl
-    DAVA_TEST(SmallControlRightThanView_ScrollToLeft)
+    DAVA_TEST(SmallControlOnScreen_ScrollDontWork)
     {
+        SmallControlOnScreen_ScrollDontWork(false);
+        SmallControlOnScreen_ScrollDontWork(true);
+    }
+
+    void SmallControlRightThanView_ScrollToLeft(bool withPivots)
+    {
+        Start(withPivots);
+
         Vector2 result;
 
         scrollView->SetScrollPosition(Vector2(0.0f, 0.0f));
@@ -98,11 +124,21 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = smallControl->GetAbsolutePosition() - smallControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, 50.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, 50.0f));
+
+        End();
     }
 
     // UIControlHelpers::ScrollToControl
-    DAVA_TEST(SmallControlLeftThanView_ScrollToRight)
+    DAVA_TEST(SmallControlRightThanView_ScrollToLeft)
     {
+        SmallControlRightThanView_ScrollToLeft(false);
+        SmallControlRightThanView_ScrollToLeft(true);
+    }
+
+    void SmallControlLeftThanView_ScrollToRight(bool withPivots)
+    {
+        Start(withPivots);
+
         Vector2 result;
 
         // don't scroll if control visible
@@ -116,11 +152,21 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = smallControl->GetAbsolutePosition() - smallControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, 0.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, 0.0f));
+
+        End();
     }
 
     // UIControlHelpers::ScrollToControl
-    DAVA_TEST(BigControlOnScreen_DontScroll)
+    DAVA_TEST(SmallControlLeftThanView_ScrollToRight)
     {
+        SmallControlLeftThanView_ScrollToRight(false);
+        SmallControlLeftThanView_ScrollToRight(true);
+    }
+
+    void BigControlOnScreen_DontScroll(bool withPivots)
+    {
+        Start(withPivots);
+
         Vector2 result;
 
         scrollView->SetScrollPosition(Vector2(-250.0f, -250.0f));
@@ -133,11 +179,21 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = bigControl->GetAbsolutePosition() - bigControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, -50.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, -50.0f));
+
+        End();
     }
 
     // UIControlHelpers::ScrollToControl
-    DAVA_TEST(BigControlLeftThanView_ScrollToRight)
+    DAVA_TEST(BigControlOnScreen_DontScroll)
     {
+        BigControlOnScreen_DontScroll(false);
+        BigControlOnScreen_DontScroll(true);
+    }
+
+    void BigControlLeftThanView_ScrollToRight(bool withPivots)
+    {
+        Start(withPivots);
+
         Vector2 result;
 
         scrollView->SetScrollPosition(Vector2(-900.0f, -900));
@@ -157,11 +213,21 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = bigControl->GetAbsolutePosition() - bigControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, 0.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, 0.0f));
+
+        End();
     }
 
     // UIControlHelpers::ScrollToControl
-    DAVA_TEST(BigControlRightThanView_ScrollToLeft)
+    DAVA_TEST(BigControlLeftThanView_ScrollToRight)
     {
+        BigControlLeftThanView_ScrollToRight(false);
+        BigControlLeftThanView_ScrollToRight(true);
+    }
+
+    void BigControlRightThanView_ScrollToLeft(bool withPivots)
+    {
+        Start(withPivots);
+
         Vector2 result;
 
         scrollView->SetScrollPosition(Vector2(0.0f, 0.0f));
@@ -181,5 +247,14 @@ DAVA_TESTCLASS(UIControlHelpersTest)
         result = bigControl->GetAbsolutePosition() - bigControl->GetPivotPoint();
         TEST_VERIFY(FLOAT_EQUAL(result.x, 0.0f));
         TEST_VERIFY(FLOAT_EQUAL(result.y, 0.0f));
+
+        End();
+    }
+
+    // UIControlHelpers::ScrollToControl
+    DAVA_TEST(BigControlRightThanView_ScrollToLeft)
+    {
+        BigControlRightThanView_ScrollToLeft(false);
+        BigControlRightThanView_ScrollToLeft(true);
     }
 };
