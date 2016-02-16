4f3e4539aa1dc1ac2d845c5177ed497f78961bc5
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-02-16 17:11:21 +0300

    cmake-tool: tmpcommit
    Former-commit-id: db2f790df12b73b52cd6cf06267f96cc93e333fa

--- Tools/CMakeTool/main.cpp
+++ Tools/CMakeTool/main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QFile>
+#include <QJsonDocument>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +16,31 @@ int main(int argc, char *argv[])
     qCritical() << "application started on undefined platform";
     return 1;
 #endif //platform
+    if(!QFile::exists(configPath))
+    {
+        QMessageBox::critical(nullptr, tr("Config file not available!"), tr("Can not find config file %1").arg(configPath));
+        return;
+    }
     QFile configFile(fileName);
-    if(configFile.op)
+    if(configFile.open(QIODevice::ReadOnly))
+    {
+        QByteArray data = configFile.readAll();
+        QJsonParseError err;
+        QJsonDocument::fromJson(data, &err);
+        if(err.error != QJsonParseError::NoError)
+        {
+            QMessageBox::critical(nullptr, tr("Config file corrupted!"), tr("Failed to parse config file: error %1").arg(err.errorString()));
+            return;
+        }
+    }
+    else
+    {
+        QMessageBox::critical(nullptr, tr("Failed to open config file!"), tr("Failed to open config file %1").arg(configPath));
+        return;
+    }
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
 
     return app.exec();
 }
