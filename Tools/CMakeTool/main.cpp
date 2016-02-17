0a4534c4ad6e94f47325f97523f21e012f1d44cf
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-02-18 00:36:53 +0300

    ready to create lineedit
    Former-commit-id: 37f563e9eb780ae14ee84a1cc7ccf44fc3ff6417

--- Tools/CMakeTool/main.cpp
+++ Tools/CMakeTool/main.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
 #include <QString>
 #include <QQmlApplicationEngine>
-#include <QFile>
-#include <QJsonDocument>
-#include <QMessageBox>
 #include <QQmlContext>
+#include "configstorage.h"
 
 int main(int argc, char *argv[])
 {
@@ -12,40 +10,22 @@ int main(int argc, char *argv[])
     QString configPath;
 #ifdef Q_OS_WIN
     configPath = ":/Resources/config_windows.txt";
-#elif Q_OS_MAC
+#elif defined Q_OS_MAC
     configPath = ":/Resources/config_mac.txt";
 #else
     qCritical() << "application started on undefined platform";
     return 1;
 #endif //platform
-    if(!QFile::exists(configPath))
+    ConfigStorage configStorage(configPath);
+    QString configuration = configStorage.GetJSONTextFromConfig();
+    if(configuration.isEmpty())
     {
-        QMessageBox::critical(nullptr, QObject::tr("Config file not available!"), QObject::tr("Can not find config file %1").arg(configPath));
-        return 0;
-    }
-    QFile configFile(configPath);
-    QVariant configuration;
-    if(configFile.open(QIODevice::ReadOnly))
-    {
-        QByteArray data = configFile.readAll();
-        QJsonParseError err;
-        QJsonDocument::fromJson(data, &err);
-        if(err.error != QJsonParseError::NoError)
-        {
-            QMessageBox::critical(nullptr, QObject::tr("Config file corrupted!"), QObject::tr("Failed to parse config file: error %1").arg(err.errorString()));
-            return 0;
-        }
-        configuration.setValue(data);
-    }
-    else
-    {
-        QMessageBox::critical(nullptr, QObject::tr("Failed to open config file!"), QObject::tr("Failed to open config file %1").arg(configPath));
         return 0;
     }
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("configuration", configuration);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
+    QObject::connect(engine.rootObjects().at(0), SIGNAL(dataReadyToSave(QString)), &configStorage, SLOT(SaveJSONTestToConfig(QString)));
     return app.exec();
 }
 
