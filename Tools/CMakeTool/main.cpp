02ecd7b06e0ded1bdcd9ea6da35ae98b329f79b0
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-02-18 15:53:46 +0300

    [DF-9532] created cmake-tool
    Former-commit-id: 09f0ff3797177293dc7ba44db274deef4d985a6c

---
+++ Tools/CMakeTool/main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QString>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "configstorage.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QString configPath;
+#ifdef Q_OS_WIN
+    configPath = ":/Resources/config_windows.txt";
+#elif defined Q_OS_MAC
+    configPath = ":/Resources/config_mac.txt";
+#else
+    qCritical() << "application started on undefined platform";
+    return 1;
+#endif //platform
+    ConfigStorage configStorage(configPath);
+    QString configuration = configStorage.GetJSONTextFromConfig();
+    if(configuration.isEmpty())
+    {
+        return 0;
+    }
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("configuration", configuration);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    //QObject::connect(engine.rootObjects().at(0), SIGNAL(dataReadyToSave(QString)), &configStorage, SLOT(SaveJSONTestToConfig(QString)));
+    return app.exec();
+}
+
