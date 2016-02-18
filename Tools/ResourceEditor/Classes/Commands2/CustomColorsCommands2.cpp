07899ac0f06f846ec0487fbeedf5c2ef41a28931
Viktor Kleschenko <v_kleschenko@wargaming.net>
2016-02-18 16:15:30 +0300

    [DF-8876] Fixed tilemask and custom colors texture using
    Former-commit-id: 577372a81890721bab3d24b3a3f4f85349131265

--- Tools/ResourceEditor/Classes/Commands2/CustomColorsCommands2.cpp
+++ Tools/ResourceEditor/Classes/Commands2/CustomColorsCommands2.cpp
@@ -119,11 +119,8 @@ void ActionDisableCustomColors::Redo()
     SceneSignals::Instance()->EmitCustomColorsToggled(sceneEditor);
 }
 
-ModifyCustomColorsCommand::ModifyCustomColorsCommand(Image* originalImage, Image* currentImage,
-                                                     CustomColorsProxy* _customColorsProxy,
-                                                     const Rect& _updatedRect)
+ModifyCustomColorsCommand::ModifyCustomColorsCommand(Image* originalImage, Image* currentImage, CustomColorsProxy* _customColorsProxy, const Rect& _updatedRect)
     : Command2(CMDID_CUSTOM_COLORS_MODIFY, "Custom Colors Modification")
-    , texture(nullptr)
 {
     const Vector2 topLeft(floorf(_updatedRect.x), floorf(_updatedRect.y));
     const Vector2 bottomRight(ceilf(_updatedRect.x + _updatedRect.dx), ceilf(_updatedRect.y + _updatedRect.dy));
@@ -141,7 +138,6 @@ ModifyCustomColorsCommand::~ModifyCustomColorsCommand()
     SafeRelease(undoImage);
     SafeRelease(redoImage);
     SafeRelease(customColorsProxy);
-    SafeRelease(texture);
 }
 
 void ModifyCustomColorsCommand::Undo()
@@ -158,18 +154,14 @@ void ModifyCustomColorsCommand::Redo()
 
 void ModifyCustomColorsCommand::ApplyImage(DAVA::Image* image)
 {
-    SafeRelease(texture);
-
-    Texture* customColorsTarget = customColorsProxy->GetTexture();
-    texture = Texture::CreateFromData(image->GetPixelFormat(), image->GetData(),
-                                      image->GetWidth(), image->GetHeight(), false);
+    ScopedPtr<Texture> fboTexture(Texture::CreateFromData(image->GetPixelFormat(), image->GetData(), image->GetWidth(), image->GetHeight(), false));
 
     RenderSystem2D::RenderTargetPassDescriptor desc;
-    desc.target = customColorsTarget;
+    desc.target = customColorsProxy->GetTexture();
     desc.shouldClear = false;
     desc.shouldTransformVirtualToPhysical = false;
     RenderSystem2D::Instance()->BeginRenderTargetPass(desc);
-    RenderSystem2D::Instance()->DrawTexture(texture, customColorsProxy->GetBrushMaterial(), Color::White, updatedRect);
+    RenderSystem2D::Instance()->DrawTexture(fboTexture, customColorsProxy->GetBrushMaterial(), Color::White, updatedRect);
     RenderSystem2D::Instance()->EndRenderTargetPass();
 
     customColorsProxy->UpdateRect(updatedRect);
