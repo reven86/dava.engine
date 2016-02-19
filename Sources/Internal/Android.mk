850f6c3fa82fd7f4dddb273756ca7ce284d74d10
Andrey Derkach <a_derkach2@wargaming.net>
2016-02-19 15:58:42 +0300

    [DF-9503] Android project fix
    Former-commit-id: 31484d5a2da1ddf2229e1144a77d04bd6eb8c216

--- Sources/Internal/Android.mk
+++ Sources/Internal/Android.mk
@@ -388,7 +388,8 @@ LOCAL_SRC_FILES := \
                      $(wildcard $(LOCAL_PATH)/DataStorage/Android/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Timer/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Notification/*.cpp)) \
-                     $(wildcard $(LOCAL_PATH)/CommandLine/*.cpp))
+                     $(wildcard $(LOCAL_PATH)/CommandLine/*.cpp)) \
+                     $(wildcard $(LOCAL_PATH)/Logger/*.cpp))
 
 include $(BUILD_STATIC_LIBRARY)
 
