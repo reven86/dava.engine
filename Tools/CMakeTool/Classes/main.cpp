08bc157848b431091b356e5f4e664465ad463822
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-02-22 00:45:10 +0300

    [DF-9532] UI settings added, code refactored, bugs fixed
    Former-commit-id: 179ab1225821ba3fa209c30c4cbb503c7a39e4b8

--- Tools/CMakeTool/Classes/main.cpp
+++ Tools/CMakeTool/Classes/main.cpp
@@ -1,9 +1,10 @@
 #include <QApplication>
 #include <QString>
 #include <QQmlApplicationEngine>
-#include <QQmlContext>
 #include <QProcessEnvironment>
 #include <QMessageBox>
+#include <QtQuick>
+#include <QtQML>
 #include "configstorage.h"
 #include "processwrapper.h"
 #include "filesystemhelper.h"
@@ -13,37 +14,22 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QString configPath;
-#ifdef Q_OS_WIN
-    configPath = "../CMakeTool/Resources/config_windows.txt";
-#elif defined Q_OS_MAC
-    configPath = app.applicationDirPath() + "/../Resources/Data/config_mac.txt";
-#else
-    qCritical() << "application started on undefined platform";
-    return 1;
-#endif //platform
-    ConfigStorage configStorage(configPath);
-    QString configuration = configStorage.GetJSONTextFromConfig();
-    if(configuration.isEmpty())
-    {
-        return 0;
-    }
+
     FileSystemHelper fileSystemHelper;
     ProcessWrapper processWrapper;
     QQmlApplicationEngine engine;
     auto rootContext = engine.rootContext();
+    qmlRegisterType<ProcessWrapper>("Cpp.Utils", 1, 0, "ProcessWrapper");
+    qmlRegisterType<FileSystemHelper>("Cpp.Utils", 1, 0, "FileSystemHelper");
+    qmlRegisterType<ConfigStorage>("Cpp.Utils", 1, 0, "ConfigStorage");
+    
     rootContext->setContextProperty("applicationDirPath", app.applicationDirPath());
-    rootContext->setContextProperty("configuration", configuration);
-    rootContext->setContextProperty("fileSystemHelper", &fileSystemHelper);
-    rootContext->setContextProperty("processWrapper", &processWrapper);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if(!engine.rootObjects().empty())
+    if(engine.rootObjects().empty())
     {
-        auto rootObject = engine.rootObjects().at(0);
-        QObject::connect(rootObject, SIGNAL(dataReadyToSave(QString)), &configStorage, SLOT(SaveJSONTestToConfig(QString)));
-        QObject::connect(rootObject, SIGNAL(invokeCmake(QString)), &processWrapper, SLOT(LaunchCmake(QString)));
-        return app.exec();
+        QMessageBox::critical(nullptr, QObject::tr("Failed to load QML file"), QObject::tr("QML file not loaded!"));
+        return 0;
     }
-    QMessageBox::critical(nullptr, QObject::tr("Failed to load QML file"), QObject::tr("QML file not loaded!"));
-    return 0;
+    return app.exec();
 }
 
