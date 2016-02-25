1bf785e9799198af27b6be61e89107c67c53af67
Dmitry Belsky <d_belsky@wargaming.net>
2016-02-25 14:03:26 +0300

    [DF-9068] focus in control
    Former-commit-id: cfa44dac7d49f9039daeaf2d43373ae2cf67091d

---
+++ Sources/Internal/UI/Focus/TabTraversalAlgorithm.cpp
@@ -0,0 +1,22 @@
+#include "TabTraversalAlgorithm.h"
+
+#include "UI/UIControl.h"
+
+namespace DAVA
+{
+TabTraversalAlgorithm::TabTraversalAlgorithm(UIControl* root)
+{
+}
+
+TabTraversalAlgorithm::~TabTraversalAlgorithm()
+{
+}
+
+UIControl* TabTraversalAlgorithm::GetNextControl(UIControl* focusedControl, FocusHelpers::TabDirection dir)
+{
+}
+
+UIControl* TabTraversalAlgorithm::FindFirstControl(UIControl* focusedControl, FocusHelpers::TabDirection dir)
+{
+}
+}
