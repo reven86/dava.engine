386058ca5efde05aceebef79fcd08b2c230d3987
Igor Solovey <i_solovey@wargaming.net>
2016-02-28 14:17:32 +0300

    DF-6370 merge two heightmap textures into one RGBA8888
    Former-commit-id: 9f4ef715a87d2606bc0e14914e1ed0dd8700d0f0

--- Sources/Internal/Render/Highlevel/Landscape.h
+++ Sources/Internal/Render/Highlevel/Landscape.h
@@ -341,7 +341,6 @@ protected:
     void AllocateGeometryDataInstancing();
 
     Texture* CreateHeightTexture(Heightmap* heightmap);
-    Texture* CreateHeightAvgTexture(Heightmap* heightmap);
 
     void DrawLandscapeInstancing();
     void DrawPatchInstancing(uint32 level, uint32 x, uint32 y, float32 patchMorph, const Vector4& nearSizePow2, const Vector4& nearMorph);
