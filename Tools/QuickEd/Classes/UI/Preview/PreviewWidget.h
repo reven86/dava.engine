d707976656e6102d95ac03015877a0f423937319
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-03-21 15:18:54 +0300

    [DF-8152] review fix
    Former-commit-id: 3d45dc94bb27ae025ce7ec7ea3f55e08781960f5

--- Tools/QuickEd/Classes/UI/Preview/PreviewWidget.h
+++ Tools/QuickEd/Classes/UI/Preview/PreviewWidget.h
@@ -77,7 +77,7 @@ signals:
     void CloseTabRequested();
     void SelectionChanged(const SelectedNodes& selected, const SelectedNodes& deselected);
     void OpenPackageFile(QString path);
-    void DropRequested(const QMimeData* data, Qt::DropAction action, PackageBaseNode* targetNode, int destIndex, const DAVA::Vector2* pos);
+    void DropRequested(const QMimeData* data, Qt::DropAction action, PackageBaseNode* targetNode, DAVA::int32 destIndex, const DAVA::Vector2* pos);
 
 public slots:
     void OnDocumentChanged(Document* document);
