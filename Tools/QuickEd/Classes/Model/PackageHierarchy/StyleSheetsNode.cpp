4fc4df93baff2f88d54aeee9a41245d41f8b2d3b
Sergey Reznik <s_reznik@wargaming.net>
2016-03-22 14:53:48 +0300

    [DF-9163] c-style casts fixed/reverted
    Former-commit-id: 0eec883c929419964688d40cd5ae2c662b8cf98c

--- Tools/QuickEd/Classes/Model/PackageHierarchy/StyleSheetsNode.cpp
+++ Tools/QuickEd/Classes/Model/PackageHierarchy/StyleSheetsNode.cpp
@@ -79,7 +79,7 @@ void StyleSheetsNode::Remove(StyleSheetNode* node)
 
 int StyleSheetsNode::GetCount() const
 {
-    return static_cast<int32>(styleSheets.size());
+    return static_cast<int>(styleSheets.size());
 }
 
 StyleSheetNode* StyleSheetsNode::Get(int index) const
