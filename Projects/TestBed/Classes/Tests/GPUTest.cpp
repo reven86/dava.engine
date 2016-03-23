ba5dd08dae75d9d2cf93e737034236eddccc00f2
klesch <victor.kleschenko@gmail.com>
2016-03-23 14:37:59 +0300

    [DF-9840] Added test of sprite into TestBed. Fixed spelling in flags.txt
    Former-commit-id: 8d29dbed31d300d4c904b789d926f85e6b78d263

--- Projects/TestBed/Classes/Tests/GPUTest.cpp
+++ Projects/TestBed/Classes/Tests/GPUTest.cpp
@@ -41,16 +41,22 @@ void GPUTest::LoadResources()
     ScopedPtr<Font> font(FTFont::Create("~res:/Fonts/korinna.ttf"));
     font->SetSize(16.0F);
 
-    ScopedPtr<UIControl> imageBackground(new UIControl(Rect(10.f, 10.f, 256.f, 128.f)));
+    { //create control to display texture, loaded with default GPU
+        ScopedPtr<UIControl> textureBackground(new UIControl(Rect(10.f, 10.f, 256.f, 128.f)));
+        ScopedPtr<Texture> texture(Texture::CreateFromFile("~res:/TestData/GPUTest/Texture/texture.tex"));
+        ScopedPtr<Sprite> sprite(Sprite::CreateFromTexture(texture, 0, 0, 128.f, 64.f));
+        textureBackground->SetSprite(sprite, 0);
+        AddControl(textureBackground);
+    }
 
-    ScopedPtr<Texture> texture(Texture::CreateFromFile("~res:/TestData/GPUTest/texture.tex"));
-    ScopedPtr<Sprite> sprite(Sprite::CreateFromTexture(texture, 0, 0, 128.f, 64.f));
+    { //create control to display sprite, loaded with default GPU
+        ScopedPtr<UIControl> spriteBackground(new UIControl(Rect(276.f, 10.f, 256.f, 128.f)));
+        ScopedPtr<Sprite> sprite(Sprite::Create("~res:/TestData/GPUTest/Sprite/texture"));
+        spriteBackground->SetSprite(sprite, 0);
+        AddControl(spriteBackground);
+    }
 
-    imageBackground->SetSprite(sprite, 0);
-
-    AddControl(imageBackground);
-
-    ScopedPtr<UIStaticText> textControl(new UIStaticText(Rect(10.0f, 138.0f, 256.f, 30.f)));
+    ScopedPtr<UIStaticText> textControl(new UIStaticText(Rect(10.0f, 138.0f, 512.f, 30.f)));
     textControl->SetTextColor(Color::White);
     textControl->SetFont(font);
     textControl->SetTextAlign(ALIGN_HCENTER | ALIGN_VCENTER);
