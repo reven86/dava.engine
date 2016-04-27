011fb9e4555553ed5ae8677d0dab7a1bbb006218
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-04-27 19:01:29 +0300

    [DF-9967] working on configuration
    Former-commit-id: fb54ed04f2c234d1b440f3c674314e6e7d7bc85c

--- Tools/Launcher/Classes/main.cpp
+++ Tools/Launcher/Classes/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
     QApplication a(argc, argv);
 
     ErrorMessanger::Instance();
-    qInstallMessageHandler(LogMessageHandler);
+    //qInstallMessageHandler(LogMessageHandler);
 
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
