bb0398c4abb765f739c3e2e73cacee5fbe3d052b
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-05-12 18:46:36 +0300

    [DF-9190] preferences dialog changed
    Former-commit-id: fdfec6f6dbb61538bb621265297d47fb02a5e657

--- Tools/QuickEd/Classes/Preferences/PreferencesDialog.cpp
+++ Tools/QuickEd/Classes/Preferences/PreferencesDialog.cpp
@@ -35,6 +35,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "Preferences/PreferencesStorage.h"
 #include <QVBoxLayout>
 #include <QHeaderView>
+#include <QHBoxLayout>
+#include <QSpacerItem>
+#include <QDialogButtonBox>
 
 REGISTER_PREFERENCES_ON_START(PreferencesDialog,
                               PREF_ARG("currentGeometry", DAVA::String()),
@@ -47,15 +50,22 @@ PreferencesDialog::PreferencesDialog(QWidget* parent, Qt::WindowFlags flags)
 {
     setWindowTitle(tr("Editor preferences"));
     resize(800, 600);
-    setLayout(new QVBoxLayout());
-    PreferencesModel* preferencesModel = new PreferencesModel(this);
+    QVBoxLayout* verticalLayout = new QVBoxLayout(this);
+    preferencesModel = new PreferencesModel(this);
     PreferencesFilterModel* preferencesFilterModel = new PreferencesFilterModel(this);
     preferencesFilterModel->setSourceModel(preferencesModel);
     treeView->setModel(preferencesFilterModel);
-    layout()->addWidget(treeView);
     treeView->setItemDelegate(new PropertiesTreeItemDelegate());
-
     treeView->expandToDepth(0);
+    verticalLayout->addWidget(treeView);
+
+    QHBoxLayout* horizontalLayout = new QHBoxLayout();
+    verticalLayout->addItem(horizontalLayout);
+    horizontalLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Maximum));
+    QDialogButtonBox* dialogButtonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    connect(dialogButtonBox, &QDialogButtonBox::accepted, this, &PreferencesDialog::OnButtonBoxAccepted);
+    connect(dialogButtonBox, &QDialogButtonBox::rejected, this, &PreferencesDialog::OnButtonBoxRejected);
+    verticalLayout->addWidget(dialogButtonBox);
 
     PreferencesStorage::Instance()->RegisterPreferences(this);
 }
@@ -63,6 +73,19 @@ PreferencesDialog::PreferencesDialog(QWidget* parent, Qt::WindowFlags flags)
 PreferencesDialog::~PreferencesDialog()
 {
     PreferencesStorage::Instance()->UnregisterPreferences(this);
+}
+
+void PreferencesDialog::OnButtonBoxAccepted()
+{
+    //save all changed properties
+    preferencesModel->ApplyAllChangedProperties();
+    accept();
+}
+
+void PreferencesDialog::OnButtonBoxRejected()
+{
+    //do nothing
+    reject();
 }
 
 DAVA::String PreferencesDialog::GetGeometry() const
