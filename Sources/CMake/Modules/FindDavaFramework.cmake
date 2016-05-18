df875c179bcaedb3724d421137c8eb0a04450ef0
Maksim Kiselev <m_kiselev@wargaming.net>
2016-05-18 19:01:46 +0300

    merge from development
    Former-commit-id: 5d2f215adc9898e2846a03bf784bb76523b637ed

--- Sources/CMake/Modules/FindDavaFramework.cmake
+++ Sources/CMake/Modules/FindDavaFramework.cmake
@@ -1,6 +1,14 @@
+
+if( DavaFramework_FIND_COMPONENTS )
+    append_property( DAVA_COMPONENTS "DavaFramework;${DavaFramework_FIND_COMPONENTS}" )
+else()
+    append_property( DAVA_COMPONENTS  "DavaFramework;Sound"  )
+endif()
+
 if ( DAVA_FOUND )
     return ()
 endif ()
+
 set ( DAVA_FOUND 1 )
 
 get_filename_component( CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH CACHE )
