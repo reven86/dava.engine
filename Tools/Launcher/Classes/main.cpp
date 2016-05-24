44c66a604bb0505b09288e871b5dfb8a7842c33e
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-05-24 14:22:56 +0300

    [DF-10357] new update mechanism implemented
    Former-commit-id: 34b437045aa028f8f66bde267135da3274f1f357

--- Tools/Launcher/Classes/main.cpp
+++ Tools/Launcher/Classes/main.cpp
@@ -38,13 +38,16 @@ void LogMessageHandler(QtMsgType type, const QMessageLogContext&, const QString&
 
 int main(int argc, char* argv[])
 {
+#ifdef Q_OS_WIN
+    //this code is deprecated and fix update mechamism from old versions of launcher.
+    //remove this block in 2017
     QDir currentDir(".");
     if (!currentDir.exists("platforms"))
     {
         currentDir.mkpath("platforms");
         QFile::copy("qwindows.dll", "platforms/qwindows.dll");
     }
-
+#endif //windows
     QApplication a(argc, argv);
 
     qInstallMessageHandler(LogMessageHandler);
