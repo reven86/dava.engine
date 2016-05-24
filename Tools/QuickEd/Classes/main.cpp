ea43902d6b76d03fc289526c36760ffd819f4542
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-05-24 12:37:07 +0300

    [DF-9190] format fixed. 2QA: ignore it
    Former-commit-id: cb3dc31424d1b932eff9b1923339207a279df08a

--- Tools/QuickEd/Classes/main.cpp
+++ Tools/QuickEd/Classes/main.cpp
@@ -71,7 +71,6 @@ int main(int argc, char* argv[])
         Themes::InitFromQApplication();
         Q_INIT_RESOURCE(QtToolsResources);
 
-
         InitPVRTexTool();
         {
             EditorCore editorCore;
