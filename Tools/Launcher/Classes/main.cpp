79e82f57a1aac93072f06cd46c224ae877bcce07
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-05-25 16:51:11 +0300

    [DF-10357] update without infoList policy fixed, clearing platrorms folder on start
    Former-commit-id: 7efd6d6809598e606de601eba65fdd7e2a1a7364

--- Tools/Launcher/Classes/main.cpp
+++ Tools/Launcher/Classes/main.cpp
@@ -12,11 +12,18 @@ int main(int argc, char* argv[])
 #ifdef Q_OS_WIN
     //this code is deprecated and fix update mechamism from old versions of launcher.
     //remove this block in 2017
-    QDir currentDir(".");
-    if (!currentDir.exists("platforms"))
+
+    QFileInfo fi(argv[0]);
+    QDir currentDir(fi.absoluteDir());
+    QString platformsDir = "platforms";
+    QString windowsDll = "qwindows.dll";
+    currentDir.cd(platformsDir);
+    currentDir.removeRecursively();
+    currentDir.cdUp();
+    //if (!currentDir.exists(platformsDir))
     {
-        currentDir.mkpath("platforms");
-        QFile::copy("qwindows.dll", "platforms/qwindows.dll");
+        currentDir.mkpath(platformsDir);
+        QFile::copy(windowsDll, platformsDir + "/" + windowsDll);
     }
 #endif //windows
     QApplication a(argc, argv);
