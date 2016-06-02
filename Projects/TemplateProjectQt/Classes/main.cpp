b896a55378282f649fbd51abb798bba694fd2b5f
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-06-02 22:42:10 +0300

    [DF-10678] test main.cpp created
    Former-commit-id: b58654da0deb9199a2757ea369eb9d29fa01f8bb

--- Projects/TemplateProjectQt/Classes/main.cpp
+++ Projects/TemplateProjectQt/Classes/main.cpp
@@ -4,51 +4,123 @@
 
 #include "FrameworkMain.h"
 #include "MainWindow.h"
+#include "Application.h"
 
-#include "NgtTools/Application/NGTApplication.h"
 #include "NgtTools/Common/GlobalContext.h"
-#include "core_ui_framework/i_ui_framework.hpp"
-#include "core_ui_framework/i_window.hpp"
+#include <core_ui_framework/i_ui_framework.hpp>
+#include <core_ui_framework/i_window.hpp>
+#include <core_qt_common/qt_window.hpp>
+#include <core_qt_common/i_qt_framework.hpp>
+#include <core_qt_common/i_qt_view.hpp>
+#include <core_ui_framework/i_ui_application.hpp>
+#include <core_ui_framework/i_view.hpp>
+
+#include "QtTools/DavaGLWidget/DavaGLWidget.h"
 #include <QApplication>
 #include <QtCore>
 #include <QtGui>
 #include <QtWidgets>
 
-class TemplateApplication : public NGTLayer::BaseApplication
+//GL widget NGT wrapper
+class TestWidget : public IQtView
 {
 public:
-    TemplateApplication(int argc, char** argv)
-        : BaseApplication(argc, argv)
+    TestWidget()
+    {
+        button = new DavaGLWidget();
+    }
+    ~TestWidget() override = default;
+
+    const char* id() const override
+    {
+        return "pushBUtton";
+    }
+    const char* title() const override
+    {
+        return "button title";
+    }
+    const char* windowId() const override
+    {
+        return "";
+    }
+    const LayoutHint& hint() const override
+    {
+        static LayoutHint hint("default");
+        return hint;
+    };
+    void update() override
+    {
+        button->update();
+    };
+
+    void focusInEvent() override{};
+    void focusOutEvent() override{};
+
+    virtual void registerListener(IViewEventListener* listener) override
+    {
+    }
+    virtual void deregisterListener(IViewEventListener* listener) override
     {
     }
 
-protected:
-    void GetPluginsForLoad(DAVA::Vector<DAVA::WideString>& names) const override
+    QWidget* releaseView() override
     {
-        names.push_back(L"plg_reflection");
-        names.push_back(L"plg_variant");
-        names.push_back(L"plg_command_system");
-        names.push_back(L"plg_serialization");
-        names.push_back(L"plg_file_system");
-        names.push_back(L"plg_editor_interaction");
-        names.push_back(L"plg_qt_app");
-        names.push_back(L"plg_qt_common");
-    }
+        return button;
+    };
+    void retainView() override{};
+    QWidget* view() const override
+    {
+        return button;
+    };
+
+    DavaGLWidget* button;
 };
 
 int main(int argc, char* argv[])
 {
+    //helper class which connect Qt openGL and DAVA framework together
     DAVA::QtLayer qtLayer;
+
+    //DAVA Framework require to launch Core first
     DAVA::Core::Run(argc, argv);
+
     int retCode = 0;
     {
-        TemplateApplication a(argc, argv);
+        //our application wrapper. Here it only load plugins
+        Application a(argc, argv);
         a.LoadPlugins();
 
-        MainWindow w;
-        w.show();
+        //the only way to display something in a mainWindow - is to create in manually
+        std::unique_ptr<QMainWindow> mainWindow(new QMainWindow());
+        mainWindow->setCentralWidget(new QWidget());
+        //also we need to add layout manually
+        mainWindow->centralWidget()->setLayout(new QHBoxLayout());
 
-        retCode = a.StartApplication(&w);
+        //unfortunatly i need to use tabWidget and i can not just place GLWidget to the centralWidget of mainWindow
+        QTabWidget* tabWidget = new QTabWidget(mainWindow.get());
+        //also i need to register property manually, so i can not use createWindow("MainWindow.ui")
+        tabWidget->setProperty("layoutTags", "default");
+        //add tabWidget to the mainWindow layout manually. Without this GLWidget will be small as possible
+        mainWindow->centralWidget()->layout()->addWidget(tabWidget);
+
+        //just test QDockWidget
+        QDockWidget* dockWidget = new QDockWidget(mainWindow.get());
+        dockWidget->setProperty("layoutTags", "dock");
+        mainWindow->addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
+
+        IUIFramework* framework = Context::queryInterface<IUIFramework>();
+        IQtFramework* qtFramework = dynamic_cast<IQtFramework*>(framework);
+        //create mainWindow manually from Qt QMainWindow object
+        std::unique_ptr<IWindow> window(new QtWindow(*qtFramework, std::move(mainWindow)));
+        window->show();
+
+        //now create GLWidget and add it to the IUIApplication instance.
+        TestWidget testWidget;
+        IUIApplication* app = Context::queryInterface<IUIApplication>();
+        app->addWindow(*window.get());
+        app->addView(testWidget);
+        DVASSERT(app != nullptr);
+        retCode = app->startApplication();
     }
     return retCode;
 }
