c2bd0fc12698982f79dfe5106dc1c91ac1f75771
Maksim Lazarev <m_lazarev@wargaming.net>
2016-06-02 17:33:31 +0300

    DF-10696 refine CEFHelperProcess project
    Former-commit-id: 81995716cbfa27b7de8988677c01ea23f71276f6

--- Tools/CEFHelperProcess/main.cpp
+++ Tools/CEFHelperProcess/main.cpp
@@ -1,26 +1,7 @@
-#include "CEFDavaRenderApp.h"
-#include "Base/Platform.h"
-
-/*#ifdef __DAVAENGINE_WINDOWS__
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int)
-{
-    CefMainArgs mainArgs(hInstance);
-    return CefExecuteProcess(mainArgs, nullptr, nullptr);
-}
-
-#else
-
-int main(int argc, char* argv[])
-{
-    CefMainArgs mainArgs(argc, argv);
-    return CefExecuteProcess(mainArgs, nullptr, nullptr);
-}
-
-#endif*/
+#include <cef/include/cef_app.h>
 
 int main(int argc, char* argv[])
 {
     CefMainArgs mainArgs(::GetModuleHandle(nullptr));
-    return CefExecuteProcess(mainArgs, new CEFDavaRenderApp, nullptr);
+    return CefExecuteProcess(mainArgs, nullptr, nullptr);
 }
