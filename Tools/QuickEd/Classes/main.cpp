efdef7067970bd8bc88e099b5c41d3ca8e7fb5c6
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-06-07 11:47:04 +0300

    [DF-10768] documents and application moved outside
    Former-commit-id: 91e3dd6d06fed0d79d9d6387a352a973e2ba29cb

--- Tools/QuickEd/Classes/main.cpp
+++ Tools/QuickEd/Classes/main.cpp
@@ -6,43 +6,15 @@
 #include "UI/mainwindow.h"
 
 #include "EditorCore.h"
+#include "QEApplication.h"
 
 #include "Platform/Qt5/QtLayer.h"
 #include "TextureCompression/PVRConverter.h"
 #include "QtTools/Utils/Themes/Themes.h"
 #include "QtTools/Utils/MessageHandler.h"
 #include "QtTools/Utils/AssertGuard.h"
-#include "NgtTools/Application/NGTApplication.h"
 
 #include <QtGlobal>
-
-class QEApplication : public NGTLayer::BaseApplication
-{
-public:
-    QEApplication(int argc, char** argv)
-        : BaseApplication(argc, argv)
-    {
-    }
-
-protected:
-    void GetPluginsForLoad(DAVA::Vector<DAVA::WideString>& names) const override
-    {
-        names.push_back(L"plg_reflection");
-        names.push_back(L"plg_variant");
-        names.push_back(L"plg_command_system");
-        names.push_back(L"plg_serialization");
-        names.push_back(L"plg_file_system");
-        names.push_back(L"plg_editor_interaction");
-        names.push_back(L"plg_qt_app");
-        names.push_back(L"plg_qt_common");
-    }
-
-    void OnPostLoadPugins() override
-    {
-        qApp->setOrganizationName("DAVA");
-        qApp->setApplicationName("QuickEd");
-    }
-};
 
 void InitPVRTexTool()
 {
