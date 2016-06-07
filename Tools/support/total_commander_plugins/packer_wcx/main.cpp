98e0adfdedf1b45a9976bb06f74bb8928728da74
leanid <leanid.chaika@gmail.com>
2016-06-07 14:17:53 +0300

    DF-10716 fix build DLL on windows for total commander with out windows.h header - prepare to build plugin on MacOS
    Former-commit-id: 759568da16f5a3e5ab6f5a480dec16b13e8bfb24

--- Tools/support/total_commander_plugins/packer_wcx/main.cpp
+++ Tools/support/total_commander_plugins/packer_wcx/main.cpp
@@ -1,66 +1,40 @@
 #include <sstream>
 #include <algorithm>
-
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <advpub.h>
+#include <cstring>
 
 #include "pack_archive.h"
+
+using DWORD = std::uint32_t;
+#define MAX_PATH 260
 #include "wcxhead.h"
 
-namespace plugin
-{
-HANDLE hInstance = 0;
-} // end namespace plugin
+#if defined(__MINGW32__)
+#define DLL_EXPORT __declspec(dllexport)
+#define STDCALL __stdcall
+#else /* defined (_WIN32) */
+#define DLL_EXPORT
+#define STDCALL
+#endif
 
-std::vector<std::string>&
-split(const std::string& s, char delim, std::vector<std::string>& elems)
-{
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim))
-    {
-        elems.push_back(item);
-    }
-    return elems;
-}
-
-std::vector<std::string> split(const std::string& s, char delim)
-{
-    std::vector<std::string> elems;
-    split(s, delim, elems);
-    return elems;
-}
+using HANDLE = void*;
 
 extern "C"
 {
-__declspec(dllexport) HANDLE __stdcall
+DLL_EXPORT HANDLE STDCALL
 OpenArchive(tOpenArchiveData* ArchiveData);
-__declspec(dllexport) int __stdcall
+DLL_EXPORT int STDCALL
 ReadHeader(HANDLE hArcData, tHeaderData* HeaderData);
-__declspec(dllexport) int __stdcall
+DLL_EXPORT int STDCALL
 ProcessFile(HANDLE hArcData, int Operation, char* DestPath, char* DestName);
-__declspec(dllexport) int __stdcall CloseArchive(HANDLE hArcData);
-__declspec(dllexport) void __stdcall
+DLL_EXPORT int STDCALL CloseArchive(HANDLE hArcData);
+DLL_EXPORT void STDCALL
 SetChangeVolProc(HANDLE hArcData, tChangeVolProc pChangeVolProc1);
-__declspec(dllexport) void __stdcall
+DLL_EXPORT void STDCALL
 SetProcessDataProc(HANDLE hArcData, tProcessDataProc pProcessDataProc);
-__declspec(dllexport) int __stdcall GetPackerCaps();
+DLL_EXPORT int STDCALL GetPackerCaps();
 }
 
-BOOL APIENTRY DllMain(HANDLE hModule,
-                      DWORD ul_reason_for_call,
-                      LPVOID lpReserved
-                      )
-{
-    // Save HINSTANCE value
-    plugin::hInstance = static_cast<HINSTANCE>(hModule);
-    OutputDebugStringA("start DllMain PackerExtension");
-    return TRUE;
-}
-
-HANDLE __stdcall OpenArchive(tOpenArchiveData* ArchiveData)
+HANDLE STDCALL OpenArchive(tOpenArchiveData* ArchiveData)
 {
     PackArchive* archive = nullptr;
     try
@@ -69,20 +43,19 @@ HANDLE __stdcall OpenArchive(tOpenArchiveData* ArchiveData)
     }
     catch (std::exception& ex)
     {
-        OutputDebugStringA(ex.what());
         ArchiveData->OpenResult = E_BAD_ARCHIVE;
     }
     return archive;
 }
 
-int __stdcall CloseArchive(HANDLE hArcData)
+int STDCALL CloseArchive(HANDLE hArcData)
 {
     PackArchive* archive = reinterpret_cast<PackArchive*>(hArcData);
     delete archive;
     return 0;
 }
 
-int __stdcall ReadHeader(HANDLE hArcData, tHeaderData* HeaderData)
+int STDCALL ReadHeader(HANDLE hArcData, tHeaderData* HeaderData)
 {
     PackArchive* archive = reinterpret_cast<PackArchive*>(hArcData);
 
@@ -101,8 +74,8 @@ int __stdcall ReadHeader(HANDLE hArcData, tHeaderData* HeaderData)
             }
         });
 
-        HeaderData->FileAttr = FILE_SHARE_READ;
-        strcpy(HeaderData->FileName, name.c_str());
+        HeaderData->FileAttr = 1; // FILE_SHARE_READ;
+        std::strcpy(HeaderData->FileName, name.c_str());
         archive->fileIndex++;
 
         strcpy(HeaderData->ArcName, archive->arcName.c_str());
@@ -125,13 +98,11 @@ int __stdcall ReadHeader(HANDLE hArcData, tHeaderData* HeaderData)
     return 0;
 }
 
-int __stdcall ProcessFile(HANDLE hArcData, int Operation, char* DestPath,
-                          char* DestName)
+int STDCALL ProcessFile(HANDLE hArcData, int Operation, char* DestPath,
+                        char* DestName)
 {
-    // Extract a frame data from avi file
     if (PK_SKIP == Operation)
     {
-        // just check intergity skip it
     }
     else if (PK_TEST == Operation)
     {
@@ -147,19 +118,17 @@ int __stdcall ProcessFile(HANDLE hArcData, int Operation, char* DestPath,
 
             std::ofstream out(outputName, std::ios_base::binary);
             out.write(reinterpret_cast<const char*>(data.data()), data.size());
-        }
+    }
     }
     return 0;
 }
 
-void __stdcall
+void STDCALL
 SetChangeVolProc(HANDLE hArcData, tChangeVolProc pChangeVolProc1)
 {
 }
 
-int __stdcall GetPackerCaps()
+int STDCALL GetPackerCaps()
 {
-    // Accept New file, Options dialog, Multiple files, and hide (press CTRL+Down to enter avi file for reading)
-    return PK_CAPS_MULTIPLE |
-    PK_CAPS_HIDE /*|PK_CAPS_MODIFY|PK_CAPS_DELETE|PK_CAPS_OPTIONS|PK_CAPS_NEW*/;
+    return PK_CAPS_MULTIPLE | PK_CAPS_HIDE;
 }
