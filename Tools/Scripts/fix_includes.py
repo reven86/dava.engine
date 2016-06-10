cf28a183ae97ae06e3975149ebc939da5c53dd73
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-06-10 14:00:58 +0300

    [DF-10694] reviewfix
    Former-commit-id: 5d6d8596e14f29ba8f7e4d03291cc07a643b106a

--- Tools/Scripts/fix_includes.py
+++ Tools/Scripts/fix_includes.py
@@ -126,8 +126,8 @@ def process_files(arg, dirname, names):
 			
 	return
 
-pathname = os.path.dirname(sys.argv[0])         
-export_script_dir = os.path.abspath(pathname + "../../../");
+pathname = os.path.dirname(__file__);
+export_script_dir = os.path.abspath(pathname + "/../../");
 os.path.walk(export_script_dir, visit_directory, None);
 os.path.walk(export_script_dir, process_files, None);
 
