ee26ca790308e34d8f818af877b220f32f4f5ceb
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-06-23 11:58:50 +0300

    [DF-10768] code refactored
    Former-commit-id: 739af7d5d65afd1ce5ea66e2d2ad0936089b7b1c

--- Tools/QuickEd/Classes/QECommands/ChangePropertyValueCommand.h
+++ Tools/QuickEd/Classes/QECommands/ChangePropertyValueCommand.h
@@ -12,37 +12,19 @@ class AbstractProperty;
 class ChangePropertyValueCommand : public Command
 {
 public:
-    ChangePropertyValueCommand(PackageNode* _root, const DAVA::Vector<ChangePropertyAction>& propertyActions, size_t hash = 0);
-
-    ChangePropertyValueCommand(PackageNode* _root, ControlNode* _node, AbstractProperty* _property, const DAVA::VariantType& newValue, size_t hash = 0);
-    ChangePropertyValueCommand(PackageNode* _root, ControlNode* _node, AbstractProperty* _property);
-    virtual ~ChangePropertyValueCommand();
+    ChangePropertyValueCommand(PackageNode* _root, ControlNode* _node, AbstractProperty* _property, const DAVA::VariantType& newValue);
+    ~ChangePropertyValueCommand() override = default;
 
     void Redo() override;
     void Undo() override;
 
 private:
-    struct PropertyActionWithOldValue
-    {
-        PropertyActionWithOldValue(ControlNode* node_, AbstractProperty* property_, const DAVA::VariantType& oldValue_, const DAVA::VariantType& newValue_)
-            : node(node_)
-            , property(property_)
-            , oldValue(oldValue_)
-            , newValue(newValue_)
-        {
-        }
-        ControlNode* node = nullptr;
-        AbstractProperty* property = nullptr;
-        DAVA::VariantType oldValue;
-        DAVA::VariantType newValue;
-    };
-    void Init();
     DAVA::VariantType GetValueFromProperty(AbstractProperty* property);
     PackageNode* root = nullptr;
-
-    DAVA::Vector<PropertyActionWithOldValue> changedProperties;
-
-    size_t hash = 0;
+    ControlNode* node = nullptr;
+    AbstractProperty* property = nullptr;
+    DAVA::VariantType oldValue;
+    DAVA::VariantType newValue;
 };
 
 #endif // __QUICKED_CHANGE_PROPERTY_VALUE_COMMAND_H__
