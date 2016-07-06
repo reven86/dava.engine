1ea97c214a8f1363d44eb7185fdb3d7aaf364684
Ivan Radkevich <i_radkevich@wargaming.net>
2016-07-06 15:36:16 +0300

    refactor
    Former-commit-id: 02353ecab65c1f80672b6d17dc039902767b9c2d

--- Sources/Internal/Render/RHI/Common/FrameLoop.cpp
+++ Sources/Internal/Render/RHI/Common/FrameLoop.cpp
@@ -9,41 +9,38 @@ namespace FrameLoop
 static void ProcessFrame()
 {
     TRACE_BEGIN_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "exec_que_cmds");
+
     std::vector<RenderPassGLES2_t*> pass;
     std::vector<Handle> pass_h;
     unsigned frame_n = 0;
-    bool do_exit = false;
 
     //sort and test
     frameSync.Lock();
-    if (_GLES2_Frame.size())
+    DVASSERT(frames.size()); //if no frames ready should not call frame loop
+
+    //sort passes
+    for (std::vector<Handle>::iterator p = _GLES2_Frame.begin()->pass.begin(), p_end = _GLES2_Frame.begin()->pass.end(); p != p_end; ++p)
     {
-        for (std::vector<Handle>::iterator p = _GLES2_Frame.begin()->pass.begin(), p_end = _GLES2_Frame.begin()->pass.end(); p != p_end; ++p)
+        RenderPassGLES2_t* pp = RenderPassPoolGLES2::Get(*p);
+        bool do_add = true;
+
+        for (unsigned i = 0; i != pass.size(); ++i)
         {
-            RenderPassGLES2_t* pp = RenderPassPoolGLES2::Get(*p);
-            bool do_add = true;
-
-            for (unsigned i = 0; i != pass.size(); ++i)
+            if (pp->priority > pass[i]->priority)
             {
-                if (pp->priority > pass[i]->priority)
-                {
-                    pass.insert(pass.begin() + i, 1, pp);
-                    do_add = false;
-                    break;
-                }
+                pass.insert(pass.begin() + i, 1, pp);
+                do_add = false;
+                break;
             }
-
-            if (do_add)
-                pass.push_back(pp);
         }
 
-        pass_h = _GLES2_Frame.begin()->pass;
-        frame_n = _GLES2_Frame.begin()->number;
+        if (do_add)
+            pass.push_back(pp);
     }
-    else
-    {
-        do_exit = true;
-    }
+
+    pass_h = _GLES2_Frame.begin()->pass;
+    frame_n = _GLES2_Frame.begin()->number;
+
     if (_GLES2_Frame.size() && (_GLES2_Frame.begin()->sync != InvalidHandle))
     {
         SyncObjectGLES2_t* sync = SyncObjectPoolGLES2::Get(_GLES2_Frame.begin()->sync);
@@ -53,9 +50,6 @@ static void ProcessFrame()
         sync->is_used = true;
     }
     _GLES2_FrameSync.Unlock();
-
-    if (do_exit)
-        return;
 
     Trace("\n\n-------------------------------\nexecuting frame %u\n", frame_n);
     for (std::vector<RenderPassGLES2_t *>::iterator p = pass.begin(), p_end = pass.end(); p != p_end; ++p)
@@ -96,39 +90,7 @@ static void ProcessFrame()
 
     if (_GLES2_Context)
     {
-        // take screenshot, if needed
-
-        _GLES2_ScreenshotCallbackSync.Lock();
-        if (_GLES2_PendingScreenshotCallback)
-        {
-            const uint32 stride = 4 * _GLES2_DefaultFrameBuffer_Width;
-            uint8* rgba = new uint8[stride * _GLES2_DefaultFrameBuffer_Height];
-
-            GLCommand cmd[] =
-            {
-              { GLCommand::BIND_FRAMEBUFFER, { GL_FRAMEBUFFER, _GLES2_Default_FrameBuffer } },
-              { GLCommand::PIXEL_STORE_I, { GL_PACK_ALIGNMENT, 1 } },
-              { GLCommand::READ_PIXELS, { 0, 0, uint64(_GLES2_DefaultFrameBuffer_Width), uint64(_GLES2_DefaultFrameBuffer_Height), GL_RGBA, GL_UNSIGNED_BYTE, uint64(rgba) } },
-              { GLCommand::BIND_FRAMEBUFFER, { GL_FRAMEBUFFER, _GLES2_Binded_FrameBuffer } },
-            };
-
-            _ExecGL(cmd, countof(cmd));
-            for (int y = 0; y < _GLES2_DefaultFrameBuffer_Height / 2; ++y)
-            {
-                uint8* line1 = rgba + y * stride;
-                uint8* line2 = rgba + (_GLES2_DefaultFrameBuffer_Height - y - 1) * stride;
-                uint8 tmp[5 * 1024 * 4];
-
-                DVASSERT(stride <= sizeof(tmp));
-                memcpy(tmp, line1, stride);
-                memcpy(line1, line2, stride);
-                memcpy(line2, tmp, stride);
-            }
-            (*_GLES2_PendingScreenshotCallback)(_GLES2_DefaultFrameBuffer_Width, _GLES2_DefaultFrameBuffer_Height, rgba);
-            delete[] rgba;
-            _GLES2_PendingScreenshotCallback = nullptr;
-        }
-        _GLES2_ScreenshotCallbackSync.Unlock();
+        _
 
         // do swap-buffers
 
