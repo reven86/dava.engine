623dbe13e7b11d1db4cb3cddb3920eb5ea7dd52e
Ivan Radkevich <i_radkevich@wargaming.net>
2016-07-08 11:22:22 +0300

    frame loop
    Former-commit-id: 14aeaa5f7484671b7cb0711391288cf371775068

--- Sources/Internal/Render/RHI/Common/FrameLoop.cpp
+++ Sources/Internal/Render/RHI/Common/FrameLoop.cpp
@@ -10,7 +10,7 @@ static void ProcessFrame()
 {
     TRACE_BEGIN_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "exec_que_cmds");
 
-    std::vector<RenderPassGLES2_t*> pass;
+    std::vector<RenderPassBase*> pass;
     std::vector<Handle> pass_h;
     unsigned frame_n = 0;
 
@@ -19,9 +19,9 @@ static void ProcessFrame()
     DVASSERT(frames.size()); //if no frames ready should not call frame loop
 
     //sort passes
-    for (std::vector<Handle>::iterator p = _GLES2_Frame.begin()->pass.begin(), p_end = _GLES2_Frame.begin()->pass.end(); p != p_end; ++p)
+    for (std::vector<Handle>::iterator p = frames.begin()->pass.begin(), p_end = frames.begin()->pass.end(); p != p_end; ++p)
     {
-        RenderPassGLES2_t* pp = RenderPassPoolGLES2::Get(*p);
+        RenderPassBase* pp = DispatchPlatform::GetRenderPass(*p);
         bool do_add = true;
 
         for (unsigned i = 0; i != pass.size(); ++i)
@@ -38,62 +38,49 @@ static void ProcessFrame()
             pass.push_back(pp);
     }
 
-    pass_h = _GLES2_Frame.begin()->pass;
-    frame_n = _GLES2_Frame.begin()->number;
+    pass_h = frames.begin()->pass;
+    frame_n = frames.begin()->frame_n;
 
-    if (_GLES2_Frame.size() && (_GLES2_Frame.begin()->sync != InvalidHandle))
+    if (frames.begin()->sync != InvalidHandle)
     {
-        SyncObjectGLES2_t* sync = SyncObjectPoolGLES2::Get(_GLES2_Frame.begin()->sync);
+        SyncObjectBase* sync = DispatchPlatform::GetSyncObject(frames.begin()->sync);
 
         sync->frame = frame_n;
         sync->is_signaled = false;
         sync->is_used = true;
     }
-    _GLES2_FrameSync.Unlock();
+    frameSync.Unlock();
 
-    Trace("\n\n-------------------------------\nexecuting frame %u\n", frame_n);
-    for (std::vector<RenderPassGLES2_t *>::iterator p = pass.begin(), p_end = pass.end(); p != p_end; ++p)
+    //execute CB in passes
+    for (std::vector<RenderPassBase *>::iterator p = pass.begin(), p_end = pass.end(); p != p_end; ++p)
     {
-        RenderPassGLES2_t* pp = *p;
+        RenderPassBase* pp = *p;
 
         for (unsigned b = 0; b != pp->cmdBuf.size(); ++b)
         {
             Handle cb_h = pp->cmdBuf[b];
-            CommandBufferGLES2_t* cb = CommandBufferPoolGLES2::Get(cb_h);
 
             TRACE_BEGIN_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "cb::exec");
-            cb->Execute();
+            DispatchPlatform::ExecuteCommandBuffer(pp->cmdBuf[b]); // it should also set sync object for command buffer
             TRACE_END_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "cb::exec");
 
-            if (cb->sync != InvalidHandle)
-            {
-                SyncObjectGLES2_t* sync = SyncObjectPoolGLES2::Get(cb->sync);
-
-                sync->frame = frame_n;
-                sync->is_signaled = false;
-                sync->is_used = true;
-            }
-
-            CommandBufferPoolGLES2::Free(cb_h);
+            //platform or not
+            DispatchPlatform::FreeCommandBuffer(cb_h);
         }
     }
 
-    _GLES2_FrameSync.Lock();
+    frameSync.Lock();
     {
-        Trace("\n\n-------------------------------\nframe %u executed(submitted to GPU)\n", frame_n);
-        _GLES2_Frame.erase(_GLES2_Frame.begin());
+        frames.erase(frames.begin());
 
         for (std::vector<Handle>::iterator p = pass_h.begin(), p_end = pass_h.end(); p != p_end; ++p)
-            RenderPassPoolGLES2::Free(*p);
+            DispatchPlatform::FreeRenderPass(*p);
     }
-    _GLES2_FrameSync.Unlock();
+    frameSync.Unlock();
 
-    if (_GLES2_Context)
+    if (CommonDetail::renderContextReady)
     {
-        _
-
         // do swap-buffers
-
         TRACE_BEGIN_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "gl_end_frame");
 
 #if defined(__DAVAENGINE_WIN32__)
@@ -120,16 +107,6 @@ static void ProcessFrame()
 
         TRACE_END_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "gl_end_frame");
     }
-
-    // update sync-objects
-
-    _GLES2_SyncObjectsSync.Lock();
-    for (SyncObjectPoolGLES2::Iterator s = SyncObjectPoolGLES2::Begin(), s_end = SyncObjectPoolGLES2::End(); s != s_end; ++s)
-    {
-        if (s->is_used && (frame_n - s->frame >= 2))
-            s->is_signaled = true;
-    }
-    _GLES2_SyncObjectsSync.Unlock();
 
     TRACE_END_EVENT((uint32)DAVA::Thread::GetCurrentId(), "", "exec_que_cmds");
 }
