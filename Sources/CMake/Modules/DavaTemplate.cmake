a71b6b770d531e78c4649dc95d5d291b48736cae
Igor.Bratchik <i_bratchik@wargaming.net>
2016-07-19 16:24:30 +0300

    DF_10941 fix win7/win10 dpi
    Former-commit-id: 60a8ab0be4cea09bcc382e8e4a33fbeb7a1a54bb

--- Sources/CMake/Modules/DavaTemplate.cmake
+++ Sources/CMake/Modules/DavaTemplate.cmake
@@ -611,7 +611,7 @@ list ( APPEND DAVA_FOLDERS ${FILE_TREE_CHECK_FOLDERS} )
 list ( APPEND DAVA_FOLDERS ${DAVA_THIRD_PARTY_LIBRARIES_PATH} )
 
 if( WIN32 AND NOT WINDOWS_UAP )
-    set( COMMAND_PY dpiAwarness --pathVcxProj ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.vcxproj --typeAwerness HighDPIAware )
+    set( COMMAND_PY dpiAwarness --pathVcxProj ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.vcxproj --typeAwerness PerMonitorHighDPIAware )
 
 
     add_custom_target( VS_MODIFIED_${PROJECT_NAME}  ALL
