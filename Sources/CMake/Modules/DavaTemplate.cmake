1ba2aef217bbced58f1ca7c92f09846e82f28c1f
Igor.Bratchik <i_bratchik@wargaming.net>
2016-08-04 18:49:06 +0300

    DF-11211 modified generate covered scripts
    Former-commit-id: 4fb15ae95760097c96a8c542ebdbde2f46c4e9fd

--- Sources/CMake/Modules/DavaTemplate.cmake
+++ Sources/CMake/Modules/DavaTemplate.cmake
@@ -704,7 +704,6 @@ if( MACOS AND COVERAGE AND NOT DAVA_MEGASOLUTION )
                     --pathBuild     ${CMAKE_BINARY_DIR}
                     --pathReportOut ${CMAKE_BINARY_DIR}/Coverage
                     --buildConfig   $(CONFIGURATION)
-            #COMMAND open ${CMAKE_BINARY_DIR}/Coverage/index.html
         )
     
     add_dependencies( COVERAGE_${PROJECT_NAME}  ${PROJECT_NAME} )
