11cafb2f713b15ee36bab7030031912773dbd8f3
y_rakhuba <y_rakhuba@wargaming.net>
2016-08-08 13:12:06 +0300

    [DF-11108] initial commit
    Former-commit-id: b66c7010c824044b0da3e5f57ec0d0d79a584f25

---
+++ Sources/TArc/TArcCore/ContextAccessor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "DataProcessing/DataContext.h"
+#include "DataProcessing/DataWrapper.h"
+
+#include "Functional/Function.h"
+
+namespace tarc
+{
+
+class ContextAccessor
+{
+public:
+    virtual ~ContextAccessor() {}
+
+    virtual void ForEachContext(const DAVA::Function<void(DataContext&)>& functor) = 0;
+
+    // throw std::runtime_error if context with contextID doesn't exist
+    virtual DataContext& GetContext(DataContext::ContextID contextId) = 0;
+    // throw std::runtime_error if there is no active context
+    virtual DataContext& GetActiveContext() = 0;
+    virtual bool HasActiveContext() const = 0;
+
+    virtual DataWrapper CreateWrapper(const DAVA::Type* type, bool listenRecursive = false) = 0;
+    virtual DataWrapper CreateWrapper(const DataWrapper::DataAccessor& accessor, bool listenRecursive = false) = 0;
+};
+
+}
