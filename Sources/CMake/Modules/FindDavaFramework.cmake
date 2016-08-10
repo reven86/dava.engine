dc6ff2944dcbc7220efd0999f05ffe1afb31e803
Igor.Bratchik <i_bratchik@wargaming.net>
2016-08-10 20:18:06 +0300

    DF-11329 Individual dava framework for megasolution
    Former-commit-id: 011709d594d34d5e9a159e7b8545269eccb82a07

--- Sources/CMake/Modules/FindDavaFramework.cmake
+++ Sources/CMake/Modules/FindDavaFramework.cmake
@@ -1,18 +1,27 @@
 
-if( DavaFramework_FIND_COMPONENTS )
-    append_property( DAVA_COMPONENTS "DavaFramework;${DavaFramework_FIND_COMPONENTS}" )
-else()
-    append_property( DAVA_COMPONENTS  "DavaFramework;Sound"  )
+
+set( DAVA_LIBRARY    "DavaFramework" )
+
+get_property( DAVA_IDX GLOBAL PROPERTY  DAVA_IDX  )
+
+if( NOT DAVA_IDX )
+    set_property( GLOBAL PROPERTY DAVA_IDX "1" )
+
+else()    
+    math( EXPR DAVA_IDX "${DAVA_IDX} + 1" )
+    set( DAVA_LIBRARY    "DavaFramework_${DAVA_IDX}" )
+    set_property( GLOBAL PROPERTY DAVA_IDX "${DAVA_IDX}" )
 endif()
 
-if ( DAVA_FOUND )
-    return ()
-endif ()
+if( DavaFramework_FIND_COMPONENTS )
+    append_property( DAVA_COMPONENTS "${DAVA_LIBRARY};${DavaFramework_FIND_COMPONENTS}" )
+else()
+    append_property( DAVA_COMPONENTS  "ALL"  )
+endif()
 
 set ( DAVA_FOUND 1 )
 
 get_filename_component( CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH CACHE )
 add_subdirectory ( "${CURRENT_DIR}/../../Internal" ${CMAKE_CURRENT_BINARY_DIR}/DavaFramework )
-set( DAVA_LIBRARY    "DavaFramework" )
 
 
