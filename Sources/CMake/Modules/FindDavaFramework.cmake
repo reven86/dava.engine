6b73a9356f46b904a18c3cf52208c8ad8dd41b83
Igor.Bratchik <i_bratchik@wargaming.net>
2016-08-11 18:40:03 +0300

    DF-11329 fix building
    Former-commit-id: 47332989856c427c594356482bdeaf2fd12675d9

--- Sources/CMake/Modules/FindDavaFramework.cmake
+++ Sources/CMake/Modules/FindDavaFramework.cmake
@@ -1,17 +1,4 @@
-
-
 set( DAVA_LIBRARY    "DavaFramework" )
-
-get_property( DAVA_IDX GLOBAL PROPERTY  DAVA_IDX  )
-
-if( NOT DAVA_IDX )
-    set_property( GLOBAL PROPERTY DAVA_IDX "1" )
-
-else()    
-    math( EXPR DAVA_IDX "${DAVA_IDX} + 1" )
-    set( DAVA_LIBRARY    "DavaFramework_${PROJECT_NAME}" )
-    set_property( GLOBAL PROPERTY DAVA_IDX "${DAVA_IDX}" )
-endif()
 
 if( DavaFramework_FIND_COMPONENTS )
     append_property( DAVA_COMPONENTS "${DAVA_LIBRARY};${DavaFramework_FIND_COMPONENTS}" )
@@ -24,4 +11,5 @@ set ( DAVA_FOUND 1 )
 get_filename_component( CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH CACHE )
 add_subdirectory ( "${CURRENT_DIR}/../../Internal" ${CMAKE_CURRENT_BINARY_DIR}/DavaFramework )
 
-
+get_property( MODULES_NAME GLOBAL PROPERTY  MODULES_NAME )
+set( DAVA_LIBRARY ${MODULES_NAME})
