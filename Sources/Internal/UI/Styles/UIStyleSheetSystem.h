826a9e6f750ff1ffeac233262e7d8d877dda6628
Aliaksandr Valchkevich <a_valchkevich@wargaming.net>
2016-08-12 15:48:55 +0300

    DF-11368 optimizations
    Former-commit-id: 0d512d66a54fdbdd2dcd372b7765dd531a04cf30

--- Sources/Internal/UI/Styles/UIStyleSheetSystem.h
+++ Sources/Internal/UI/Styles/UIStyleSheetSystem.h
@@ -26,7 +26,12 @@ public:
     void ResetGlobalTaggedClass(const FastName& tag);
     void ClearGlobalClasses();
 
+    void ClearStats();
+    void DumpStats();
+    
 private:
+    void ProcessControl(UIControl* control, int32 distanceFromDirty);
+    
     bool StyleSheetMatchesControl(const UIStyleSheet* styleSheet, const UIControl* control);
     bool SelectorMatchesControl(const UIStyleSheetSelector& selector, const UIControl* control);
 
@@ -34,6 +39,11 @@ private:
     void DoForAllPropertyInstances(UIControl* control, uint32 propertyIndex, const CallbackType& action);
 
     UIStyleSheetClassSet globalClasses;
+    
+    uint64 statsTime = 0;
+    int32 statsProcessedControls = 0;
+    int32 statsMatches = 0;
+    int32 statsStyleSheetCount = 0;
 };
 };
 
