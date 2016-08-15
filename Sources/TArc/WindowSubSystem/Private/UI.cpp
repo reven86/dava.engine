64a53eb8f967d4c0ca131237e6538de07f245e0c
y_rakhuba <y_rakhuba@wargaming.net>
2016-08-15 11:40:24 +0300

    [DF-11108, DF-11350] Multiple targets to insert action
    Former-commit-id: 6508f5216696e6e5110b0c9fe0e831e0e842cd26

---
+++ Sources/TArc/WindowSubSystem/Private/UI.cpp
@@ -0,0 +1,60 @@
+#include "WindowSubSystem/UI.h"
+
+#include <QUrl>
+
+namespace tarc
+{
+
+WindowKey::WindowKey(DAVA::FastName appID_)
+    : appID(appID_)
+{
+}
+
+const DAVA::FastName& WindowKey::GetAppID() const
+{
+    return appID;
+}
+
+PanelKey::PanelKey(const DAVA::String& viewName_, const DockPanelInfo& info_)
+    : PanelKey(DockPanel, viewName_, info_)
+{
+}
+
+PanelKey::PanelKey(const DAVA::String& viewName_, const CentralPanelInfo& info_)
+    : PanelKey(CentralPanel, viewName_, info_)
+{
+}
+
+PanelKey::PanelKey(Type t, const DAVA::String& viewName_, const DAVA::Any& info_)
+    : type(t)
+    , viewName(viewName_)
+    , info(info_)
+{
+}
+
+const DAVA::String& PanelKey::GetViewName() const
+{
+    return viewName;
+}
+
+PanelKey::Type PanelKey::GetType() const
+{
+    return type;
+}
+
+const DAVA::Any& PanelKey::GetInfo() const
+{
+    return info;
+}
+
+ActionPlacementInfo::ActionPlacementInfo(const QUrl& url)
+{
+    AddPlacementPoint(url);
+}
+
+void ActionPlacementInfo::AddPlacementPoint(const QUrl& url)
+{
+    urls.emplace_back(url);
+}
+
+}
