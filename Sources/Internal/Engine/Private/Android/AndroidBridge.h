6a5d4b73af573e950cc7604504f49dc1d3d560a8
Maksim Lazarev <m_lazarev@wargaming.net>
2016-08-19 14:11:16 +0300

    DF-11357 cache java ClassLoader instance, introduce JNIBridge
    Former-commit-id: a078742bf41deee5154fe89cbff750b93a41766b

--- Sources/Internal/Engine/Private/Android/AndroidBridge.h
+++ Sources/Internal/Engine/Private/Android/AndroidBridge.h
@@ -1,14 +1,20 @@
-#if defined(__DAVAENGINE_COREV2__)
-
 #pragma once
 
 #include "Base/BaseTypes.h"
 
 #if defined(__DAVAENGINE_ANDROID__)
+#if defined(__DAVAENGINE_COREV2__)
 
 #include <jni.h>
+#include <android/log.h>
 
 #include "Engine/Private/EnginePrivateFwd.h"
+
+#define ANDROID_LOG_DEBUG(...) __android_log_print(ANDROID_LOG_DEBUG, "DAVA", __VA_ARGS__)
+#define ANDROID_LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "DAVA", __VA_ARGS__)
+#define ANDROID_LOG_WARN(...) __android_log_print(ANDROID_LOG_WARN, "DAVA", __VA_ARGS__)
+#define ANDROID_LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, "DAVA", __VA_ARGS__)
+#define ANDROID_LOG_FATAL(...) __android_log_print(ANDROID_LOG_FATAL, "DAVA", __VA_ARGS__)
 
 namespace DAVA
 {
@@ -18,20 +24,19 @@ struct AndroidBridge final
 {
     AndroidBridge(JavaVM* jvm);
 
+    void InitializeJNI(JNIEnv* env);
+
     static JavaVM* GetJavaVM();
     static JNIEnv* GetEnv();
     static bool AttachCurrentThreadToJavaVM();
     static bool DetachCurrentThreadFromJavaVM();
-    static bool HandleJavaException(JNIEnv* env);
+    static jclass LoadJavaClass(JNIEnv* env, const char8* className, bool throwJniException);
+    static String toString(JNIEnv* env, jobject object);
 
     static const String& GetExternalDocumentsDir();
     static const String& GetInternalDocumentsDir();
     static const String& GetApplicatiionPath();
     static const String& GetPackageName();
-
-    static String JavaStringToString(jstring string, JNIEnv* jniEnv = nullptr);
-    static WideString JavaStringToWideString(jstring string, JNIEnv* jniEnv = nullptr);
-    static jstring WideStringToJavaString(const WideString& string, JNIEnv* jniEnv = nullptr);
 
     static void AttachPlatformCore(PlatformCore* platformCore);
 
@@ -57,6 +62,9 @@ struct AndroidBridge final
     void SurfaceViewOnTouch(WindowBackend* wbackend, int32 action, int32 touchId, float32 x, float32 y);
 
     JavaVM* javaVM = nullptr;
+    jobject classLoader = nullptr; // Cached instance of ClassLoader
+    jmethodID methodClassLoader_loadClass = nullptr; // ClassLoader.loadClass method
+    jmethodID methodObject_toString = nullptr; // Object.toString method
 
     EngineBackend* engineBackend = nullptr;
     PlatformCore* core = nullptr;
@@ -70,5 +78,5 @@ struct AndroidBridge final
 } // namespace Private
 } // namespace DAVA
 
-#endif // __DAVAENGINE_ANDROID__
 #endif // __DAVAENGINE_COREV2__
+#endif // __DAVAENGINE_ANDROID__
