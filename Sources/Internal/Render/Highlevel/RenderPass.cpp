9eb0c256d8cf4c6d373b179bf435da39addd8006
Sergey Reznik <s_reznik@wargaming.net>
2016-08-24 15:53:13 +0300

    [DF-11148] Removed default multisampling in RenderPass
    Former-commit-id: 2defeeed617709e88bbad4aceab9d917bd7747a1

--- Sources/Internal/Render/Highlevel/RenderPass.cpp
+++ Sources/Internal/Render/Highlevel/RenderPass.cpp
@@ -282,8 +282,6 @@ MainForwardRenderPass::MainForwardRenderPass(const FastName& name)
     AddRenderLayer(new RenderLayer(RenderLayer::RENDER_LAYER_DEBUG_DRAW_ID, RenderLayer::LAYER_SORTING_FLAGS_DEBUG_DRAW));
 
     passConfig.priority = PRIORITY_MAIN_3D;
-    passConfig.colorBuffer[0].storeAction = rhi::STOREACTION_RESOLVE;
-    passConfig.samples = 4;
 }
 
 void MainForwardRenderPass::InitReflectionRefraction()
