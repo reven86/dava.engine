db7a0ec605bac82de837dbf6ece98443d3fdb590
k_lyashkevich <k_lyashkevich@wargaming.net>
2016-09-07 13:42:32 +0300

    [DF-11484] application start normally
    Former-commit-id: 4aa3f9f9e9f03e00a3d53f65ba85616e3def0814

--- Tools/QuickEd/Classes/main.cpp
+++ Tools/QuickEd/Classes/main.cpp
@@ -1,4 +1,8 @@
-#include "Core/Core.h"
+#include "Engine/Public/Engine.h"
+#include "Engine/Public/EngineContext.h"
+
+#include "Logger/Logger.h"
+
 #include "Particles/ParticleEmitter.h"
 #include "FileSystem/FileSystem.h"
 
@@ -22,34 +26,59 @@ void InitPVRTexTool()
     DAVA::PVRConverter::Instance()->SetPVRTexTool(pvrTexToolPath);
 }
 
-int main(int argc, char* argv[])
+int GameMain(DAVA::Vector<DAVA::String> cmdline)
 {
-    DAVA::Logger::Instance()->SetLogFilename("QuickEd.txt");
     DAVA::ParticleEmitter::FORCE_DEEP_CLONE = true;
-    const char* settingsPath = "QuickEdSettings.archive";
-    DAVA::FilePath localPrefrencesPath(DAVA::FileSystem::Instance()->GetCurrentDocumentsDirectory() + settingsPath);
-    PreferencesStorage::Instance()->SetupStoragePath(localPrefrencesPath);
-    int retCode = 0;
-    {
-        qInstallMessageHandler(DAVAMessageHandler);
-        ToolsAssetGuard::Instance()->Init();
-        QApplication a(argc, argv);
-        qApp->setOrganizationName("DAVA");
-        qApp->setApplicationName("QuickEd");
 
-        const char* settingsPath = "QuickEdSettings.archive";
-        DAVA::FilePath localPrefrencesPath(DAVA::FileSystem::Instance()->GetCurrentDocumentsDirectory() + settingsPath);
-        PreferencesStorage::Instance()->SetupStoragePath(localPrefrencesPath);
+    ToolsAssetGuard::Instance()->Init();
+
+    DAVA::Engine engine;
+
+    DAVA::KeyedArchive* appOptions = new DAVA::KeyedArchive();
+    appOptions->SetString("title", "TemplateTArc");
+    appOptions->SetInt32("fullscreen", 0);
+    appOptions->SetInt32("bpp", 32);
+    appOptions->SetInt32("rhi_threaded_frame_count", 1);
+    appOptions->SetInt32("width", 1024);
+    appOptions->SetInt32("height", 768);
+    appOptions->SetInt32("renderer", rhi::RHI_GLES2);
+
+    DAVA::Vector<DAVA::String> modules = {
+        "JobManager",
+        "NetCore",
+        "LocalizationSystem",
+        "SoundSystem",
+        "DownloadManager",
+    };
+    engine.SetOptions(appOptions);
+    engine.Init(DAVA::eEngineRunMode::GUI_EMBEDDED, modules);
+
+    DAVA::EngineContext* context = engine.GetContext();
+
+    context->logger->SetLogFilename("QuickEd.txt");
+
+    const char* settingsPath = "QuickEdSettings.archive";
+    DAVA::FilePath localPrefrencesPath(context->fileSystem->GetCurrentDocumentsDirectory() + settingsPath);
+    PreferencesStorage::Instance()->SetupStoragePath(localPrefrencesPath);
+
+    EditorCore* editorCore = nullptr;
+
+    //we need to do it after QApplication will be created
+    engine.gameLoopStarted.Connect([editorCore, &engine]() mutable {
+        qInstallMessageHandler(DAVAMessageHandler);
 
         Q_INIT_RESOURCE(QtToolsResources);
         InitPVRTexTool();
 
         Themes::InitFromQApplication();
 
-        EditorCore editorCore;
-        editorCore.Start();
+        editorCore = new EditorCore(engine);
+        editorCore->Start();
+    });
 
-        retCode = a.exec();
-    }
-    return retCode;
+    engine.gameLoopStopped.Connect([editorCore]() {
+        delete editorCore;
+    });
+
+    return engine.Run();
 }
