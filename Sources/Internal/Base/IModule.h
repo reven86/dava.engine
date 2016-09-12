1856481ddf261b5224f2a14e7ea3d724782ca0f3
Igor.Bratchik <i_bratchik@wargaming.net>
2016-09-13 02:02:33 +0300

    DF-11582 code formater
    Former-commit-id: 4e4e951c44f8d6d22747056a35e7cfc3f377014f

--- Sources/Internal/Base/IModule.h
+++ Sources/Internal/Base/IModule.h
@@ -5,25 +5,29 @@
 
 namespace DAVA
 {
-    
 class IModule
 {
 public:
-    virtual ~IModule() {}
-    
-    virtual void Init() {}
-    virtual void PostInit() {}
-    virtual void Shutdown() {}
+    virtual ~IModule()
+    {
+    }
 
-    static void ReleaseModules();    
+    virtual void Init()
+    {
+    }
+    virtual void PostInit()
+    {
+    }
+    virtual void Shutdown()
+    {
+    }
+
+    static void ReleaseModules();
     static void InitModules();
-    
+
 private:
     static Vector<IModule*> listModules;
-
-    
 };
-    
 }
 
 #endif
