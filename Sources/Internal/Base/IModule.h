199669d8a559d685f5c16feefe485c6f3036e044
Igor.Bratchik <i_bratchik@wargaming.net>
2016-09-12 19:49:21 +0300

    DF-11582 cmake module update
    Former-commit-id: 3fe1dd7d2f7797857d6b4de84ad3b7aeff509bbb

--- Sources/Internal/Base/IModule.h
+++ Sources/Internal/Base/IModule.h
@@ -1,19 +1,17 @@
 #ifndef __DAVAENGINE_IMODULE_H__
 #define __DAVAENGINE_IMODULE_H__
 
-#include "Base/BaseTypes.h"
-
 namespace DAVA
 {
     
 class IModule
 {
-    virtual void Init(Engine* engine) {};
+    virtual void Init() {};
     virtual void PostInit() {};
     virtual void Shutdown() {};
     
     static void InitModules();
-}
+};
     
 }
 
