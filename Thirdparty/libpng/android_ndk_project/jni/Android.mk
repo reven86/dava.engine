6b438531606d5c9c2e16f50ce785e1dbe06f179e
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-09-19 18:34:11 +0300

    F-11620: libpng: windows fixes
    Former-commit-id: 56a365a19e18b6421aa450add62e9153b3826c57

--- Thirdparty/libpng/android_ndk_project/jni/Android.mk
+++ Thirdparty/libpng/android_ndk_project/jni/Android.mk
@@ -6,26 +6,25 @@ LOCAL_MODULE := libpng
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../tmp/libpng_source/
 
-# set exported includes
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
-# set source files
+# SRC_PATH should be specified via command line arg, to avoid hardcoding pathes to downloaded sources
 LOCAL_SRC_FILES := \
-                  ../../tmp/libpng_source/png.c \
-                  ../../tmp/libpng_source/pngerror.c \
-                  ../../tmp/libpng_source/pngget.c \
-                  ../../tmp/libpng_source/pngmem.c \
-                  ../../tmp/libpng_source/pngpread.c \
-                  ../../tmp/libpng_source/pngread.c \
-                  ../../tmp/libpng_source/pngrio.c \
-                  ../../tmp/libpng_source/pngrtran.c \
-                  ../../tmp/libpng_source/pngrutil.c \
-                  ../../tmp/libpng_source/pngset.c \
-                  ../../tmp/libpng_source/pngtrans.c \
-                  ../../tmp/libpng_source/pngwio.c \
-                  ../../tmp/libpng_source/pngwrite.c \
-                  ../../tmp/libpng_source/pngwtran.c \
-                  ../../tmp/libpng_source/pngwutil.c \
+                  $(SRC_PATH)/png.c \
+                  $(SRC_PATH)/pngerror.c \
+                  $(SRC_PATH)/pngget.c \
+                  $(SRC_PATH)/pngmem.c \
+                  $(SRC_PATH)/pngpread.c \
+                  $(SRC_PATH)/pngread.c \
+                  $(SRC_PATH)/pngrio.c \
+                  $(SRC_PATH)/pngrtran.c \
+                  $(SRC_PATH)/pngrutil.c \
+                  $(SRC_PATH)/pngset.c \
+                  $(SRC_PATH)/pngtrans.c \
+                  $(SRC_PATH)/pngwio.c \
+                  $(SRC_PATH)/pngwrite.c \
+                  $(SRC_PATH)/pngwtran.c \
+                  $(SRC_PATH)/pngwutil.c \
 
 LOCAL_CFLAGS := -O2
 
