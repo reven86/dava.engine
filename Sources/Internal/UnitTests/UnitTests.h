40bf18ad8ae109db365566e2ed596b75318cab4e
m_kiselev <m_kiselev@wargaming.net>
2016-09-21 13:22:43 +0300

    [DF-11553] small changes
    Former-commit-id: abe32a12aa26504ba498eb828ef513a2fcd53a89

--- Sources/Internal/UnitTests/UnitTests.h
+++ Sources/Internal/UnitTests/UnitTests.h
@@ -221,7 +221,6 @@
 #define DEDUCE_COVERED_FILES_FROM_TESTCLASS() \
 BEGIN_FILES_COVERED_BY_TESTS() \
 \
-\
 DECLARE_COVERED_FILES(PrettifyTypeName(DAVA::String(typeid(*this).name())) + DAVA::String(".cpp")) \
 END_FILES_COVERED_BY_TESTS()
 
