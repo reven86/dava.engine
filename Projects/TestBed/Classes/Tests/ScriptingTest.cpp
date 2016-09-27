a2b3f0e7e8b2c5d1ce34e1cd382d17e0e525bb44
Mikhail Polubisok <m_polubisok@wargaming.net>
2016-09-27 14:33:49 +0300

    DF-11278 Add containers elements access
    Former-commit-id: 41954bc5401155a8dd5bb1612d71b7ec3a8dc495

--- Projects/TestBed/Classes/Tests/ScriptingTest.cpp
+++ Projects/TestBed/Classes/Tests/ScriptingTest.cpp
@@ -31,6 +31,7 @@ class DemoObj : public ReflectedBase
         .Field("b", &DemoObj::b)
         .Field("c", &DemoObj::c)
         .Field("d", &DemoObj::d)
+        .Field("v", &DemoObj::v)
         .End();
     }
 
@@ -39,6 +40,7 @@ public:
     String b = "String";
     Color c = Color::White;
     SubObj d;
+    Vector<int32> v;
 };
 
 ScriptingTest::ScriptingTest(GameCore* g)
@@ -53,8 +55,11 @@ void ScriptingTest::LoadResources()
     static const String demo_script = R"script(
 
 function main(context)
+    DV.Debug("GlobObj.a: "..tostring(GlobObj.a))
+    
     DV.Debug("context: "..tostring(context))
     DV.Debug("context.a: "..tostring(context.a))
+    DV.Debug("context[\"a\"]: "..tostring(context["a"]))
     DV.Debug("context.b: "..tostring(context.b))
     DV.Debug("context.c: "..tostring(context.c))
     DV.Debug("context.d: "..tostring(context.d))
@@ -64,32 +69,41 @@ function main(context)
 
     context.a = 1
     DV.Debug("context.a: "..tostring(context.a))
-
     context.b = "New String"
     DV.Debug("context.b: "..tostring(context.b))
-
     context.c = context.d.c
     DV.Debug("context.c: "..tostring(context.c))
-
     context.d.a = 2    
     DV.Debug("context.d.a: "..tostring(context.d.a))
-
     context.d.b = "New WideString"
     DV.Debug("context.d.b: "..tostring(context.d.b))
 
-
     DV.Debug("context.a + context.d.a: "..(context.a + context.d.a))
 
+    DV.Debug("context.v: "..tostring(context.v))
+    DV.Debug("Length #context.v: "..tostring(#context.v))
+    context.v[3] = 999;
+    DV.Debug("----- index for -----")
+    for i = 1, #context.v do
+        DV.Debug("  context.v["..i.."]: "..context.v[i])
+    end
 end
 
 )script";
 
     DemoObj obj;
+    obj.v.assign({ 1, 2, 3, 4, 5 });
+
     Reflection objRef = Reflection::Create(&obj).ref;
 
+    Reflection globRef = Reflection::Create(&obj.d).ref;
+
     Script s;
+    s.RegisterGlobalReflection("GlobObj", globRef);
     s.LoadString(demo_script);
     s.Run(objRef);
+
+    DAVA::Logger::Debug("CPP: obj.v[3] == %d", obj.v[3]);
 }
 
 void ScriptingTest::UnloadResources()
