f4e6fbe25701f2c60bc202ca0a0ebf85645e6800
Andrey Derkach <a_derkach2@wargaming.net>
2016-10-06 18:09:24 +0300

    [DF-11231] Analytics initial implementation
    Former-commit-id: ea3eebed505ed7f27f4898883140ccd395fc6a8d

---
+++ Sources/Internal/Analytics/Private/Analytics.cpp
@@ -0,0 +1,53 @@
+#include "Analytics/Analytics.h"
+
+namespace DAVA
+{
+namespace Analytics
+{
+void Core::Start()
+{
+    isStarted = true;
+}
+
+void Core::Stop()
+{
+    isStarted = false;
+}
+
+bool Core::IsStarted() const
+{
+    return isStarted;
+}
+
+void Core::SetConfig(const KeyedArchive& newConfig)
+{
+    config.Set(new KeyedArchive(newConfig));
+
+    for (auto& backend : backends)
+    {
+        backend.second->ConfigChanged(*config);
+    }
+}
+
+const KeyedArchive& Core::GetConfig() const
+{
+    return *config;
+}
+
+void Core::SetBackend(const String& name, std::shared_ptr<IBackend>& backend)
+{
+    backends[name] = backend;
+}
+
+bool Core::PostEvent(const EventRecord& event) const
+{
+    if (!IsStarted())
+    {
+        return false;
+    }
+
+    return false;
+}
+
+} // namespace Analytics
+} // namespace DAVA
