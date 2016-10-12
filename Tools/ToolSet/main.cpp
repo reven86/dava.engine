ec42e7088748618abe0a20f751e94c5687d67f30
Igor.Bratchik <i_bratchik@wargaming.net>
2016-10-12 17:33:05 +0300

    DF-10440  code formater
    Former-commit-id: 637011208acd4ab7bd3ccc9fd49f7b1e24d94441

--- Tools/ToolSet/main.cpp
+++ Tools/ToolSet/main.cpp
@@ -1,21 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
-int main( int argc,char** argv )
+int main(int argc, char** argv)
 {
-    std::string pathExec( argv[0] );
+    std::string pathExec(argv[0]);
     std::string pathDir, openComand;
-    
+
     const size_t last_slash_idx = pathExec.rfind('/');
-    
+
     if (std::string::npos != last_slash_idx)
     {
         pathDir = pathExec.substr(0, last_slash_idx);
     }
 
     openComand = "open " + pathDir;
-    
-    system ( openComand.c_str() );
-    
+
+    system(openComand.c_str());
+
     return 1;
 }
