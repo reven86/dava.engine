d5dbcfe6043cade47fb90c9e10334500f560f827
Sergey Zdanevich <s_zdanevich@wargaming.net>
2016-10-14 14:31:04 +0300

    [DF-11384] Window scaling for Android draft implementation
    Former-commit-id: a48a39408244c33917cd13a4cddc82303d5a7a39

--- Sources/Internal/Render/2D/Systems/VirtualCoordinatesSystem.cpp
+++ Sources/Internal/Render/2D/Systems/VirtualCoordinatesSystem.cpp
@@ -123,6 +123,8 @@ void VirtualCoordinatesSystem::ScreenSizeChanged()
         TextBlock::ScreenResolutionChanged();
     }
 
+    Logger::Info("InputOffset = (%f, %f), inputScaleFactor = %f", inputOffset.x, inputOffset.y, inputScaleFactor);
+
     RenderSystem2D::Instance()->ScreenSizeChanged();
     UIControlSystem::Instance()->ScreenSizeChanged(GetFullScreenVirtualRect());
 }
