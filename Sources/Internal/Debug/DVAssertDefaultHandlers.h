df4a633ae4f06de57dee0060dd803b1688f42041
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2016-10-14 17:25:21 +0300

    DF-11489: changed DefaultLogHandler return value + adding default assert handlers for coreV1
    Former-commit-id: a84b905c06da12153d1909b96aa390a2dff36354

--- Sources/Internal/Debug/DVAssertDefaultHandlers.h
+++ Sources/Internal/Debug/DVAssertDefaultHandlers.h
@@ -12,10 +12,13 @@ Can be used to distinguish assert messages when processing log for some reason
 */
 extern const String AssertMessageTag;
 
-/** Log assert info as an error and return FailBehaviour::Default */
+/** Log assert info as an error and return FailBehaviour::Continue */
 FailBehaviour DefaultLoggerHandler(const AssertInfo& assertInfo);
 
 /** Show message box with assert info asking user if program should be halted. Return FailBehaviour::Halt if it should */
 FailBehaviour DefaultDialogBoxHandler(const AssertInfo& assertInfo);
+
+/** Setup assert system using default handlers, based on preprocessor flags: ENABLE_ASSERT_MESSAGE & ENABLE_ASSERT_LOGGING */
+void SetupDefaultHandlers(); // TODO: Remove after CoreV2 arrives
 }
 }
