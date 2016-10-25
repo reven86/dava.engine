a85daf1bd1e0f4aeb2977434903dac2a86f439c7
Alexey Strokachuk <leronxp@gmail.com>
2016-10-26 00:19:15 +0300

    [DF-11908] modify ProjectStructure. wip
    Former-commit-id: 7a9ebca79680bda1f96c6d87083971ef29305fdf

--- Sources/Tools/QtTools/FileDialogs/FindFileDialog.h
+++ Sources/Tools/QtTools/FileDialogs/FindFileDialog.h
@@ -1,42 +1,37 @@
 #pragma once
 
-#include "Base/BaseTypes.h"
-
 #include <QDialog>
 #include <QMap>
+#include <memory>
 
 namespace Ui
 {
 class FindFileDialog;
 }
 
-namespace DAVA
-{
-class FilePath;
-}
-
 class ProjectStructure;
-class QFileSystemModel;
 class QCompleter;
 class QAction;
 
 class FindFileDialog : public QDialog
 {
 public:
-    static QString GetFilePath(const ProjectStructure* projectStructure, const DAVA::String& suffix, QWidget* parent);
+    static QString GetFilePath(const ProjectStructure* projectStructure, const QString& extension, QWidget* parent);
     static QAction* CreateFindInFilesAction(QWidget* parent);
 
 private:
-    explicit FindFileDialog(const ProjectStructure* projectStructure, const DAVA::String& suffix, QWidget* parent = nullptr);
+    explicit FindFileDialog(const ProjectStructure* projectStructure, const QString& extension, QWidget* parent = nullptr);
 
-    void Init(const DAVA::Vector<DAVA::FilePath>& files);
+    void Init(const QStringList& files);
 
     bool eventFilter(QObject* obj, QEvent* event);
 
-    std::unique_ptr<Ui::FindFileDialog> ui;
+    QString GetCommonParent(const QString& path1, const QString& path2);
 
     QString ToShortName(const QString& name) const;
     QString FromShortName(const QString& name) const;
+
+    std::unique_ptr<Ui::FindFileDialog> ui;
 
     QString prefix;
     QCompleter* completer = nullptr;
