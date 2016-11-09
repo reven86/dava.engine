c231508d7bf702d0aa9ffe987b74292aa9d25010
Maksim Lazarev <m_lazarev@wargaming.net>
2016-11-09 16:27:28 +0300

    DF-11514 win10: DeviceManager now handles display devices
    Former-commit-id: aa3104e6b2ace631caeb6e86b40972e94622d940

--- Sources/Internal/Engine/Private/UWP/DeviceManagerImplWin10.cpp
+++ Sources/Internal/Engine/Private/UWP/DeviceManagerImplWin10.cpp
@@ -5,14 +5,57 @@
 #include "Engine/DeviceManager.h"
 #include "Engine/Private/Dispatcher/MainDispatcher.h"
 
+#include "Logger/Logger.h"
+
 namespace DAVA
 {
 namespace Private
 {
+// Windows Universal Application is an "outstanding" platform where Microsoft tries to hide as much API as possible.
+// Application cannot enumerate all available displays, it only has access to current display window is on.
+// Also DisplayInformation provides display size only early on application start, further calls will return
+// only window dimension. As a result DeviceManagerImpl reads all display information one time on initialization,
+// and when display properties change (only DisplayInformation::LogicalDpi are supported) update only dpi
+// providing cached display size.
+
 DeviceManagerImpl::DeviceManagerImpl(DeviceManager* devManager, Private::MainDispatcher* dispatcher)
     : deviceManager(devManager)
     , mainDispatcher(dispatcher)
 {
+    using ::Windows::UI::ViewManagement::ApplicationView;
+    using ::Windows::Graphics::Display::DisplayInformation;
+
+    ApplicationView ^ applicationView = ApplicationView::GetForCurrentView();
+    DisplayInformation ^ displayInformation = DisplayInformation::GetForCurrentView();
+
+    ::Windows::Foundation::Rect displayRect = applicationView->VisibleBounds;
+    float32 rawPixelsPerViewPixel = static_cast<float32>(displayInformation->RawPixelsPerViewPixel);
+
+    displayInfo.systemId = 0;
+    displayInfo.name = "display"; // Any name as DisplayInformation does not provide display name
+    displayInfo.dpiX = displayInformation->LogicalDpi;
+    displayInfo.dpiY = displayInformation->LogicalDpi;
+    displayInfo.rect.x = displayRect.X;
+    displayInfo.rect.y = displayRect.Y;
+    displayInfo.rect.dx = displayRect.Width * rawPixelsPerViewPixel;
+    displayInfo.rect.dy = displayRect.Height * rawPixelsPerViewPixel;
+
+    deviceManager->displays.push_back(displayInfo);
+}
+
+void DeviceManagerImpl::UpdateDisplayConfig()
+{
+    using ::Windows::UI::ViewManagement::ApplicationView;
+    using ::Windows::Graphics::Display::DisplayInformation;
+
+    DisplayInformation ^ displayInformation = DisplayInformation::GetForCurrentView();
+
+    DisplayInfo* di = new DisplayInfo[1];
+    *di = displayInfo;
+
+    di->dpiX = displayInformation->LogicalDpi;
+    di->dpiY = displayInformation->LogicalDpi;
+    mainDispatcher->PostEvent(MainDispatcherEvent::CreateDisplayConfigChangedEvent(di, 1));
 }
 
 } // namespace Private
