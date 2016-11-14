87a5e0d2968b81466ea9cf74dd98d835e970da9d
leanid <leanid.chaika@gmail.com>
2016-11-14 18:49:17 +0300

    DF-11992 implement and add test to check if full path inside added resource dir
    Former-commit-id: a52dac9c34f30a28f8d2da78e2326fb119133749

--- Projects/UnitTests/Classes/Tests/FileSystemTest.cpp
+++ Projects/UnitTests/Classes/Tests/FileSystemTest.cpp
@@ -362,5 +362,48 @@ DAVA_TESTCLASS (FileSystemTest)
         FileSystem::Instance()->DeleteFile(textFilePath);
         FileSystem::Instance()->DeleteFile(textFilePath2);
     }
+
+    DAVA_TEST (GetFrameworkPathTest)
+    {
+        FileSystem* fs = FileSystem::Instance();
+
+        const char* tmp = std::getenv("TEMP");
+
+        if (tmp != nullptr)
+        {
+            String tmps = String(tmp);
+            std::replace(begin(tmps), end(tmps), '\\', '/');
+
+            const String dataDir = tmps + "/data/";
+            if (fs->CreateDirectory(dataDir, true) != FileSystem::DIRECTORY_CANT_CREATE)
+            {
+                const String innerDir = tmps + "/inner_data/";
+                if (fs->CreateDirectory(innerDir, true != FileSystem::DIRECTORY_CANT_CREATE))
+                {
+                    FilePath::AddResourcesFolder(dataDir);
+                    FilePath::AddResourcesFolder(innerDir);
+
+                    String filePath = innerDir + String("file.yaml");
+                    File* f = File::Create(filePath, File::CREATE | File::WRITE);
+
+                    if (f != nullptr)
+                    {
+                        SafeRelease(f);
+                    }
+
+                    String fullPath = tmps + "/inner_data/file.yaml";
+
+                    FilePath absPath(fullPath);
+
+                    FilePath resPath = absPath.GetFrameworkPath();
+                    TEST_VERIFY(resPath.GetStringValue() == "~res:/file.yaml");
+
+                    fs->DeleteDirectoryFiles(innerDir, true);
+                    fs->DeleteDirectory(innerDir);
+                }
+                fs->DeleteDirectory(dataDir);
+            }
+        }
+    }
 }
 ;
