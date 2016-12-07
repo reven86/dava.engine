9f745c57572539d937623f205a506ae4ca85cf91
Sergey Reznik <s_reznik@wargaming.net>
2016-12-07 16:11:21 +0300

    Mitsuba exporter code cleaned up
    Former-commit-id: 0adf3ec28482991f61c4e2878295539a7ad3fc18

--- Tools/ResourceEditor/Classes/Export/Mitsuba/MitsubaExporter.h
+++ Tools/ResourceEditor/Classes/Export/Mitsuba/MitsubaExporter.h
@@ -3,17 +3,8 @@
 #include "TArc/Core/ClientModule.h"
 #include "TArc/Utils/QtConnections.h"
 
-namespace DAVA
-{
-class Scene;
-class FilePath;
-}
-
 class MitsubaExporter : public DAVA::TArc::ClientModule
 {
-public:
-    //bool Export(DAVA::Scene* scene, const DAVA::FilePath& toFile);
-
 protected:
     void PostInit() override;
     void Export();
