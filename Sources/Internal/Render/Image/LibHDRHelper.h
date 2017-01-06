9824aa0e5e55c8e4f96fab96156ff423c77d6a6e
Sergey Reznik <s_reznik@wargaming.net>
2017-01-06 17:13:42 +0300

    [DF-12347] HDR format support added
    Former-commit-id: 10ed5d0656f2b2fdc29a12f40e440172620ee492

---
+++ Sources/Internal/Render/Image/LibHDRHelper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Render/Image/Image.h"
+#include "Render/Image/ImageFormatInterface.h"
+#include <memory>
+
+namespace DAVA
+{
+class LibHDRHelper : public ImageFormatInterface
+{
+public:
+    LibHDRHelper();
+
+    ImageInfo GetImageInfo(File* infile) const override;
+
+    eErrorCode ReadFile(File* infile, Vector<Image*>& imageSet, const ImageSystem::LoadingParams& loadingParams) const override;
+    eErrorCode WriteFile(const FilePath& fileName, const Vector<Image*>& imageSet, PixelFormat compressionFormat, ImageQuality quality) const override;
+    eErrorCode WriteFileAsCubeMap(const FilePath& fileName, const Vector<Vector<Image*>>& imageSet, PixelFormat compressionFormat, ImageQuality quality) const override;
+};
+}
