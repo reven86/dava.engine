2be71ddc920124a0342737e7611d58825d168a00
unknown <a_valchkevich@BY1-WW-958.corp.wargaming.local>
2017-01-06 13:47:05 +0300

    DF-12294 merge fix
    Former-commit-id: b9d0e9651f68aecff06f6f14364dc560c314f551

--- Sources/Internal/UI/Styles/UIStyleSheetStructs.h
+++ Sources/Internal/UI/Styles/UIStyleSheetStructs.h
@@ -4,6 +4,7 @@
 #include "Base/IntrospectionBase.h"
 #include "Base/BaseTypes.h"
 #include "Base/FastName.h"
+#include "FileSystem/FilePath.h"
 #include "FileSystem/VariantType.h"
 #include "Animation/Interpolation.h"
 
