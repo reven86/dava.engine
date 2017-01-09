7d2611acbe523a05e26eb2daa38d8bf1d2f529ca
Sergey Reznik <s_reznik@wargaming.net>
2017-01-09 14:25:21 +0300

    [DF-12347] Fixes in HDR loader, unit test added
    Former-commit-id: 9379d49842daa747efda3dcef67d6ec0d22b4ebd

--- Sources/Internal/Render/Image/LibHDRHelper.h
+++ Sources/Internal/Render/Image/LibHDRHelper.h
@@ -16,5 +16,8 @@ public:
     eErrorCode ReadFile(File* infile, Vector<Image*>& imageSet, const ImageSystem::LoadingParams& loadingParams) const override;
     eErrorCode WriteFile(const FilePath& fileName, const Vector<Image*>& imageSet, PixelFormat compressionFormat, ImageQuality quality) const override;
     eErrorCode WriteFileAsCubeMap(const FilePath& fileName, const Vector<Vector<Image*>>& imageSet, PixelFormat compressionFormat, ImageQuality quality) const override;
+
+private:
+    bool CanProcessFileInternal(File* file) const override;
 };
 }
