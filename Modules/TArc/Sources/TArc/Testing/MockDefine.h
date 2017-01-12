bbfac3af9e570184b8cf11b6bf4b4dab1cc213a7
klesch <victor.kleschenko@gmail.com>
2017-01-12 22:54:55 +0300

    [DF-185] Removed extra semicolon
    Former-commit-id: 89513a03b2d088b1a5d8f466bec13cfca541f457

--- Modules/TArc/Sources/TArc/Testing/MockDefine.h
+++ Modules/TArc/Sources/TArc/Testing/MockDefine.h
@@ -5,22 +5,22 @@
 #if __clang__
 
     #define MOCK_METHOD0_VIRTUAL(m, arg) \
-        _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic push") \
     _Pragma("clang diagnostic ignored \"-Winconsistent-missing-override\"") \
     MOCK_METHOD0(m, arg); \
-        _Pragma("clang diagnostic pop") \
+    _Pragma("clang diagnostic pop") \
 
     #define MOCK_METHOD1_VIRTUAL(m, arg) \
-        _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic push") \
     _Pragma("clang diagnostic ignored \"-Winconsistent-missing-override\"") \
     MOCK_METHOD1(m, arg); \
-        _Pragma("clang diagnostic pop") \
+    _Pragma("clang diagnostic pop") \
 
     #define MOCK_METHOD2_VIRTUAL(m, arg) \
-        _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic push") \
     _Pragma("clang diagnostic ignored \"-Winconsistent-missing-override\"") \
     MOCK_METHOD2(m, arg); \
-        _Pragma("clang diagnostic pop") \
+    _Pragma("clang diagnostic pop") \
 
 #else
     #define MOCK_METHOD0_VIRTUAL MOCK_METHOD0
