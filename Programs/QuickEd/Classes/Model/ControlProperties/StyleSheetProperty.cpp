1ada36e2237754c6fe56cfe67832831c48990285
dima <dima@Dima-MacBook-Pro.local>
2017-01-15 12:34:42 +0300

    [DF-11834] quicked fixes
    Former-commit-id: 2a85d623b3a60288c30cdecd7d5bf549b275a337

--- Programs/QuickEd/Classes/Model/ControlProperties/StyleSheetProperty.cpp
+++ Programs/QuickEd/Classes/Model/ControlProperties/StyleSheetProperty.cpp
@@ -7,40 +7,56 @@
 #include "Model/PackageHierarchy/StyleSheetNode.h"
 #include "UI/Styles/UIStyleSheet.h"
 
+#include "Reflection/ReflectionRegistrator.h"
+
 using namespace DAVA;
 
 namespace SStyleSheetProperty
 {
-static VariantType::eVariantType GetValueType(uint32 propertyIndex)
+static const Type *GetValueType(uint32 propertyIndex)
 {
     const UIStyleSheetPropertyDescriptor& descr = UIStyleSheetPropertyDataBase::Instance()->GetStyleSheetPropertyByIndex(propertyIndex);
-    return VariantType::TypeFromMetaInfo(descr.memberInfo->Type());
+    return descr.field_s->reflectedType->GetType();
 }
-static const InspDesc* GetInspDesc(uint32 propertyIndex)
+    
+static const ReflectedStructure::Field* GetField(uint32 propertyIndex)
 {
     const UIStyleSheetPropertyDescriptor& descr = UIStyleSheetPropertyDataBase::Instance()->GetStyleSheetPropertyByIndex(propertyIndex);
-    return &descr.memberInfo->Desc();
+    return descr.field_s;
 }
 }
 
+DAVA_REFLECTION_IMPL(StyleSheetProperty)
+{
+    ReflectionRegistrator<StyleSheetProperty>::Begin()
+    .Field("transition", &StyleSheetProperty::HasTransition, &StyleSheetProperty::SetTransition)
+    .Field("transitionTime", &StyleSheetProperty::GetTransitionTime, &StyleSheetProperty::SetTransitionTime)
+    .Field("transitionFunction", &StyleSheetProperty::GetTransitionFunction, &StyleSheetProperty::SetTransitionFunction)
+    [
+     EnumMeta::Create<Interpolation::FuncType>(EnumMeta::EM_NOCAST)
+     ]
+
+    .End();
+}
+
 StyleSheetProperty::StyleSheetProperty(const DAVA::UIStyleSheetProperty& aProperty)
-    : ValueProperty("prop", SStyleSheetProperty::GetValueType(aProperty.propertyIndex), false, SStyleSheetProperty::GetInspDesc(aProperty.propertyIndex))
+    : ValueProperty("prop", SStyleSheetProperty::GetValueType(aProperty.propertyIndex), false, SStyleSheetProperty::GetField(aProperty.propertyIndex))
     , property(aProperty)
 {
     const UIStyleSheetPropertyDescriptor& descr = UIStyleSheetPropertyDataBase::Instance()->GetStyleSheetPropertyByIndex(property.propertyIndex);
     SetName(String(descr.GetFullName().c_str()));
     SetOverridden(true);
 
-    RefPtr<VariantTypeProperty> valueProp(new VariantTypeProperty("Value", &descr.memberInfo->Desc(), property.value));
+    RefPtr<VariantTypeProperty> valueProp(new VariantTypeProperty("Value", descr.field_s, property.value));
     valueProp->SetValue(property.value);
     valueProp->SetParent(this);
     AddSubValueProperty(valueProp.Get());
 
-    for (int32 i = 0; i < TypeInfo()->MembersCount(); i++)
+    const ReflectedType *reflectedType = GetReflectedType();
+    for (const std::unique_ptr<ReflectedStructure::Field> &field : reflectedType->GetStrucutre()->fields)
     {
-        const InspMember* member = TypeInfo()->Member(i);
-        RefPtr<IntrospectionProperty> inspProp(new IntrospectionProperty(this, member, nullptr, CT_COPY));
-        inspProp->SetValue(member->Value(this));
+        RefPtr<IntrospectionProperty> inspProp(new IntrospectionProperty(this, field.get(), nullptr, CT_COPY));
+        inspProp->SetValue(field->valueWrapper->GetValue(this));
         inspProp->SetParent(this);
         inspProp->DisableResetFeature();
         AddSubValueProperty(inspProp.Get());
@@ -79,16 +95,6 @@ Interpolation::FuncType StyleSheetProperty::GetTransitionFunction() const
 void StyleSheetProperty::SetTransitionFunction(Interpolation::FuncType type)
 {
     property.transitionFunction = type;
-}
-
-DAVA::int32 StyleSheetProperty::GetTransitionFunctionAsInt() const
-{
-    return GetTransitionFunction();
-}
-
-void StyleSheetProperty::SetTransitionFunctionFromInt(DAVA::int32 type)
-{
-    SetTransitionFunction(static_cast<Interpolation::FuncType>(type));
 }
 
 float32 StyleSheetProperty::GetTransitionTime() const
