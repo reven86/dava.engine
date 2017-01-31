2ddf3832eafece7b4038e769c27aad18468937ab
Maksim Lazarev <m_lazarev@wargaming.net>
2017-01-31 11:56:17 +0300

    DF-12174 android: list missing source code folders in Android.mk
    Former-commit-id: 7285745c883fed016752d76f3bb56e3b920c0359

--- Sources/Internal/Android.mk
+++ Sources/Internal/Android.mk
@@ -344,6 +344,7 @@ LOCAL_SRC_FILES := \
                      $(wildcard $(LOCAL_PATH)/Analytics/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Analytics/Private/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Animation/*.cpp) \
+                     $(wildcard $(LOCAL_PATH)/AnyCasts/Private/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Autotesting/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Autotesting/*.c) \
                      $(wildcard $(LOCAL_PATH)/Base/*.cpp) \
@@ -384,6 +385,8 @@ LOCAL_SRC_FILES := \
                      $(wildcard $(LOCAL_PATH)/Platform/TemplateAndroid/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Platform/TemplateAndroid/BacktraceAndroid/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Platform/TemplateAndroid/ExternC/*.cpp) \
+                     $(wildcard $(LOCAL_PATH)/PluginManager/*.cpp) \
+                     $(wildcard $(LOCAL_PATH)/PluginManager/Private/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Reflection/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Reflection/Private/*.cpp) \
                      $(wildcard $(LOCAL_PATH)/Reflection/Private/Wrappers/*.cpp))
