97776f68f9643e11cbed200d865945aa345f773a
Aliaksandr Valchkevich <a_valchkevich@wargaming.net>
2017-02-07 14:07:57 +0300

    DF-12772 code style fixes
    Former-commit-id: 3dfb10007ffbc6721eec094ff3c6e6f232d15a8c

--- Sources/Internal/UI/Sound/UISoundSystem.cpp
+++ Sources/Internal/UI/Sound/UISoundSystem.cpp
@@ -10,7 +10,7 @@
 namespace DAVA
 {
 const FastName UISoundSystem::SOUND_PARAM_PAN("pan");
-const FastName UISoundSystem::UI_SOUND_GROUP("UI_SOUND_GROUP");
+const FastName UISoundSystem::SOUND_GROUP("UI_SOUND_GROUP");
 
 UISoundSystem::UISoundSystem()
 {
@@ -21,132 +21,132 @@ UISoundSystem::~UISoundSystem()
 }
 
 void UISoundSystem::ProcessControlEvent(int32 eventType, const UIEvent* uiEvent, UIControl* control)
+{
+    UISoundComponent* soundComponent = control->GetComponent<UISoundComponent>();
+
+    if (soundComponent)
     {
-        UISoundComponent* soundComponent = control->GetComponent<UISoundComponent>();
-
-        if (soundComponent)
+        if (!ShouldSkipEvent(eventType, uiEvent, control))
         {
-            if (!ShouldSkipEvent(eventType, uiEvent, control))
-            {
-                const FastName& soundEventName = soundComponent->GetSoundEventName(static_cast<UIControl::eEventType>(eventType));
+            const FastName& soundEventName = soundComponent->GetSoundEventName(static_cast<UIControl::eEventType>(eventType));
 
-                if (soundEventName.IsValid())
-                {
-                    TriggerEvent(soundEventName, uiEvent, control);
-                }
+            if (soundEventName.IsValid())
+            {
+                TriggerEvent(soundEventName, uiEvent, control);
             }
         }
     }
+}
 
-    void UISoundSystem::FreeEvents()
+void UISoundSystem::FreeEvents()
+{
+    soundEvents.clear();
+}
+
+void UISoundSystem::SetGlobalParameter(const DAVA::FastName& parameter, float value)
+{
+    globalParameters[parameter] = value;
+}
+
+void UISoundSystem::TriggerEvent(const FastName& eventName, const UIEvent* uiEvent, UIControl* control)
+{
+    RefPtr<SoundEvent> event = GetEvent(eventName);
+
+    if (event->IsActive())
     {
-        soundEvents.clear();
+        event->Stop();
     }
 
-    void UISoundSystem::SetGlobalParameter(const DAVA::FastName& parameter, float value)
-    {
-        globalParameters[parameter] = value;
-    }
+    SetupEventPan(event.Get(), uiEvent, control);
+    SetupEventGlobalParameters(event.Get());
 
-    void UISoundSystem::TriggerEvent(const FastName& eventName, const UIEvent* uiEvent, UIControl* control)
-    {
-        RefPtr<SoundEvent> event = GetEvent(eventName);
+    event->Trigger();
+}
 
-        if (event->IsActive())
+void UISoundSystem::SetupEventPan(SoundEvent* event, const UIEvent* uiEvent, UIControl* control)
+{
+    if (event->IsParameterExists(SOUND_PARAM_PAN))
+    {
+        const float32 PAN_DEFAULT = 0.0f;
+        const float32 PAN_LEFT = -1.0f;
+        const float32 PAN_RIGHT = 1.0f;
+        const float32 PAN_RANGE = PAN_RIGHT - PAN_LEFT;
+
+        float32 pan = PAN_DEFAULT;
+        float32 pointX = 0.0f;
+        if (uiEvent)
         {
-            event->Stop();
-        }
-
-        SetupEventPan(event.Get(), uiEvent, control);
-        SetupEventGlobalParameters(event.Get());
-
-        event->Trigger();
-    }
-
-    void UISoundSystem::SetupEventPan(SoundEvent* event, const UIEvent* uiEvent, UIControl* control)
-    {
-        if (event->IsParameterExists(SOUND_PARAM_PAN))
-        {
-            const float32 PAN_DEFAULT = 0.0f;
-            const float32 PAN_LEFT = -1.0f;
-            const float32 PAN_RIGHT = 1.0f;
-            const float32 PAN_RANGE = PAN_RIGHT - PAN_LEFT;
-
-            float32 pan = PAN_DEFAULT;
-            float32 pointX = 0.0f;
-            if (uiEvent)
-            {
-                pointX = uiEvent->point.x;
-            }
-            else
-            {
-                pointX = control->GetAbsoluteRect().GetCenter().x;
-            }
-
-#if defined(__DAVAENGINE_COREV2__)
-            pan = PAN_LEFT + PAN_RANGE * (pointX / GetPrimaryWindow()->GetVirtualSize().dx);
-#else
-            pan = PAN_LEFT + PAN_RANGE * (pointX / VirtualCoordinatesSystem::Instance()->GetVirtualScreenSize().dx);
-#endif
-
-            event->SetParameterValue(SOUND_PARAM_PAN, pan);
-        }
-    }
-
-    void UISoundSystem::SetupEventGlobalParameters(SoundEvent* event)
-    {
-        for (const GlobalParameterMap::value_type& param : globalParameters)
-        {
-            if (event->IsParameterExists(param.first))
-            {
-                event->SetParameterValue(param.first, param.second);
-            }
-        }
-    }
-
-    RefPtr<SoundEvent> UISoundSystem::GetEvent(const FastName& eventName)
-    {
-        SoundEventMap::iterator eventIter = soundEvents.find(eventName);
-        if (eventIter == soundEvents.end())
-        {
-            RefPtr<SoundEvent> event(SoundSystem::Instance()->CreateSoundEventByID(eventName, UI_SOUND_GROUP));
-
-            soundEvents[eventName] = event;
-
-            return event;
+            pointX = uiEvent->point.x;
         }
         else
         {
-            return eventIter->second;
+            pointX = control->GetAbsoluteRect().GetCenter().x;
+        }
+
+#if defined(__DAVAENGINE_COREV2__)
+        pan = PAN_LEFT + PAN_RANGE * (pointX / GetPrimaryWindow()->GetVirtualSize().dx);
+#else
+        pan = PAN_LEFT + PAN_RANGE * (pointX / VirtualCoordinatesSystem::Instance()->GetVirtualScreenSize().dx);
+#endif
+
+        event->SetParameterValue(SOUND_PARAM_PAN, pan);
+    }
+}
+
+void UISoundSystem::SetupEventGlobalParameters(SoundEvent* event)
+{
+    for (const GlobalParameterMap::value_type& param : globalParameters)
+    {
+        if (event->IsParameterExists(param.first))
+        {
+            event->SetParameterValue(param.first, param.second);
         }
     }
+}
 
-    bool UISoundSystem::ShouldSkipEvent(int32 eventType, const UIEvent* uiEvent, UIControl* control)
+RefPtr<SoundEvent> UISoundSystem::GetEvent(const FastName& eventName)
+{
+    SoundEventMap::iterator eventIter = soundEvents.find(eventName);
+    if (eventIter == soundEvents.end())
     {
-        if (eventType == UIControl::EVENT_VALUE_CHANGED)
+        RefPtr<SoundEvent> event(SoundSystem::Instance()->CreateSoundEventByID(eventName, SOUND_GROUP));
+
+        soundEvents[eventName] = event;
+
+        return event;
+    }
+    else
+    {
+        return eventIter->second;
+    }
+}
+
+bool UISoundSystem::ShouldSkipEvent(int32 eventType, const UIEvent* uiEvent, UIControl* control)
+{
+    if (eventType == UIControl::EVENT_VALUE_CHANGED)
+    {
+        UISoundValueFilterComponent* valueFilterComponent = control->GetComponent<UISoundValueFilterComponent>();
+        if (valueFilterComponent)
         {
-            UISoundValueFilterComponent* valueFilterComponent = control->GetComponent<UISoundValueFilterComponent>();
-            if (valueFilterComponent)
+            // we need some kind of generic way to get control value, for now branch here
+            UISlider* slider = dynamic_cast<UISlider*>(control);
+
+            if (slider)
             {
-                // we need some kind of generic way to get control value, for now branch here
-                UISlider* slider = dynamic_cast<UISlider*>(control);
+                const float32 value = slider->GetValue();
 
-                if (slider)
+                const int32 normalizedValue = (int32)(slider->GetValue() / valueFilterComponent->GetStep());
+
+                if (valueFilterComponent->normalizedValue == normalizedValue)
                 {
-                    const float32 value = slider->GetValue();
-
-                    const int32 normalizedValue = (int32)(slider->GetValue() / valueFilterComponent->GetStep());
-
-                    if (valueFilterComponent->normalizedValue == normalizedValue)
-                    {
-                        return true;
-                    }
-
-                    valueFilterComponent->normalizedValue = normalizedValue;
+                    return true;
                 }
+
+                valueFilterComponent->normalizedValue = normalizedValue;
             }
         }
-
-        return false;
     }
+
+    return false;
+}
 }
