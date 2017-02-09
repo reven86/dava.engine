590f0607ce65971ba74248c973277e5146b57674
unknown <a_valchkevich@BY1-WW-958.corp.wargaming.local>
2017-02-09 10:53:36 +0300

    DF-12833 finder PackageNode interface
    Former-commit-id: cd8c03f5969b85e4003f2eeb1a5fe6beeeb9a2a5

--- Programs/QuickEd/Classes/UI/Find/Finder.h
+++ Programs/QuickEd/Classes/UI/Find/Finder.h
@@ -6,15 +6,18 @@
 
 #include <QObject>
 
+class PackageNode;
+
 class Finder : public QObject
 {
     Q_OBJECT
 
 public:
-    Finder(const QStringList& files_, std::unique_ptr<FindFilter>&& filter, const DAVA::Map<DAVA::String, DAVA::Set<DAVA::FastName>>* prototypes);
+    Finder(std::unique_ptr<FindFilter>&& filter, const DAVA::Map<DAVA::String, DAVA::Set<DAVA::FastName>>* prototypes);
     ~Finder() override;
 
-    void Process();
+    void Process(const QStringList& files);
+    void Process(const PackageNode* package);
     void Stop();
 
 signals:
@@ -23,12 +26,12 @@ signals:
     void ProgressChanged(int filesProcessed, int totalFiles);
 
 private:
-    void CollectControls(const DAVA::FilePath& path, const ControlInformation* control, bool inPrototypeSection);
+    static void CollectControls(FindItem& currentItem, const FindFilter& filter, const ControlInformation* control);
 
-    QStringList files;
+    void ProcessPackage(FindItem& currentItem, const PackageInformation* package);
+
     std::unique_ptr<FindFilter> filter;
     const DAVA::Map<DAVA::String, DAVA::Set<DAVA::FastName>>* prototypes;
-    FindItem currentItem;
     bool cancelling = false;
     QMutex mutex;
 };
