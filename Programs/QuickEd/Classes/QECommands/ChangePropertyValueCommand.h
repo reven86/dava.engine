8da1dbaa04556dde5f717ce0b448cd1f0bc1e84b
dima <dima@dima-mbp.corp.wargaming.local>
2017-02-13 15:38:59 +0300

    [DF-11834] review fixes
    Former-commit-id: 400986d8604ead76c34d8e0797b3dec293480f48

--- Programs/QuickEd/Classes/QECommands/ChangePropertyValueCommand.h
+++ Programs/QuickEd/Classes/QECommands/ChangePropertyValueCommand.h
@@ -1,10 +1,11 @@
 #ifndef __QUICKED_CHANGE_PROPERTY_VALUE_COMMAND_H__
 #define __QUICKED_CHANGE_PROPERTY_VALUE_COMMAND_H__
 
-#include "Base/Any.h"
 #include "EditorSystems/EditorSystemsManager.h"
 
 #include "Command/Command.h"
+
+#include <Base/Any.h>
 
 class PackageNode;
 class ControlNode;
@@ -13,7 +14,7 @@ class AbstractProperty;
 class ChangePropertyValueCommand : public DAVA::Command
 {
 public:
-    ChangePropertyValueCommand(PackageNode* _root, ControlNode* _node, AbstractProperty* _property, const DAVA::Any& _newValue);
+    ChangePropertyValueCommand(PackageNode* root, ControlNode* node, AbstractProperty* property, const DAVA::Any& newValue);
     ~ChangePropertyValueCommand() override = default;
 
     void Redo() override;
