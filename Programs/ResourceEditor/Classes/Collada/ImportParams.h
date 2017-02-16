4fcf300e5e3db2313cd4886d4effb0af64424696
y_rakhuba <y_rakhuba@wargaming.net>
2017-02-16 12:57:37 +0300

    Dae reimport.
    Former-commit-id: 8a7f812cec0c533a7ee2503064df943c735108d1

---
+++ Programs/ResourceEditor/Classes/Collada/ImportParams.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Base/BaseTypes.h>
+#include <Base/FastName.h>
+#include <Base/RefPtr.h>
+#include <Scene3D/Scene.h>
+
+namespace DAVA
+{
+class CustomPropertiesComponent;
+} // namespace DAVA
+
+namespace DAEConverter
+{
+struct ImportParams
+{
+    ImportParams() = default;
+    ImportParams(const ImportParams& other) = delete;
+    ImportParams& operator=(const ImportParams& other) = delete;
+    ~ImportParams();
+
+    DAVA::Map<DAVA::FastName, DAVA::Component*> customPropertiesMap;
+    DAVA::Map<DAVA::FastName, DAVA::Component*> lodComponentsMap;
+    DAVA::Map<DAVA::FastName, DAVA::RefPtr<DAVA::KeyedArchive>> materialsMap;
+};
+
+void RestoreCustomProperties(DAVA::RefPtr<DAVA::Scene> scene, const DAVA::FilePath& scenePath, ImportParams* params);
+void AccumulateImportParams(DAVA::RefPtr<DAVA::Scene> scene, const DAVA::FilePath& scenePath, ImportParams* params);
+
+} // namespace DAEConverter
