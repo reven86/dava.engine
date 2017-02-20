7fc5aebdb1bb357aaedc1cca671e46a03026412f
Mikhail Polubisok <m_polubisok@wargaming.net>
2017-02-20 15:51:37 +0300

    DF-11834 Fix review issue
    Former-commit-id: 58de9f8e178deff92b72d65216b1f5931166be86

--- Sources/Internal/UI/UIWebView.cpp
+++ Sources/Internal/UI/UIWebView.cpp
@@ -27,10 +27,7 @@ DAVA_VIRTUAL_REFLECTION_IMPL(UIWebView)
     ReflectionRegistrator<UIWebView>::Begin()
     .ConstructorByPointer()
     .DestructorByPointer([](UIWebView* o) { o->Release(); })
-    .Field("dataDetectorTypes", &UIWebView::GetDataDetectorTypes, &UIWebView::SetDataDetectorTypes) // TODO: make enum
-    [
-    M::EnumT<eDataDetectorType>()
-    ]
+    .Field("dataDetectorTypes", &UIWebView::GetDataDetectorTypes, &UIWebView::SetDataDetectorTypes)[M::EnumT<eDataDetectorType>()]
     .End();
 }
 
