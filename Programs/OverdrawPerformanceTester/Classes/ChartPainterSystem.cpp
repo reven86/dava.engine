ee5d1e19ceea776e060b43efd1370f852a3048c4
Aleksandr Vorontsov <a_vorontsov@wargaming.net>
2017-02-22 16:39:22 +0300

    DF-12948 Graph axis
    Former-commit-id: dfa4c1c9ca47fd3be907d038b65a6a0c4a9d930a

--- Programs/OverdrawPerformanceTester/Classes/ChartPainterSystem.cpp
+++ Programs/OverdrawPerformanceTester/Classes/ChartPainterSystem.cpp
@@ -1,6 +1,7 @@
 #include "ChartPainterSystem.h"
 
 #include "Render/2D/Systems/RenderSystem2D.h"
+#include "Render/rhi/dbg_Draw.h"
 
 namespace OverdrawPerformanceTester
 {
@@ -28,14 +29,38 @@ ChartPainterSystem::~ChartPainterSystem()
 
 void ChartPainterSystem::Process(float32 timeElapsed)
 {
+    DAVA::DbgDraw::EnsureInited();
+    uint32 TEXT_COLOR = rhi::NativeColorRGBA(1.f, 1.f, 1.f, 1.f);
+    DAVA::DbgDraw::Text2D(100, 100, TEXT_COLOR, "some text %d", 5);
+    DAVA::DbgDraw::FilledRect2D(0, 0, 200, 200, TEXT_COLOR);
+
+
+    VirtualCoordinatesSystem* vcs = DAVA::UIControlSystem::Instance()->vcs;
+//     vcs->SetVirtualScreenSize(10, 10);
+
+
+    int32 w = vcs->GetVirtualScreenSize().dx;
+    int32 h = vcs->GetVirtualScreenSize().dy;
+
+    Vector2 origin(0.1f * w, 0.9f * h);
+    Vector2 xAxis(0.9f * w, 0.9f * h);
+    Vector2 yAxis(0.1f * w, 0.1f * h);
+
+    Polygon2 p;
+    p.AddPoint(origin);
+    p.AddPoint(xAxis);
+    RenderSystem2D::Instance()->DrawPolygon(p, false, Color::White);
+    p.Clear();
+    p.AddPoint(origin);
+    p.AddPoint(yAxis);
+    RenderSystem2D::Instance()->DrawPolygon(p, false, Color::White);
+
     if (!shouldDrawGraph) return;
 
-    int32 h = Renderer::GetFramebufferHeight();
-    int32 w = Renderer::GetFramebufferWidth();
     for (int i = 0; i < 6; i++)
     {
         Polygon2 p;
-        for (int j = 0; j < performanceData[i].size(); j++)
+        for (int j = 0; j < (*performanceData)[i].size(); j++)
         {
             float32 overdraw = (*performanceData)[i][j].Overdraw;
             float32 fps = static_cast<DAVA::float32>((*performanceData)[i][j].FPS);
