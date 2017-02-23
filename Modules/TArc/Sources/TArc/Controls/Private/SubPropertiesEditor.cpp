59f575d279351860f54d8abbee08056589455f45
y_rakhuba <y_rakhuba@wargaming.net>
2017-02-23 11:26:23 +0300

    [DF-13017] PropertyPanel rendering refactoring.
    Former-commit-id: 96895166846f0892390dbb31054524d2f1a0d4df

--- Modules/TArc/Sources/TArc/Controls/Private/SubPropertiesEditor.cpp
+++ Modules/TArc/Sources/TArc/Controls/Private/SubPropertiesEditor.cpp
@@ -25,13 +25,13 @@ TControl* CreateControl(typename TControl::Fields role, const String& fieldName,
 }
 
 SubPropertiesEditor::SubPropertiesEditor(const ControlDescriptorBuilder<Fields>& fields, DataWrappersProcessor* wrappersProcessor, Reflection model, QWidget* parent)
-    : ControlProxy<QWidget>(fields, wrappersProcessor, model, parent)
+    : ControlProxyImpl<QWidget>(fields, wrappersProcessor, model, parent)
 {
     SetupControl(wrappersProcessor);
 }
 
 SubPropertiesEditor::SubPropertiesEditor(const ControlDescriptorBuilder<Fields>& fields, ContextAccessor* accessor, Reflection model, QWidget* parent)
-    : ControlProxy<QWidget>(fields, accessor, model, parent)
+    : ControlProxyImpl<QWidget>(fields, accessor, model, parent)
 {
     SetupControl(accessor);
 }
