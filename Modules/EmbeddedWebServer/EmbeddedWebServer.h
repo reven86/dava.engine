1c651f78067e04fc5980d49beb0b181a05a5b059
leanid <leanid.chaika@gmail.com>
2017-03-22 11:47:47 +0300

    DF-12956 fix more max comments
    Former-commit-id: 7509c5839ff0e4f23e78f85c3634948a8a9cfdea

--- Modules/EmbeddedWebServer/EmbeddedWebServer.h
+++ Modules/EmbeddedWebServer/EmbeddedWebServer.h
@@ -3,18 +3,18 @@
 namespace DAVA
 {
 /**
-		Start in separete thread web server.
-		Example:
-		StartEmbeddedWebServer(
-		    "/var/www",
-			"80,443s"
-		)
-		On error throw std::runtime_error exception
-	*/
-void StartEmbeddedWebServer(const char* documentRoot, const char* listeningPorts);
+	Start in separete thread web server.
+	Example:
+	StartEmbeddedWebServer(
+		"/var/www",
+		"80"
+	)
+	On error return false
+*/
+bool StartEmbeddedWebServer(const char* documentRoot, const char* listeningPorts);
 
 /**
-		Stop web server. Wait till all job threads finished.
-	*/
+	Stop web server. Wait till all job threads finished.
+*/
 void StopEmbeddedWebServer();
 }
