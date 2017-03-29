6f75b18bbd782729068337f398a97104465cb25f
leanid <leanid.chaika@gmail.com>
2017-03-29 16:37:46 +0300

    DF-11515 local android build on windows compiles
    Former-commit-id: 65b0cd6ce2b9e7160888aee57108161322fe09e6

--- Programs/UnitTests/android/jni/Android.mk
+++ Programs/UnitTests/android/jni/Android.mk
@@ -17,6 +17,7 @@ LOCAL_MODULE := UnitTests
 LOCAL_C_INCLUDES := $(DV_PROJECT_ROOT)/Classes
 LOCAL_C_INCLUDES += $(DAVA_ROOT)/Sources/Tools
 LOCAL_C_INCLUDES += $(DAVA_ROOT)/Modules/SampleModule
+LOCAL_C_INCLUDES += $(DAVA_ROOT)/Modules/EmbeddedWebServer
 
 # set exported includes
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
@@ -28,7 +29,8 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(DV_PROJECT_ROOT)/Classes/Tests/*.cpp) \
 	$(wildcard $(DAVA_ROOT)/Sources/Tools/TeamcityOutput/*.cpp) \
 	$(wildcard $(DAVA_ROOT)/Sources/Tools/CommandLine/CommandLineParser.cpp) \
-	$(wildcard $(DAVA_ROOT)/Modules/SampleModule/Private/*.cpp))
+	$(wildcard $(DAVA_ROOT)/Modules/SampleModule/Private/*.cpp) \
+	$(wildcard $(DAVA_ROOT)/Modules/EmbeddedWebServer/Private/EmbeddedWebServer.cpp))
 
 LOCAL_CPPFLAGS += -std=c++14
 
