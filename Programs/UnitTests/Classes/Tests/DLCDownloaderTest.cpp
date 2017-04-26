d31ad2bb422e1ae985b53321f2564a65ce25f29f
leanid <leanid.chaika@gmail.com>
2017-04-26 18:08:06 +0300

    DF-13430 get remote file size works again)))
    Former-commit-id: aa0df0b258229d05d8060b0aa0691c61035d3df1

--- Programs/UnitTests/Classes/Tests/DLCDownloaderTest.cpp
+++ Programs/UnitTests/Classes/Tests/DLCDownloaderTest.cpp
@@ -1,9 +1,7 @@
 #include "UnitTests/UnitTests.h"
 
-#include "DLCManager/Private/DLCDownloaderImpl.h"
-
+#include "DLCManager/DLCDownloader.h"
 #include <Logger/Logger.h>
-#include <Time/SystemTimer.h>
 #include <DLC/Downloader/DownloadManager.h>
 #include <FileSystem/FileSystem.h>
 
@@ -19,115 +17,134 @@ DAVA_TESTCLASS (DLCDownloaderTest)
     {
         using namespace DAVA;
 
-        DLCDownloaderImpl downloader;
+        DLCDownloader* downloader = DLCDownloader::Create();
         String url = URL;
-        DLCDownloader::Task* task = downloader.StartTask(url, "", DLCDownloader::TaskType::SIZE);
+        DLCDownloader::Task* task = downloader->StartTask(url, "", DLCDownloader::TaskType::SIZE);
 
-        downloader.WaitTask(task);
+        downloader->WaitTask(task);
 
-        TEST_VERIFY(task->status.state == DLCDownloader::TaskState::Finished);
-        TEST_VERIFY(task->status.sizeTotal >= 0);
+        auto& info = downloader->GetTaskInfo(task);
+        auto& status = downloader->GetTaskStatus(task);
+
+        TEST_VERIFY(info.rangeOffset == -1);
+        TEST_VERIFY(info.rangeSize == -1);
+        TEST_VERIFY(info.dstPath == "");
+        TEST_VERIFY(info.srcUrl == url);
+        TEST_VERIFY(info.timeoutSec >= 0);
+        TEST_VERIFY(info.type == DLCDownloader::TaskType::SIZE);
+
+        TEST_VERIFY(status.error.curlErr == 0);
+        TEST_VERIFY(status.error.curlEasyStrErr == nullptr);
+        TEST_VERIFY(status.error.curlMErr == 0);
+        TEST_VERIFY(status.error.fileErrno == 0);
+        TEST_VERIFY(status.sizeDownloaded == 0);
+        TEST_VERIFY(status.state.Get() == DLCDownloader::TaskState::Finished);
+        TEST_VERIFY(status.sizeTotal >= 0);
+
+        downloader->RemoveTask(task);
+
+        DLCDownloader::Destroy(downloader);
     }
 
     DAVA_TEST (DowloadLargeFileTest)
     {
-        using namespace DAVA;
+        //using namespace DAVA;
 
-        FileSystem* fs = FileSystem::Instance();
-        DLCDownloaderImpl downloader;
-        String url = URL;
-        FilePath path("~doc:/big_tmp_file_from_server.remove.me");
-        fs->DeleteFile(path);
-        String p = path.GetAbsolutePathname();
-        int64 start = 0;
-        DLCDownloader::Task* task = nullptr;
-        int64 finish = 0;
-        float seconds = 0.f;
+        //FileSystem* fs = FileSystem::Instance();
+        //DLCDownloaderImpl downloader;
+        //String url = URL;
+        //FilePath path("~doc:/big_tmp_file_from_server.remove.me");
+        //fs->DeleteFile(path);
+        //String p = path.GetAbsolutePathname();
+        //int64 start = 0;
+        //DLCDownloader::Task* task = nullptr;
+        //int64 finish = 0;
+        //float seconds = 0.f;
 
-        DownloadManager* dm = DownloadManager::Instance();
+        //DownloadManager* dm = DownloadManager::Instance();
 
-        FilePath pathOld("~doc:/big_tmp_file_from_server.old.remove.me");
-        fs->DeleteFile(pathOld);
+        //FilePath pathOld("~doc:/big_tmp_file_from_server.old.remove.me");
+        //fs->DeleteFile(pathOld);
 
-        ////----first--------------------------------------------------------
-        start = SystemTimer::GetMs();
+        //////----first--------------------------------------------------------
+        //start = SystemTimer::GetMs();
 
-        int numOfParts = 4;
+        //int numOfParts = 4;
 
-        uint32 id = dm->Download(url, pathOld, FULL);
+        //uint32 id = dm->Download(url, pathOld, FULL);
 
-        dm->Wait(id);
+        //dm->Wait(id);
 
-        finish = SystemTimer::GetMs();
+        //finish = SystemTimer::GetMs();
 
-        seconds = (finish - start) / 1000.f;
+        //seconds = (finish - start) / 1000.f;
 
-        Logger::Info("old downloader 1.5 Gb parts(%d) download from in house server for: %f", numOfParts, seconds);
-        // ----next-------------------------------------------------------
-        {
-            start = SystemTimer::GetMs();
+        //Logger::Info("old downloader 1.5 Gb parts(%d) download from in house server for: %f", numOfParts, seconds);
+        //// ----next-------------------------------------------------------
+        //{
+        //    start = SystemTimer::GetMs();
 
-            task = downloader.StartTask(url, p, DLCDownloader::TaskType::FULL);
+        //    task = downloader.StartTask(url, p, DLCDownloader::TaskType::FULL);
 
-            downloader.WaitTask(task);
-        }
+        //    downloader.WaitTask(task);
+        //}
 
-        finish = SystemTimer::GetMs();
+        //finish = SystemTimer::GetMs();
 
-        seconds = (finish - start) / 1000.f;
+        //seconds = (finish - start) / 1000.f;
 
-        Logger::Info("new downloader 1.5 Gb download from in house server for: %f", seconds);
+        //Logger::Info("new downloader 1.5 Gb download from in house server for: %f", seconds);
 
-        //-----multi-files------------------------------------------------
-        DLCDownloader::Task* taskSize = downloader.StartTask(url, "", DLCDownloader::TaskType::SIZE);
-        downloader.WaitTask(taskSize);
-        uint64 sizeTotal = taskSize->status.sizeTotal;
+        ////-----multi-files------------------------------------------------
+        //DLCDownloader::Task* taskSize = downloader.StartTask(url, "", DLCDownloader::TaskType::SIZE);
+        //downloader.WaitTask(taskSize);
+        //uint64 sizeTotal = taskSize->status.sizeTotal;
 
-        uint64 firstIndex = 0;
-        uint64 nextIndex = 0;
-        const uint64 lastIndex = sizeTotal - 1;
+        //uint64 firstIndex = 0;
+        //uint64 nextIndex = 0;
+        //const uint64 lastIndex = sizeTotal - 1;
 
-        FilePath dir("~doc:/multy_tmp/");
-        fs->DeleteDirectory(dir, true);
-        fs->CreateDirectory(dir);
+        //FilePath dir("~doc:/multy_tmp/");
+        //fs->DeleteDirectory(dir, true);
+        //fs->CreateDirectory(dir);
 
-        const size_t numAll = 1024;
-        const size_t onePart = static_cast<size_t>(sizeTotal / numAll);
+        //const size_t numAll = 1024;
+        //const size_t onePart = static_cast<size_t>(sizeTotal / numAll);
 
-        nextIndex += onePart;
+        //nextIndex += onePart;
 
-        Vector<DLCDownloader::Task*> allTasks;
-        allTasks.reserve(numAll);
+        //Vector<DLCDownloader::Task*> allTasks;
+        //allTasks.reserve(numAll);
 
-        start = SystemTimer::GetMs();
+        //start = SystemTimer::GetMs();
 
-        for (size_t i = 0; i < numAll; ++i, firstIndex += onePart, nextIndex += onePart)
-        {
-            StringStream ss;
-            ss << "part_" << std::setw(5) << std::setfill('0') << i << '_' << firstIndex << '-' << nextIndex;
-            String fileName = ss.str() + ".part";
-            FilePath pathFull = dir + fileName;
-            String full = pathFull.GetAbsolutePathname();
-            task = downloader.StartTask(url, full, DLCDownloader::TaskType::FULL, nullptr, firstIndex, nextIndex - firstIndex);
-            allTasks.push_back(task);
-        }
+        //for (size_t i = 0; i < numAll; ++i, firstIndex += onePart, nextIndex += onePart)
+        //{
+        //    StringStream ss;
+        //    ss << "part_" << std::setw(5) << std::setfill('0') << i << '_' << firstIndex << '-' << nextIndex;
+        //    String fileName = ss.str() + ".part";
+        //    FilePath pathFull = dir + fileName;
+        //    String full = pathFull.GetAbsolutePathname();
+        //    task = downloader.StartTask(url, full, DLCDownloader::TaskType::FULL, nullptr, firstIndex, nextIndex - firstIndex);
+        //    allTasks.push_back(task);
+        //}
 
-        for (auto t : allTasks)
-        {
-            downloader.WaitTask(t);
-        }
+        //for (auto t : allTasks)
+        //{
+        //    downloader.WaitTask(t);
+        //}
 
-        finish = SystemTimer::GetMs();
+        //finish = SystemTimer::GetMs();
 
-        seconds = (finish - start) / 1000.f;
+        //seconds = (finish - start) / 1000.f;
 
-        Logger::Info("1024 part of 1.5 Gb download from in house server for: %f", seconds);
+        //Logger::Info("1024 part of 1.5 Gb download from in house server for: %f", seconds);
 
-        // free memory
-        for (auto t : allTasks)
-        {
-            downloader.RemoveTask(t);
-        }
-        allTasks.clear();
+        //// free memory
+        //for (auto t : allTasks)
+        //{
+        //    downloader.RemoveTask(t);
+        //}
+        //allTasks.clear();
     }
 };
