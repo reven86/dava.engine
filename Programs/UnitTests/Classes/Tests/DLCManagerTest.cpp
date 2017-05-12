c116ecd9383c4104b00fb04ded7531dc16368ece
Maksim Molokovskih <m_molokovskih@wargaming.net>
2017-05-12 19:18:08 +0300

    [DF-13746] - DLCManagerTest.cpp, ScriptTest.cpp - disabled for test
    Former-commit-id: 10b98ef62a0e17cd41b3e725e4b789aeace050c0

--- Programs/UnitTests/Classes/Tests/DLCManagerTest.cpp
+++ Programs/UnitTests/Classes/Tests/DLCManagerTest.cpp
@@ -8,7 +8,7 @@
 #include <EmbeddedWebServer.h>
 
 #include "UnitTests/UnitTests.h"
-
+/*
 #ifndef __DAVAENGINE_WIN_UAP__
 
 struct DownloaderTest
@@ -145,3 +145,4 @@ DAVA_TESTCLASS (DLCManagerTest)
 };
 
 #endif // __DAVAENGINE_WIN_UAP__
+*/
