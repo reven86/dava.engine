5b6b7c1d29b431be403d426ef3c89b84b592d404
Maksim Lazarev <m_lazarev@wargaming.net>
2017-05-22 13:55:29 +0300

    DF-13764 linux: port Engine folder
    Former-commit-id: d37aad85f01931c06e153044125aff9998b1d72d

---
+++ Sources/Internal/Engine/Private/Linux/PlatformCoreLinux.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Base/BaseTypes.h"
+
+#if defined(__DAVAENGINE_LINUX__)
+
+#include "Base/Platform.h"
+#include "Engine/Private/EnginePrivateFwd.h"
+
+namespace DAVA
+{
+namespace Private
+{
+class PlatformCore final
+{
+public:
+    PlatformCore(EngineBackend* engineBackend);
+    ~PlatformCore();
+
+    void Init();
+    void Run();
+    void PrepareToQuit();
+    void Quit();
+
+private:
+    EngineBackend& engineBackend;
+};
+
+} // namespace Private
+} // namespace DAVA
+
+#endif // __DAVAENGINE_LINUX__
