1bdf1ba81b8ed2631a41a33eead223f6533d748a
Maksim Molokovskih <m_molokovskih@wargaming.net>
2017-06-04 14:49:09 +0300

    [DF-13883] - Modified checking returning code, after exit PVS-Studio_Cmd.exe
    Former-commit-id: cca312cdd6f868ebbc888b0a60d9ebc7bd48eca9

--- Bin/RepoTools/PvsCheck/PvsCheck.py
+++ Bin/RepoTools/PvsCheck/PvsCheck.py
@@ -2,6 +2,16 @@ import subprocess
 import sys
 import argparse
 
+ERROR_LEVEL_1 = 1    # FilesFail
+ERROR_LEVEL_2 = 2    # GeneralExeption
+ERROR_LEVEL_3 = 4    # IncorrectArguments
+ERROR_LEVEL_4 = 8    # FileNotFound
+ERROR_LEVEL_5 = 16   # IncorrectConfiguration
+ERROR_LEVEL_6 = 32   # IncorrectCfg
+ERROR_LEVEL_7 = 64   # IncorrectExtension
+ERROR_LEVEL_8 = 128  # IncorrectLicense
+ERROR_LEVEL_9 = 256  # AnalysisDiff
+
 def teamcity_message(error, status):
     teamcity_error = "##teamcity[message text='{0}' status='{2}']\n" \
         .format(error, status)
@@ -21,37 +31,36 @@ def main():
         "--settings", "Settings.xml"])
     proc.communicate()
 
+    if proc.returncode & ERROR_LEVEL_1:
+        teamcity_message("error (crash) during analysis of some source file(s)", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_2:
+        teamcity_message("general (nonspecific) error in the analyzer|'s operation, a possible handled exception", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_3:
+        teamcity_message("some of the command line arguments passed to the tool were incorrect", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_4:
+        teamcity_message("some of the analyzed source files or project files were not found", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_5:
+        teamcity_message("specified configuration and (or) platform were not found in a solution file", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_6:
+        teamcity_message("solution file is not supported", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_7:
+        teamcity_message("incorrect extension of analyzed project", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_8:
+        teamcity_message("incorrect or out-of-date analyzer license", "ERROR")
+
+    if proc.returncode & ERROR_LEVEL_9:
+        teamcity_message("some issues were found in the source code", "ERROR")
+
     if proc.returncode == 0:
         teamcity_message("analysis was successfully completed, no issues were found in the source code", "SUCCESS")
         sys.exit(proc.returncode)
-
-    if proc.returncode == 1:
-        teamcity_message("error (crash) during analysis of some source file(s)", "ERROR")
-
-    if proc.returncode == 2:
-        teamcity_message("general (nonspecific) error in the analyzer|'s operation, a possible handled exception", "ERROR")
-
-    if proc.returncode == 4:
-        teamcity_message("some of the command line arguments passed to the tool were incorrect", "ERROR")
-
-    if proc.returncode == 8:
-        teamcity_message("some of the analyzed source files or project files were not found", "ERROR")
-
-    if proc.returncode == 16:
-        teamcity_message("specified configuration and (or) platform were not found in a solution file", "ERROR")
-
-    if proc.returncode == 32:
-        teamcity_message("solution file is not supported", "ERROR")
-
-    if proc.returncode == 64:
-        teamcity_message("incorrect extension of analyzed project", "ERROR")
-
-    if proc.returncode == 128:
-        teamcity_message("incorrect or out-of-date analyzer license", "ERROR")
-
-    if proc.returncode == 128:
-        teamcity_message("some issues were found in the source code", "ERROR")
-
 
     proc = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PlogConverter.exe",
                              "-t", "Html",
