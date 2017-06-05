e669a3a3fa0ec66c57a70a356c4a2c76c17c6f03
Maksim Molokovskih <m_molokovskih@wargaming.net>
2017-06-05 17:58:16 +0300

    Revert "[DF-13883] - return old version for checking"
    This reverts commit 26f0e54598721508cc35e06921f7b760321a0883 [formerly 9b72c52c174d6f41fb51643623e2c2f6363d9823].
    
    
    Former-commit-id: edbc69f49ed25419af79d45cce8021a16223f28a

--- Bin/RepoTools/PvsCheck/PvsCheck.py
+++ Bin/RepoTools/PvsCheck/PvsCheck.py
@@ -2,6 +2,19 @@ import subprocess
 import sys
 import argparse
 
+ERROR_LEVEL_1 = 1    # FilesFail
+ERROR_LEVEL_2 = 2    # GeneralExeption
+ERROR_LEVEL_3 = 4    # IncorrectArguments
+ERROR_LEVEL_4 = 8    # FileNotFound
+ERROR_LEVEL_5 = 16   # IncorrectConfiguration
+ERROR_LEVEL_6 = 32   # IncorrectCfg
+ERROR_LEVEL_7 = 64   # IncorrectExtension
+ERROR_LEVEL_8 = 128  # IncorrectLicense
+ERROR_LEVEL_9 = 256  # AnalysisDiff
+
+def teamcity_message(error, status):
+    teamcity_error = "##teamcity[message text='{0}' status='{1}']\n".format(error, status)
+    print (teamcity_error)
 
 def main():
     parser = argparse.ArgumentParser(description='Analyze .sln with PVS')
@@ -10,31 +23,58 @@ def main():
 
     args = parser.parse_args()
 
-    proc = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PVS-Studio_Cmd.exe",
+    pvs_process = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PVS-Studio_Cmd.exe",
                              "--progress",
                              "--target", args.sln_path,
                              "--output", args.output_log,
                              "--settings", "Settings.xml"])
-    proc.communicate()
-    if proc.returncode != 0 and proc.returncode != 7:
-        sys.exit(proc.returncode)
+    pvs_process.communicate()
 
-    proc = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PlogConverter.exe",
+    return_code = pvs_process.returncode
+
+    if return_code & ERROR_LEVEL_1:
+        teamcity_message("error (crash) during analysis of some source file(s)", "ERROR")
+
+    if return_code & ERROR_LEVEL_2:
+        teamcity_message("general (nonspecific) error in the analyzer|'s operation, a possible handled exception",
+                         "ERROR")
+
+    if return_code & ERROR_LEVEL_3:
+        teamcity_message("some of the command line arguments passed to the tool were incorrect", "ERROR")
+
+    if return_code & ERROR_LEVEL_4:
+        teamcity_message("some of the analyzed source files or project files were not found", "ERROR")
+
+    if return_code & ERROR_LEVEL_5:
+        teamcity_message("specified configuration and (or) platform were not found in a solution file", "ERROR")
+
+    if return_code & ERROR_LEVEL_6:
+        teamcity_message("solution file is not supported", "ERROR")
+
+    if return_code & ERROR_LEVEL_7:
+        teamcity_message("incorrect extension of analyzed project", "ERROR")
+
+    if return_code & ERROR_LEVEL_8:
+        teamcity_message("incorrect or out-of-date analyzer license", "ERROR")
+
+    if return_code & ERROR_LEVEL_9:
+        teamcity_message("some issues were found in the source code", "ERROR")
+
+    if return_code == 0:
+        teamcity_message("analysis was successfully completed, no issues were found in the source code", "SUCCESS")
+        sys.exit(return_code)
+
+    converter_process = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PlogConverter.exe",
                              "-t", "Html",
                              "-a", "GA:1",
                              "-d", "V520",
                              args.output_log])
-    proc.communicate()
-    if proc.returncode != 0:
-        sys.exit(proc.returncode)
+    converter_process.communicate()
 
-    file = open("log.plog.html", "r")
-    content = file.read()
-    if content.find("No Messages Generated") == -1:
-        errorMsg = "##teamcity[message text=\'PVS found some issues, see .plog.html for details\' errorDetails=\'\' status=\'" + "ERROR" + "\']\n"
-        print errorMsg
-        sys.exit(7)
-
+    if converter_process.returncode != 0:
+        sys.exit(converter_process.returncode)
+    else:
+        sys.exit(return_code)
 
 if "__main__" == __name__:
     main()
