b4b24496cf59e0869b5dcc8ab48c5badbd33f83c
Maksim Molokovskih <m_molokovskih@wargaming.net>
2017-06-06 10:25:18 +0300

    [DF-13883] - Adding messages for starting PVS analyze and end, replace print to stdout.write. Adding argument to set path, for PVS Studio
    Former-commit-id: aee7f29e1ad9c369d94d0d4cac5b59334d5f1d6a

--- Bin/RepoTools/PvsCheck/PvsCheck.py
+++ Bin/RepoTools/PvsCheck/PvsCheck.py
@@ -12,18 +12,29 @@ ERROR_LEVEL_7 = 64   # IncorrectExtension
 ERROR_LEVEL_8 = 128  # IncorrectLicense
 ERROR_LEVEL_9 = 256  # AnalysisDiff
 
-def teamcity_message(error, status):
-    teamcity_error = "##teamcity[message text='{0}' status='{1}']\n".format(error, status)
-    print (teamcity_error)
+PVS_STUDIO_PATH = 'C:\Program Files (x86)\PVS-Studio'
+
+def teamcity_message(error, errorDetails, status):
+    sys.stdout.write("##teamcity[message text='{0}' errorDetails='{1}' status='{2}']\n".format(error,
+                                                                                               errorDetails,
+                                                                                               status))
+    sys.stdout.flush()
 
 def main():
+    sys.stdout.write("##teamcity[progressStart 'PVS analyze started...']\n")
+    sys.stdout.flush()
+
     parser = argparse.ArgumentParser(description='Analyze .sln with PVS')
+    parser.add_argument('--pvsPath',
+                        dest='pvs_path',
+                        default=PVS_STUDIO_PATH,
+                        help="path to PVS studio, default: " + PVS_STUDIO_PATH)
     parser.add_argument('--output', dest='output_log', default='log.plog', help="path to PVS log output file")
     parser.add_argument("--sln", dest="sln_path", help="path to a project solution", required=True)
 
     args = parser.parse_args()
 
-    pvs_process = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PVS-Studio_Cmd.exe",
+    pvs_process = subprocess.Popen([args.pvs_path + "\PVS-Studio_Cmd.exe",
                              "--progress",
                              "--target", args.sln_path,
                              "--output", args.output_log,
@@ -34,47 +45,71 @@ def main():
     return_code = pvs_process.returncode
 
     if return_code & ERROR_LEVEL_1:
-        teamcity_message("error (crash) during analysis of some source file(s)", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Error (crash) during analysis of some source file(s)",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_2:
-        teamcity_message("general (nonspecific) error in the analyzer|'s operation, a possible handled exception",
+        teamcity_message("PVS found some issues",
+                         "General (nonspecific) error in the analyzer|'s operation, a possible handled exception",
                          "ERROR")
 
     if return_code & ERROR_LEVEL_3:
-        teamcity_message("some of the command line arguments passed to the tool were incorrect", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Some of the command line arguments passed to the tool were incorrect",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_4:
-        teamcity_message("some of the analyzed source files or project files were not found", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Some of the analyzed source files or project files were not found",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_5:
-        teamcity_message("specified configuration and (or) platform were not found in a solution file", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Specified configuration and (or) platform were not found in a solution file",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_6:
-        teamcity_message("solution file is not supported", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Solution file is not supported",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_7:
-        teamcity_message("incorrect extension of analyzed project", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "incorrect extension of analyzed project",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_8:
-        teamcity_message("incorrect or out-of-date analyzer license", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Incorrect or out-of-date analyzer license",
+                         "ERROR")
 
     if return_code & ERROR_LEVEL_9:
-        teamcity_message("some issues were found in the source code", "ERROR")
+        teamcity_message("PVS found some issues",
+                         "Some issues were found in the source code",
+                         "ERROR")
 
     if return_code == 0:
-        teamcity_message("analysis was successfully completed, no issues were found in the source code", "SUCCESS")
+        teamcity_message("PVS not found any issues",
+                         "Analysis was successfully completed, no issues were found in the source code",
+                         "SUCCESS")
+
         sys.exit(return_code)
 
-    converter_process = subprocess.Popen(["C:\Program Files (x86)\PVS-Studio\PlogConverter.exe",
-                             "-t", "Html",
-                             "-a", "GA:1",
-                             "-d", "V520",
-                             args.output_log])
+    sys.stdout.write("##teamcity[progressFinish 'PVS analyze finished...']\n")
+    sys.stdout.flush()
 
-    converter_process.communicate()
+    if return_code != 0:
+        converter_process = subprocess.Popen([args.pvs_path + "\PlogConverter.exe",
+                                              "-t", "Html",
+                                              "-a", "GA:1",
+                                              "-d", "V520",
+                                              args.output_log])
 
-    if converter_process.returncode != 0:
-        sys.exit(converter_process.returncode)
+        converter_process.communicate()
+
+        if converter_process.returncode != 0:
+            sys.exit(converter_process.returncode)
     else:
         sys.exit(return_code)
 
