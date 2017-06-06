87b5f92b1aad6f12a0ad4d2288b4844fc5a28efe
Ivan Petrochenko <i_petrochenko@wargaming.net>
2017-06-06 17:43:04 +0300

    DF-11834 sorting components as in old enum code
    Former-commit-id: b6f7a592cb7358b336e5a2e2aef17df79e738e21

--- Sources/Internal/ReflectionDeclaration/Private/ReflectionDeclaration.cpp
+++ Sources/Internal/ReflectionDeclaration/Private/ReflectionDeclaration.cpp
@@ -304,15 +304,15 @@ void RegisterPermanentNames()
 DAVA_REFLECTION_REGISTER_CUSTOM_PERMANENT_NAME(type, string); \
 GetEngineContext()->componentManager->RegisterComponent<type>();
 
-    DELC_UI_COMPONENT(UISoundComponent, "Sound");
-    DELC_UI_COMPONENT(UISoundValueFilterComponent, "SoundValueFilter");
-    DELC_UI_COMPONENT(UIControlBackground, "Background");
     DELC_UI_COMPONENT(UILinearLayoutComponent, "LinearLayout");
     DELC_UI_COMPONENT(UIFlowLayoutComponent, "FlowLayout");
     DELC_UI_COMPONENT(UIFlowLayoutHintComponent, "FlowLayoutHint");
     DELC_UI_COMPONENT(UIIgnoreLayoutComponent, "IgnoreLayout");
     DELC_UI_COMPONENT(UISizePolicyComponent, "SizePolicy");
     DELC_UI_COMPONENT(UIAnchorComponent, "Anchor");
+    DELC_UI_COMPONENT(UILayoutSourceRectComponent, "UILayoutSourceRectComponent");
+    DELC_UI_COMPONENT(UILayoutIsolationComponent, "UILayoutIsolationComponent");
+    DELC_UI_COMPONENT(UIControlBackground, "Background");
     DELC_UI_COMPONENT(UIModalInputComponent, "ModalInput");
     DELC_UI_COMPONENT(UIFocusComponent, "Focus");
     DELC_UI_COMPONENT(UIFocusGroupComponent, "FocusGroup");
@@ -321,13 +321,13 @@ GetEngineContext()->componentManager->RegisterComponent<type>();
     DELC_UI_COMPONENT(UIActionComponent, "Action");
     DELC_UI_COMPONENT(UIActionBindingComponent, "ActionBinding");
     DELC_UI_COMPONENT(UIScrollBarDelegateComponent, "ScrollBarDelegate");
-    DELC_UI_COMPONENT(UIUpdateComponent, "UIUpdateComponent");
+    DELC_UI_COMPONENT(UIScrollComponent, "ScrollComponent");
+    DELC_UI_COMPONENT(UISoundComponent, "Sound");
+    DELC_UI_COMPONENT(UISoundValueFilterComponent, "SoundValueFilter");
+    DELC_UI_COMPONENT(UIUpdateComponent, "Update");
     DELC_UI_COMPONENT(UICustomUpdateDeltaComponent, "CustomDeltaUpdate");
     DELC_UI_COMPONENT(UIRichContentComponent, "RichContent");
     DELC_UI_COMPONENT(UIRichContentObjectComponent, "RichContentObject");
-    DELC_UI_COMPONENT(UILayoutSourceRectComponent, "UILayoutSourceRectComponent");
-    DELC_UI_COMPONENT(UILayoutIsolationComponent, "UILayoutIsolationComponent");
-    DELC_UI_COMPONENT(UIScrollComponent, "ScrollComponent");
     DELC_UI_COMPONENT(UISceneComponent, "SceneComponent");
     DELC_UI_COMPONENT(UIDebugRenderComponent, "DebugRender");
     DELC_UI_COMPONENT(UIClipContentComponent, "ClipContent");
