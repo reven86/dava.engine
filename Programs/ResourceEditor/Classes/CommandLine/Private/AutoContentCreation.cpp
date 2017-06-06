acd043d0e4116fdd25c8eb7b5eef4540f3578534
Ivan Petrochenko <i_petrochenko@wargaming.net>
2017-06-06 12:01:12 +0300

    DF-11834 fix after merge
    Former-commit-id: c9c856d20bc81d56854548167d2435278722c6b4

--- Programs/ResourceEditor/Classes/CommandLine/Private/AutoContentCreation.cpp
+++ Programs/ResourceEditor/Classes/CommandLine/Private/AutoContentCreation.cpp
@@ -11,6 +11,8 @@
 #include <Logger/Logger.h>
 #include <Base/ScopedPtr.h>
 
+#include <TArc/Utils/ModuleCollection.h>
+
 DuplicateObjectTool::DuplicateObjectTool(const DAVA::Vector<DAVA::String>& commandLine)
     : CommandLineModule(commandLine, "-duplicate")
 {
